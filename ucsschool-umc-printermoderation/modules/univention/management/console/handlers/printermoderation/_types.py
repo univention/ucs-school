3024c968f789a3c628f7b7142ff5d729f679f654
Sönke Schwardt-Krummrich <schwardt@univention.de>
2009-07-02 10:08:14 +0000

    copied from customer scope - warning: new directory name (Bug #14958)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/component/ucsschool@10665 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ ucsschool-umc-printermoderation/modules/univention/management/console/handlers/printermoderation/_types.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python2.4
+# -*- coding: utf-8 -*-
+#
+# Univention Management Console
+#  module: printer moderation module
+#
+# Copyright (C) 2007 Univention GmbH
+#
+# http://www.univention.de/
+#
+# All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# Binary versions of this file provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+import univention.management.console as umc
+
+_ = umc.Translation( 'univention.management.console.handlers.printermoderation' ).translate
+
+ou = umc.String( _( 'School' ) )
+user = umc.String( _( 'User' ) )
+group = umc.String( _( 'Group' ) )
+filename = umc.String( _( 'Filename' ) )
+printer = umc.String( _( 'Printer' ) )
+jobs = umc.StringList (_ ('DNs and Filenames'))
