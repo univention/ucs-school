0e388567fd606aab72c0ce50e2510f818cd557fc
Sönke Schwardt-Krummrich <schwardt@univention.de>
2009-07-02 16:05:49 +0000

    * fixed package description (Bug #14958) * renamed package * fixed postinst * added copyright * renamed UCR variables   system/old_sharedir_target ==> ucsschool/listener/oldsharedir/targetdir   system/old_sharedir_prefixes ==> ucsschool/listener/oldsharedir/prefixes
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/component/ucsschool@10697 1b283449-9f2f-0410-b571-8eb228e1a901

--- ucs-school-old-sharedirs/remove-old-sharedirs.py
+++ ucs-school-old-sharedirs/remove-old-sharedirs.py
@@ -1,3 +1,32 @@
+#
+# Univention UCS@School
+#  listener module
+#
+# Copyright (C) 2007-2009 Univention GmbH
+#
+# http://www.univention.de/
+#
+# All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# Binary versions of this file provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
 name='remove-old-sharedirs'
 description='moves directories of removed group shares to backup folder'
 filter='(objectClass=univentionShare)'
@@ -9,8 +38,8 @@ import re
 import univention.config_registry, commands, sys, os
 import univention.debug
 
-target_dir_config = "system/old_sharedir_target"
-source_dir_prefixes = "system/old_sharedir_prefixes"
+target_dir_config = "ucsschool/listener/oldsharedir/targetdir"
+source_dir_prefixes = "ucsschool/listener/oldsharedir/prefixes"
 
 def check_target_dir(configRegistry):
 	# either returns "" if everything is ok, or returns an error message
