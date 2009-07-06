bc510d3231e8548e8538f2273aac3128cca7ea8b
Sönke Schwardt-Krummrich <schwardt@univention.de>
2009-07-06 12:29:28 +0000

    * removed outdated packages (Bug #14958)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/component/ucsschool@10761 1b283449-9f2f-0410-b571-8eb228e1a901

--- univention-management-console-distribution/debian/univention-management-console-distribution.cron.d
+++
@@ -1,4 +0,0 @@
-SHELL=/bin/sh
-PATH=/sbin:/bin:/usr/sbin:/usr/bin
-
-20 3 * * * root find /tmp/webui/ -mtime +2 -name univention-management-console.*uploadFile.tmp | xargs -n20 -r rm -f >> /dev/null
