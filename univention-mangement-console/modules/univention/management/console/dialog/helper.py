ec7a422b88f10db5a6a580ea6670ae85c260208f
Sönke Schwardt-Krummrich <schwardt@univention.de>
2009-07-06 15:11:03 +0000

    copied from customer scope (Bug #14958)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/component/ucsschool@10779 1b283449-9f2f-0410-b571-8eb228e1a901

---
+++ univention-mangement-console/modules/univention/management/console/dialog/helper.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python2.4
+# -*- coding: utf-8 -*-
+#
+# Univention Management Console
+#  base classes for UMCP dialogs
+#
+# Copyright (C) 2006-2009 Univention GmbH
+#
+# http://www.univention.de/
+#
+# All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# Binary versions of this file provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+import univention.management.console.tools as umct
+import univention.management.console.values as umcv
+import univention.management.console.protocol as umcp
+import univention.management.console.locales as locales
+
+import base
+import image
+import button
+import input
+import widget
+
+_ = locales.Translation( 'univention.management.console.dialog' ).translate
+
+class InfoBox( base.Text, image.Image ):
+	def __init__( self, text = '', columns = 1, icon = 'actions/info', size = umct.SIZE_SMALL ):
+		image.Image.__init__( self, icon, size )
+		base.Text.__init__( self, text, attributes = { 'colspan' : str( columns ) } )
+
+class Question( base.List ):
+	def __init__( self, text = '', actions = [], okay = _( 'Ok' ) ):
+		base.List.__init__( self )
+		self._image = image.Image( 'actions/info', umct.SIZE_SMALL )
+		self._text = base.Text( text, attributes = { 'colspan' : '2' } )
+		self.add_row( [ self._image, self._text ] )
+		btn = button.CancelButton()
+		self.add_row( [ '', button.Button( okay, 'actions/ok', actions = actions ), btn ] )
+
+class SearchForm( base.List ):
+	def __init__( self, command = None, fields = [], opts = {}, search_button_label = _("Search"), paged_results = False ):
+		base.List.__init__( self )
+		ids = []
+		defaults = {}
+		for row in fields:
+			line = []
+			for pair in row:
+				if isinstance( pair, ( list, tuple ) ):
+					cell, default = pair
+					line.append( cell )
+					if isinstance( cell, input.Input ):
+						ids.append( cell.id() )
+						defaults[ cell.id() ] = default
+				else:
+					line.append( pair )
+			self.add_row( line )
+		req = umcp.Command( args = [ command ], opts = opts )
+		btn = button.SearchButton( button.Action( req, ids ), label = search_button_label )
+		btn.close_dialog = False
+		reset = button.ResetButton( fields = defaults )
+		reset.close_dialog = False
+		if paged_results:
+			num_result = widget.make_readonly( ( None, umcv.Integer( _( 'Results per page' ) ) ),
+										   attributes = { 'width' : '100' } )
+		else:
+			num_result = base.Fill(1)
+		btnlst = base.List()
+		btnlst.add_row( [ btn, reset ] )
+		self.add_row( [ num_result, btnlst ] )
+		self.add_row( [ base.Fill( 2 ) ] )
+
+HelperTypes = ( type( InfoBox() ), type( Question() ), type( SearchForm() ) )
---
+++ univention-mangement-console/modules/univention/management/console/handlers/services/_revamp.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+#
+# Univention Management Console
+#  service module: revamps dialog result for the web interface
+#
+# Copyright (C) 2007-2009 Univention GmbH
+#
+# http://www.univention.de/
+#
+# All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# Binary versions of this file provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+import string
+
+import univention.management.console as umc
+import univention.management.console.tools as umct
+import univention.management.console.dialog as umcd
+import univention.management.console.protocol as umcp
+
+import univention.debug as ud
+
+_ = umc.Translation( 'univention.management.console.handlers.services' ).translate
+
+class Web( object ):
+	def _web_service_list( self, object, res ):
+		lst = umcd.List()
+		servs = res.dialog
+		boxes = []
+		lst.set_header( [ umcd.Fill( 2, _( 'Name' ) ), _( 'Status' ), _( 'Start type' ), _( 'Description' ) ] )
+		for name, srv in servs.items():
+			if srv.running:
+				icon = umcd.Image( 'actions/yes', umct.SIZE_SMALL )
+			else:
+				icon = umcd.Image( 'actions/no', umct.SIZE_SMALL )
+			chk = umcd.Checkbox( static_options = { 'service' : name } )
+			boxes.append( chk.id() )
+			image = umcd.Image( 'services/default', umct.SIZE_MEDIUM )
+			type = _( 'automatically' )
+			if srv.autostart and srv.autostart == "yes":
+				type = _( 'automatically' )
+			elif srv.autostart and srv.autostart == "manually":
+				type = _( 'manual' )
+			elif srv.autostart and srv.autostart == "no":
+				type = _( 'never' )
+			lst.add_row( [ image, name, icon, type, srv[ 'description' ], chk ] )
+		req = umcp.Command( args = [], opts= { 'service' : [] } )
+		req_list = umcp.Command( args = [ 'service/list' ],
+								 opts = {} )
+		actions = ( umcd.Action( req, boxes, True ), umcd.Action( req_list ) )
+		choices = [ ( 'service/start', _( 'Start services' ) ),
+					( 'service/stop', _( 'Stop services' ) ),
+					( 'service/start_auto', _( 'Start automatically' ) ),
+					( 'service/start_manual', _( 'Start manually' ) ), 
+					( 'service/start_never', _( 'Start never' ) ), ]
+		select = umcd.SelectionButton( _( 'Select the operation' ), choices, actions )
+		lst.add_row( [ umcd.Fill( 5 ), select ] )
+		res.dialog = [ lst ]
+		self.revamped( object.id(), res )
