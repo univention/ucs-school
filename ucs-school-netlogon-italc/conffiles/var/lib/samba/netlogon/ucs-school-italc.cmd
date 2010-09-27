4d21b54a302bfe93ff819812cd5b6f7d3d0e4358
Sönke Schwardt-Krummrich <schwardt@univention.de>
2010-09-27 13:00:38 +0000

    * link italc public key to /var/lib/samba/netlogon/italc/ (Bug #20136) * moved ucs-school-italc.cmd to /var/lib/samba/netlogon/
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/trunk/component/ucsschool@20008 1b283449-9f2f-0410-b571-8eb228e1a901

--- ucs-school-netlogon-italc/conffiles/var/lib/samba/netlogon/ucs-school-italc.cmd
+++ ucs-school-netlogon-italc/conffiles/var/lib/samba/netlogon/ucs-school-italc.cmd
@@ -3,7 +3,7 @@ REM ---
 REM script copys teachers/systems iTALC public key to windows system to enable remote logins
 echo Copying iTALC public key to local system
 @!@
-print 'copy /Y "%s" "%s"' % ( configRegistry.get('samba/netlogonscript/italc/key/public/serverpath', '\\%dctemp%\config\scripts\italc-key.pub'),
+print 'copy /Y "%s" "%s"' % ( configRegistry.get('samba/netlogonscript/italc/key/public/serverpath', '%LOGONSERVER%\netlogon\italc\italc-key.pub'),
 							  configRegistry.get('samba/netlogonscript/italc/key/public/localpath', 'C:\\Programme\\iTALC\\keys\\public\\teacher\\key') )
 @!@
 echo iTALC public key is installed and becomes active on next system reboot
