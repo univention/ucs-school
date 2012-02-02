125a64a3d886619baefe05346bee6623c736b950
Felix Botner <botner@univention.de>
2012-02-02 13:57:47 +0000

    * timestamp name * check fs on source and destination * shutil.move() * check fs with whitelist (Bug #25694)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs-school@30647 1b283449-9f2f-0410-b571-8eb228e1a901

--- ucs-school-old-sharedirs/remove-old-sharedirs.py
+++ ucs-school-old-sharedirs/remove-old-sharedirs.py
@@ -32,115 +32,151 @@
 __package__=''  # workaround for PEP 366
 import listener
 import re
-import univention.config_registry, commands, sys, os
+import commands
+import sys
+import os
+import time
+import shutil
 import univention.debug
 
+hostname=listener.baseConfig['hostname']
+domainname=listener.baseConfig['domainname']
+ip=listener.baseConfig['interfaces/eth0/address']
 name='remove-old-sharedirs'
 description='moves directories of removed group shares to backup folder'
-filter='(objectClass=univentionShare)'
+filter='(&(objectClass=univentionShare)(|(univentionShareHost=%s.%s)(univentionShareHost=%s)))' % (hostname, domainname, ip)
 attributes=[]
 modrdn='1'
 
-target_dir_config = "ucsschool/listener/oldsharedir/targetdir"
-source_dir_prefixes = "ucsschool/listener/oldsharedir/prefixes"
+DEFAUL_FS="ext2/ext3:ext2:ext3:ext4:xfs:btrfs"
+TARGET_BLACKLIST="/:/boot:/sys:/proc:/etc:/dev"
 
-def check_target_dir(configRegistry):
-	# either returns "" if everything is ok, or returns an error message
+target_dir = listener.configRegistry.get("ucsschool/listener/oldsharedir/targetdir")
+prefixlist = listener.configRegistry.get("ucsschool/listener/oldsharedir/prefixes", "").split(":")
+fs_types = listener.configRegistry.get("ucsschool/listener/oldsharedir/fs_types", DEFAUL_FS).split(":")
 
-	if not configRegistry.has_key(target_dir_config):
-		return "%s is not set" % target_dir_config
+# either returns "" if everything is ok, or returns an error message
+def check_target_dir(dir):
 
-	target_dir=configRegistry[target_dir_config]
+	if not dir:
+		return "targetdir is not set"
 
-	if os.path.exists(target_dir) and not os.path.isdir(target_dir):
-		return "%s is not a directory" % target_dir
+	# check target blacklist
+	tmp = dir.rstrip("/")
+	for i in TARGET_BLACKLIST.split(":"):
+		if not tmp or tmp == i:
+			return "%s as target dir is invalid" % dir
 
-	if not os.path.isdir(target_dir):
-		# create directory
+	if os.path.exists(dir) and not os.path.isdir(dir):
+		return "%s is not a directory" % dir
+
+	# create directory
+	if not os.path.isdir(dir):
 		listener.setuid(0)
-		ret,ret_str = commands.getstatusoutput("mkdir -p '%s'" % target_dir)
-		if not ret==0:
-			return "failed to create target directory %s" % target_dir
+		try:
+			os.makedirs(dir)
+		except:
+			return "failed to create target directory %s" % dir
+		finally:
+			listener.unsetuid()
+
+	# check fs
+	ret = check_filesystem(dir)
+	if ret:
+		return ret
 
 	return ""
 
-def check_source_dir(configRegistry, share_dir):
-	# either returns "" if everything is ok, or returns an error message
+# either returns "" if everything is ok, or returns an error message
+def check_source_dir(prefixlist, dir):
 
-	if not configRegistry.has_key(source_dir_prefixes):
+	# check dir
+	if not os.path.isdir(dir):
+		return "%s is not a directory" % dir
+
+	# check fs
+	ret = check_filesystem(dir)
+	if ret:
+		return ret
+
+	# check allowed prefix
+	if not prefixlist:
 		return "%s is not set" % source_dir_prefixes
-
-	prefixlist=configRegistry[source_dir_prefixes].split(":")
-
 	for prefix in prefixlist:
-		if share_dir.startswith(prefix):
+		if dir.startswith(prefix):
 			return ""
 
-	return "%s is not matched by %s" % (share_dir, source_dir_prefixes)
+	return "%s is not matched by %s" % (dir, source_dir_prefixes)
+
+# make sure that we are dealing with a known filesystem
+def check_filesystem(dir):
+
+	ret, out = commands.getstatusoutput("LC_ALL=C stat -f '%s'" % dir)
+	myFs = ""
+	for line in out.split("\n"):
+		tmp = line.split("Type: ")
+		if len(tmp) == 2:
+			myFs = tmp[1].strip()
+			for fs in fs_types:
+				if fs.lower() == myFs.lower():
+					# ok, 
+					return ""
+				break
+	return "%s for %s is not on a known filesystem" % (myFs, dir)
+
+# move directory
+def move_dir(src, dst, listener):
+
+	newName = os.path.basename(src) + ".%s" % int(time.time())
+	dst = os.path.join(dst, newName)
+	ret = ""
+
+	listener.setuid(0)
+	try:
+		shutil.move(src, dst)
+	except Exception, e:
+		ret = str(e)
+	finally:
+		listener.unsetuid()
+
+	return ret
 
 def handler(dn, new, old, command):
 
-	configRegistry = univention.config_registry.ConfigRegistry()
-	configRegistry.load()
-
 	# remove empty share directories
-	if old and not new and command == "d":
+	# if object is really removed (not renamed)
+	if old and not new and not command == "r":
 
-		if old.has_key('univentionShareHost'):
-			fqdn = '%s.%s' % (configRegistry['hostname'], configRegistry['domainname'])
-			if not fqdn in old['univentionShareHost']:
-				univention.debug.debug(univention.debug.LISTENER, univention.debug.WARN, "Didn't do anything because this server is not in the share host list cn=%s" % (old["cn"][0]))
-				return
-		else:
-			univention.debug.debug(univention.debug.LISTENER, univention.debug.WARN, "not removing share directory of share %s: univentionShareHost ist not set" % (old["cn"][0]))
+		name = old["cn"][0]
+
+		# check object
+		if not old.get("univentionShareHost") or not old.get("univentionSharePath"):
+			univention.debug.debug(
+				univention.debug.LISTENER, univention.debug.WARN,
+				"not removing directory of share %s: univentionShareHost(Path) ist not set" % name)
 			return
+
+		share_dir = old["univentionSharePath"][0]
 
 		# check if target directory is okay
-
-		ret=check_target_dir(configRegistry)
-		if not ret=="":
-			univention.debug.debug(univention.debug.LISTENER, univention.debug.WARN, "not removing share directory of share %s: %s" % (old["cn"][0], ret))
+		ret = check_target_dir(target_dir)
+		if ret:
+			univention.debug.debug(
+				univention.debug.LISTENER, univention.debug.WARN,
+				"not removing directory of share %s: %s" % (name, ret))
 			return
-
-		target_dir=configRegistry[target_dir_config]
 
 		# check source (share) directory
-
-		share_dir=""
-		if not old.has_key("univentionSharePath"):
-			univention.debug.debug(univention.debug.LISTENER, univention.debug.WARN, "not removing share directory of share %s: univentionSharePath is not set" % old["cn"][0])
-			return
-		else:
-			share_dir=old["univentionSharePath"][0]
-
-		ret=check_source_dir(configRegistry, share_dir)
-		if not ret=="":
-			univention.debug.debug(univention.debug.LISTENER, univention.debug.WARN, "not removing share directory of share %s: %s" % (old["cn"][0], ret))
+		ret = check_source_dir(prefixlist, share_dir)
+		if ret:
+			univention.debug.debug(
+				univention.debug.LISTENER, univention.debug.WARN, 
+				"not removing share directory of share %s: %s" % (name, ret))
 			return
 
-		if not os.path.isdir(share_dir):
-			univention.debug.debug(univention.debug.LISTENER, univention.debug.WARN, "not removing share directory of share %s: it does not exist or is no directory" % old["cn"][0])
-			return
-
-		# make sure that we are dealing with a local filesystem
-		ret, ret_str = commands.getstatusoutput("stat -f '%s' | grep 'Type:' | sed 's/.*Type:\ //'" % share_dir)
-
-		if ret_str.strip() in ["nfs", "nfs4", "cifs", "smbfs", "nfsd"]:
-			univention.debug.debug(univention.debug.LISTENER, univention.debug.INFO, "not removing share directory of share %s: is not on a local filesystem" % old["cn"][0])
-			return
-
-
-		ret=0
-		ret_str=""
-		try:
-			try:
-				listener.setuid(0)
-				# for some reason, shutil cannot be imported, so we'll do it using mv
-				ret, ret_str = commands.getstatusoutput("mv '%s' '%s'" % (share_dir, target_dir))
-			except:
-				univention.debug.debug(univention.debug.LISTENER, univention.debug.ERROR, "failed to move share directory of share %s from %s to %s: %s" % (old["cn"][0], share_dir, target_dir, sys.exc_info()[0]))
-		finally:
-			listener.unsetuid()
-			if not ret==0:
-				univention.debug.debug(univention.debug.LISTENER, univention.debug.ERROR, "failed to move share directory of share %s from %s to %s: %s" % (old["cn"][0], share_dir, target_dir, ret_str))
-
+		# move it
+		ret = move_dir(share_dir, target_dir, listener)
+		if ret:
+			univention.debug.debug(
+				univention.debug.LISTENER, univention.debug.ERROR,
+				"failed to move directory of share %s from %s to %s: %s" % (name, share_dir, target_dir, ret))
