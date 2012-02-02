22760f750f256968f697dbc7d7157c80cff5ea22
Felix Botner <botner@univention.de>
2012-02-02 13:54:28 +0000

    * timestamp name * check fs on source and destination * shutil.move() * check fs with whitelist (Bug #20820)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs-school@30646 1b283449-9f2f-0410-b571-8eb228e1a901

--- ucs-school-old-homedirs/remove-old-homedirs.py
+++ ucs-school-old-homedirs/remove-old-homedirs.py
@@ -1,4 +1,6 @@
+#
 # Univention UCS@School
+#  listener module
 #
 # Copyright 2007-2012 Univention GmbH
 #
@@ -30,7 +32,11 @@
 __package__=''  # workaround for PEP 366
 import listener
 import re
-import univention.config_registry, commands, sys, os
+import commands
+import sys
+import os
+import shutil
+import time
 import univention.debug
 
 name='remove-old-homedirs'
@@ -39,76 +45,127 @@ filter='(objectClass=posixAccount)'
 attributes=[]
 modrdn='1'
 
-target_dir_config = "ucsschool/listener/oldhomedir/targetdir"
+DEFAUL_FS="ext2/ext3:ext2:ext3:ext4:xfs:btrfs"
+TARGET_BLACKLIST="/:/boot:/sys:/proc:/etc:/dev"
 
-def check_target_dir(configRegistry):
-	# either returns "" if everything is ok, or returns an error message
+target_dir = listener.configRegistry.get("ucsschool/listener/oldhomedir/targetdir")
+fs_types = listener.configRegistry.get("ucsschool/listener/oldhomedir/fs_types", DEFAUL_FS).split(":")
 
-	target_dir=configRegistry[target_dir_config]
+# either returns "" if everything is ok, or returns an error message
+def check_target_dir(dir):
 
-	if not configRegistry.has_key(target_dir_config):
-		return "%s is not set"%target_dir_config
+	if not dir:
+		return "targetdir is not set"
 
-	if os.path.exists(target_dir) and not os.path.isdir(target_dir):
-		return "%s is not a directory"%target_dir
+	# check target blacklist
+	tmp = dir.rstrip("/")
+	for i in TARGET_BLACKLIST.split(":"):
+		if not tmp or tmp == i:
+			return "%s as target dir is invalid" % dir
 
-	if not os.path.isdir(target_dir):
-		# create directory
+	if os.path.exists(dir) and not os.path.isdir(dir):
+		return "%s is not a directory" % dir
+
+	# create directory
+	if not os.path.isdir(dir):
 		listener.setuid(0)
-		ret,ret_str = commands.getstatusoutput("mkdir -p '%s'"%target_dir)
-		if not ret==0:
-			return "failed to create target directory %s"%target_dir
+		try:
+			os.makedirs(dir)
+		except:
+			return "failed to create target directory %s" % dir
+		finally:
+			listener.unsetuid()
+
+	# check fs
+	ret = check_filesystem(dir)
+	if ret:
+		return ret
 
 	return ""
 
+# either returns "" if everything is ok, or returns an error message
+def check_source_dir(dir):
+
+	if not os.path.isdir(dir):
+		return "%s is not a directory" % dir
+
+	# check fs
+	ret = check_filesystem(dir)
+	if ret:
+		return ret
+	
+	return ""
+
+# make sure that we are dealing with a known filesystem
+def check_filesystem(dir):
+
+	ret, out = commands.getstatusoutput("LC_ALL=C stat -f '%s'" % dir)
+	myFs = ""
+	for line in out.split("\n"):
+		tmp = line.split("Type: ")
+		if len(tmp) == 2:
+			myFs = tmp[1].strip()
+			for fs in fs_types:
+				if fs.lower() == myFs.lower():
+					# ok, 
+					return ""
+				break
+	return "%s for %s is not on a known filesystem" % (myFs, dir)
+
+# move directory
+def move_dir(src, dst, listener):
+
+	newName = os.path.basename(src) + ".%s" % int(time.time())
+	dst = os.path.join(dst, newName)
+	ret = ""
+	
+	listener.setuid(0)
+	try:
+		shutil.move(src, dst)
+	except Exception, e:
+		ret = str(e)
+	finally:
+		listener.unsetuid()
+
+	return ret
+
+
 def handler(dn, new, old, command):
 
-	configRegistry = univention.config_registry.ConfigRegistry()
-	configRegistry.load()
-
 	# remove empty home directories
-	if old and not new and command == "d":
+	# if object is really removed (not renamed)
+	if old and not new and not command == "r":
+
+		uid = old["uid"][0]
+
+		# check object
+		if not old.get("homeDirectory"):
+			univention.debug.debug(
+				univention.debug.LISTENER, univention.debug.WARN,
+				"not removing home of user %s: homeDirectory not set" % uid)
+			return
+
+		home_dir = old["homeDirectory"][0]
 
 		# check if target directory is okay
-
-		ret=check_target_dir(configRegistry)
-		if not ret=="":
-			univention.debug.debug(univention.debug.LISTENER, univention.debug.WARN, "not removing home directory of user %s: %s"%(old["uid"][0], ret))
+		ret = check_target_dir(target_dir)
+		if ret:
+			univention.debug.debug(
+				univention.debug.LISTENER, univention.debug.WARN,
+				"not removing home of user %s: %s" % (uid, ret))
 			return
-
-		target_dir=configRegistry[target_dir_config]
 
 		# check source (home) directory
-
-		home_dir=""
-		if not old.has_key("homeDirectory"):
-			univention.debug.debug(univention.debug.LISTENER, univention.debug.WARN, "not removing home directory of user %s, homeDirectory is not set"%old["uid"][0])
-			return
-		else:
-			home_dir=old["homeDirectory"][0]
-
-		if not os.path.isdir(home_dir):
-			univention.debug.debug(univention.debug.LISTENER, univention.debug.WARN, "not removing home directory of user %s, does not exist or is no directory"%old["uid"][0])
+		ret = check_source_dir(home_dir)
+		if ret:
+			univention.debug.debug(
+				univention.debug.LISTENER, univention.debug.WARN,
+				"not removing home of user %s: %s" % (uid, ret))
 			return
 
-		# make sure that we are dealing with a local filesystem
-		ret, ret_str = commands.getstatusoutput("stat -f '%s' | grep 'Type:' | sed 's/.*Type:\ //'" % home_dir)
-
-		if ret_str.strip() in ["nfs", "nfs4", "cifs", "smbfs", "nfsd"]:
-			univention.debug.debug(univention.debug.LISTENER, univention.debug.INFO, "not removing home directory of user %s, is not on a local filesystem"%old["uid"][0])
-			return
-
-
-		ret=0
-		ret_str=""
-		try:
-			try:
-				listener.setuid(0)
-				# for some reason, shutil cannot be imported, so we'll do it using mv
-				ret, ret_str = commands.getstatusoutput("mv '%s' '%s'" % (home_dir, target_dir))
-			except:
-				univention.debug.debug(univention.debug.LISTENER, univention.debug.ERROR, "failed to move home directory of user %s from %s to %s: %s"%(old["uid"][0], home_dir, target_dir, sys.exc_info()[0]))
-		finally:
-			listener.unsetuid()
-			if not ret==0:
-				univention.debug.debug(univention.debug.LISTENER, univention.debug.ERROR, "failed to move home directory of user %s from %s to %s: %s"%(old["uid"][0], home_dir, target_dir, ret_str))
+		# move it
+		ret = move_dir(home_dir, target_dir, listener)
+		if ret:
+			univention.debug.debug(
+				univention.debug.LISTENER, univention.debug.WARN,
+				"failed to move home of user %s from %s to %s: %s" % (uid, home_dir, target_dir, ret))
