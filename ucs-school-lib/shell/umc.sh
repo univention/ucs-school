9599c4750cdd388b5363cadabaa510f3188e4f30
Alexander Kläser <klaeser@univention.de>
2012-02-08 11:58:25 +0000

    ucs-school-lib (1.0.16-1) unstable; urgency=low  .    * removed last shift as it would cause an error in dash; Bug #25701
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs-school@30779 1b283449-9f2f-0410-b571-8eb228e1a901

--- ucs-school-lib/shell/umc.sh
+++ ucs-school-lib/shell/umc.sh
@@ -40,7 +40,7 @@ BIND_ARGS="$@"
 ucs_school_policies_create () {
 	# create default policies
 
-	additional_policy="$1"; shift
+	additional_policy="$1"
 
 	for policyname in ucsschool-umc-teachers-default ucsschool-umc-admins-default default-umc-all $additional_policy ; do
 		udm policies/umc create $BIND_ARGS --ignore_exists \
@@ -53,7 +53,7 @@ ucs_school_policies_append () {
 	# append operation set to all policies
 
 	operation_set="$1"; shift
-	additional_policy="$1"; shift
+	additional_policy="$1"
 	for policyname in ucsschool-umc-teachers-default ucsschool-umc-admins-default default-umc-all $additional_policy ; do
 		udm policies/umc modify $BIND_ARGS --ignore_exists \
 			--dn "cn=${policyname},cn=UMC,cn=policies,$ldap_base" \
