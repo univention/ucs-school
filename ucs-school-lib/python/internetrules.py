4529beb6bc3e1673561a3429f64112cb5fa46546
Alexander Kläser <klaeser@univention.de>
2012-04-19 14:03:23 +0000

    ucs-school-lib (1.0.33-1) unstable; urgency=low  .    * adapted filter rules module, added methods remove() and findUCRVariables();      Bug #26351
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.0/ucs-school@32273 1b283449-9f2f-0410-b571-8eb228e1a901

--- ucs-school-lib/python/internetrules.py
+++ ucs-school-lib/python/internetrules.py
@@ -40,19 +40,20 @@ import re
 # regular expression to match UCR variables for filter properties
 _regFilterNames = re.compile(r'^proxy/filter/setting(?P<userPrefix>-user)?/(?P<name>[^/]*)/(?P<property>[^/]*)(/(?P<listType>[^/]*)/(?P<index>[^/]*))?$')
 
+WHITELIST, BLACKLIST, GREYLIST = range(3)
+_filterTypes = {
+	'blacklist-pass': BLACKLIST,
+	'whitelist-block': WHITELIST,
+	'whitelist-blacklist-pass': GREYLIST,
+}
+_filterTypesInv = dict([ (_i[1], _i[0]) for _i in _filterTypes.iteritems() ])
+_listTypes = {
+	'blacklisted': BLACKLIST,
+	'whitelisted': WHITELIST,
+}
+_listTypesInv = dict([ (_i[1], _i[0]) for _i in _listTypes.iteritems() ])
+
 class Rule(object):
-	WHITELIST, BLACKLIST, GREYLIST = range(3)
-	_filterTypes = {
-		'blacklist-pass': BLACKLIST,
-		'whitelist-block': WHITELIST,
-		'whitelist-blacklist-pass': GREYLIST,
-	}
-	_filterTypesInv = dict([ (_i[1], _i[0]) for _i in _filterTypes.iteritems() ])
-	_listTypes = {
-		'blacklisted': BLACKLIST,
-		'whitelisted': WHITELIST,
-	}
-	_listTypesInv = dict([ (_i[1], _i[0]) for _i in _listTypes.iteritems() ])
 
 	def __init__(self, name, type=WHITELIST, priority=5, wlan=False, domains=[], userRule=False):
 		self.name = name
@@ -89,14 +90,14 @@ class Rule(object):
 		'''domains can be a list of strings or a list of index-string-type tuples.'''
 		self._domains = []
 		for i in domains:
-			if isinstance(i, (tuple, list)):
-				self._domains.append(i)
-			else:
+			if isinstance(i, str):
 				self._domains.append((-1, i, self.type))
+			else:
+				self._domains.append(i)
 
 	def addDomain(self, domain, idx=-1, listType=None):
 		'''add a new domain with an optional fixed index and list type'''
-		if listType not in Rule._listTypesInv:
+		if listType not in _listTypesInv:
 			listType = self.type
 		self._domains.append((idx, domain, listType))
 
@@ -105,7 +106,7 @@ class Rule(object):
 		only the changed properties will be saved. In case the rules are similar,
 		no changes will be done.'''
 		# load original rule
-		orgRule = Rule.load(self.name)
+		orgRule = load(self.name)
 		if orgRule:
 			# for comparing domains, make sure that we look at
 			orgRule.type = self.type
@@ -118,7 +119,7 @@ class Rule(object):
 			# this is a user rule which has a different prefix
 			prefix = 'proxy/filter/setting-user/%s' % self.name
 		if not orgRule or orgRule.type != self.type:
-			vars.append('%s/filtertype=%s' % (prefix, Rule._listTypesInv[self.type]))
+			vars.append('%s/filtertype=%s' % (prefix, _listTypesInv[self.type]))
 		if not orgRule or orgRule.priority != self.priority:
 			vars.append('%s/priority=%s' % (prefix, self.priority))
 		if not orgRule or orgRule.wlan != self.wlan:
@@ -128,7 +129,7 @@ class Rule(object):
 			vars.append('%s/wlan=%s' % (prefix, wlan))
 
 		# iterate over all blacklist and whitelist entries
-		for itype in Rule._listTypes.values():
+		for itype in _listTypes.values():
 			# saving domains is a bit more tricky as we need to take care of the indeces
 			# ... get the original list of domains with indeces and sorted
 			orgDomains = []
@@ -140,7 +141,7 @@ class Rule(object):
 			domains = [ (i + 1, domains[i], itype) for i in range(len(domains)) ]
 
 			# find the entries that need to be changed/added
-			domainPrefix = '%s/domain/%s' % (prefix, Rule._listTypesInv[itype])
+			domainPrefix = '%s/domain/%s' % (prefix, _listTypesInv[itype])
 			iorg = 0
 			inew = 0
 			while inew < len(domains):
@@ -163,82 +164,110 @@ class Rule(object):
 		if rmVars:
 			univention.config_registry.handler_unset(rmVars)
 
-	@staticmethod
-	def load(name, userRule=False):
-		'''Wrapper for list(name).'''
-		return Rule.list(name, userRule)
+def findUCRVariables(filterName=None, userRule=False):
+	'''Returns a dict of all UCR variables or all variables matching the
+	specified rule name.'''
+	# refresh internal UCR cache
+	ucr.load()
 
-	@staticmethod
-	def list(filterName=None, userRule=False):
-		'''Returns a list of all existing rules. If name is given, returns only the
-		rule matching the specified name or None. userRule specifies whether all
-		common rules (=False) or only user-specific rules (=True) are listed. 
-		If filterName is specified, only rule matching this name is returned as
-		single object (not as list!).'''
+	# iterate over all UCR variables
+	vars = {}
+	for k, v in ucr.items():
+		imatch = _regFilterNames.match(k)
+		if imatch:
+			# we found a filter variable
+			iname = imatch.group('name')
+			if not iname or (filterName and filterName != iname):
+				# empty name or name does not match the specified filter
+				continue
 
-		# refresh internal UCR cache
-		ucr.load()
+			# see whether this is a user specific rule or a general rule
+			if userRule != bool(imatch.group('userPrefix')):
+				continue
 
-		# iterate over all UCR variables
-		rules = {}
-		for k, v in ucr.items():
-			imatch = _regFilterNames.match(k)
-			if imatch:
-				# we found a filter variable
-				iname = imatch.group('name')
-				if not iname or (filterName and filterName != iname):
-					# empty name or name does not match the specified filter
-					continue
+			# bingo, we got a match :)
+			vars[k] = v
 
-				# get the rule from our cache
-				irule = rules.get(iname, Rule(iname))
+	# return all matched variables
+	return vars
 
-				# see whether this is a user specific rule or a general rule
-				irule.userRule = bool(imatch.group('userPrefix'))
-				if userRule != irule.userRule:
-					continue
+def remove(name, userRule=False):
+	'''Removes the UCR variables corresponding to the specified rule.'''
+	if not name:
+		return False
+	rmVars = findUCRVariables(name, userRule).keys()
+	if rmVars:
+		univention.config_registry.handler_unset(rmVars)
+		return True
+	return False
 
-				# update the rule with the given property
-				# NOTE: URL black-/whitelists are not supported anymore, only domain lists
-				iproperty = imatch.group('property')
-				if iproperty == 'filtertype':
-					if v not in Rule._filterTypes:
-						irule.type = Rule.WHITELIST
-						MODULE.error('Unknown filtertype "%s" for rule "%s", using whitelist as default.' % (v, irule))
-					else:
-						irule.type = Rule._filterTypes[v]
-				elif iproperty == 'priority':
-					try:
-						irule.priority = int(v)
-					except ValueError as e:
-						irule.priority = 5
-						MODULE.error('Could not parse priority "%s" for rule "%s", using default value "5".' % (v, irule))
-				elif iproperty == 'wlan':
-					irule.wlan = ucr.is_true(k)
-				elif iproperty == 'domain':
-					# get the index
-					idx = -1
-					try:
-						idx = int(imatch.group('index'))
-					except ValueError as e:
-						pass
+def load(name, userRule=False):
+	'''Wrapper for list(name).'''
+	return list(name, userRule)
 
-					# get list type (blacklisted or whitelisted)
-					listType = Rule._listTypes.get(imatch.group('listType'))
+def list(filterName=None, userRule=False):
+	'''Returns a list of all existing rules. If name is given, returns only the
+	rule matching the specified name or None. userRule specifies whether all
+	common rules (=False) or only user-specific rules (=True) are listed.
+	If filterName is specified, only rule matching this name is returned as
+	single object (not as list!).'''
 
-					# add domain to list of domains
-					irule.addDomain(v, idx, listType)
+	# iterate over all UCR variables
+	rules = {}
+	for k, v in findUCRVariables(filterName, userRule).iteritems():
+		imatch = _regFilterNames.match(k)
+		if not imatch:
+			# should not happen
+			continue
 
-				# save the rule back to our cache
-				rules[iname] = irule
+		# get filter name
+		iname = imatch.group('name')
 
-		if filterName:
-			# handle case for filtered search
-			if not len(rules):
-				# no match
-				return None
-			# return single element
-			return rules.items()[0][1]
+		# get the rule from our cache
+		irule = rules.get(iname, Rule(iname))
 
-		return rules.values()
+		# update the rule with the given property
+		# NOTE: URL black-/whitelists are not supported anymore, only domain lists
+		iproperty = imatch.group('property')
+		if iproperty == 'filtertype':
+			if v not in _filterTypes:
+				irule.type = WHITELIST
+				MODULE.error('Unknown filtertype "%s" for rule "%s", using whitelist as default.' % (v, irule))
+			else:
+				irule.type = _filterTypes[v]
+		elif iproperty == 'priority':
+			try:
+				irule.priority = int(v)
+			except ValueError as e:
+				irule.priority = 5
+				MODULE.error('Could not parse priority "%s" for rule "%s", using default value "5".' % (v, irule))
+		elif iproperty == 'wlan':
+			irule.wlan = ucr.is_true(k)
+		elif iproperty == 'domain':
+			# get the index
+			idx = -1
+			try:
+				idx = int(imatch.group('index'))
+			except ValueError as e:
+				pass
+
+			# get list type (blacklisted or whitelisted)
+			listType = _listTypes.get(imatch.group('listType'))
+
+			# add domain to list of domains
+			irule.addDomain(v, idx, listType)
+
+		# save the rule back to our cache
+		rules[iname] = irule
+
+	if filterName:
+		# handle case for filtered search
+		if not len(rules):
+			# no match
+			return None
+		# return single element
+		return rules.items()[0][1]
+
+	return rules.values()
+
 
