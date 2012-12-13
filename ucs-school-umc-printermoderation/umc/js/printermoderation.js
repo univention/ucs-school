dff46d74bf3acd2603b0ab827f5bb79abf533190
Florian Best <best@univention.de>
2012-12-13 09:57:34 +0000

    * migrate module to Dojo 2.0 Syntax; Bug #29790
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.1/ucs-school@37771 1b283449-9f2f-0410-b571-8eb228e1a901

--- ucs-school-umc-printermoderation/umc/js/printermoderation.js
+++ ucs-school-umc-printermoderation/umc/js/printermoderation.js
@@ -26,24 +26,31 @@
  * /usr/share/common-licenses/AGPL-3; if not, see
  * <http://www.gnu.org/licenses/>.
  */
-/*global console dojo dojox dijit window umc */
+/*global define window*/
 
-dojo.provide("umc.modules.printermoderation");
+define([
+	"dojo/_base/declare",
+	"dojo/_base/lang",
+	"dojo/_base/array",
+	"dojo/Deferred",
+	"dijit/Dialog",
+	"dojo/date/locale",
+	"umc/dialog",
+	"umc/tools",
+	"umc/widgets/ExpandingTitlePane",
+	"umc/widgets/Grid",
+	"umc/widgets/Module",
+	"umc/widgets/Page",
+	"umc/widgets/Form",
+	"umc/widgets/SearchForm",
+	"umc/widgets/TextBox",
+	"umc/widgets/Text",
+	"umc/widgets/ComboBox",
+	"umc/widgets/ProgressInfo",
+	"umc/i18n!/umc/modules/printermoderation"
+], function(declare, lang, array, Deferred, Dialog, locale, dialog, tools, ExpandingTitlePane, Grid, Module, Page, Form, SearchForm, TextBox, Text, ComboBox, ProgressInfo, _) {
 
-dojo.require( "dojo.date" );
-dojo.require( "dojo.date.locale" );
-
-dojo.require("umc.dialog");
-dojo.require("umc.i18n");
-dojo.require("umc.tools");
-dojo.require("umc.widgets.ExpandingTitlePane");
-dojo.require("umc.widgets.Grid");
-dojo.require("umc.widgets.Module");
-dojo.require("umc.widgets.Page");
-dojo.require("umc.widgets.SearchForm");
-dojo.require("umc.widgets.ProgressInfo");
-
-dojo.declare("umc.modules.printermoderation", [ umc.widgets.Module, umc.i18n.Mixin ], {
+return declare("umc.modules.printermoderation", [ Module ], {
 	// summary:
 	//		Print job moderation
 	// description:
@@ -71,7 +78,7 @@ dojo.declare("umc.modules.printermoderation", [ umc.widgets.Module, umc.i18n.Mix
 		this.inherited(arguments);
 
 		// setup a progress bar with some info text
-		this._progressInfo = new umc.widgets.ProgressInfo( {
+		this._progressInfo = new ProgressInfo( {
 			style: 'min-width: 400px;'
 		} );
 
@@ -85,7 +92,7 @@ dojo.declare("umc.modules.printermoderation", [ umc.widgets.Module, umc.i18n.Mix
 
 	renderSearchPage: function(containers, superordinates) {
 		// render all GUI elements for the search formular and the grid
-		this._searchPage = new umc.widgets.Page({
+		this._searchPage = new Page({
 			headerText: this.description,
 			helpText: ''
 		});
@@ -93,8 +100,8 @@ dojo.declare("umc.modules.printermoderation", [ umc.widgets.Module, umc.i18n.Mix
 		this.addChild(this._searchPage);
 
 		// umc.widgets.ExpandingTitlePane is an extension of dijit.layout.BorderContainer
-		var titlePane = new umc.widgets.ExpandingTitlePane({
-			title: this._('Search results')
+		var titlePane = new ExpandingTitlePane({
+			title: _('Search results')
 		});
 		this._searchPage.addChild(titlePane);
 
@@ -106,58 +113,58 @@ dojo.declare("umc.modules.printermoderation", [ umc.widgets.Module, umc.i18n.Mix
 		// define grid actions
 		var actions = [{
 			name: 'view',
-			label: this._( 'View' ),
-			description: this._( 'View the print job.' ),
+			label: _( 'View' ),
+			description: _( 'View the print job.' ),
 			isStandardAction: true,
 			isMultiAction: true,
-			callback: dojo.hitch( this, function( ids, items ) {
-				dojo.forEach( items, dojo.hitch( this, function ( item ) {
+			callback: lang.hitch( this, function( ids, items ) {
+				array.forEach( items, lang.hitch( this, function ( item ) {
 					// document.location.host + '//' + document.location.host +
-					var url = dojo.replace( '/umcp/command/printermoderation/download?username={0}&printjob={1}', [ item.username, item.filename ] );
+					var url = lang.replace( '/umcp/command/printermoderation/download?username={0}&printjob={1}', [ item.username, item.filename ] );
 					window.open( url );
 				} ) );
 			} )
 		}, {
 			name: 'print',
-			label: this._( 'Print' ),
-			description: this._( 'Print the document.' ),
+			label: _( 'Print' ),
+			description: _( 'Print the document.' ),
 			isStandardAction: true,
 			isMultiAction: true,
-			callback: dojo.hitch(this, '_printJobs')
+			callback: lang.hitch(this, '_printJobs')
 		}, {
 			name: 'delete',
-			label: this._( 'Delete' ),
-			description: this._( 'Delete the print job.' ),
+			label: _( 'Delete' ),
+			description: _( 'Delete the print job.' ),
 			isStandardAction: true,
 			isMultiAction: true,
 			iconClass: 'umcIconDelete',
-			callback: dojo.hitch(this, '_deletePrintJobs')
+			callback: lang.hitch(this, '_deletePrintJobs')
 		}];
 
 		// define the grid columns
 		var columns = [{
 			name: 'user',
-			label: this._( 'User' ),
+			label: _( 'User' ),
 			width: '30%'
 		}, {
 			name: 'printjob',
-			label: this._( 'Print job' ),
+			label: _( 'Print job' ),
 			width: '35%'
 		}, {
 			name: 'pages',
-			label: this._( 'Pages' ),
+			label: _( 'Pages' ),
 			width: '8%'
 		}, {
 			name: 'date',
-			label: this._( 'Date' ),
+			label: _( 'Date' ),
 			width: '20%',
-			formatter: dojo.hitch( this, function( key, rowIndex ) {
-				return dojo.date.locale.format( new Date( key[ 0 ], key[ 1 ] - 1, key[ 2 ], key[ 3 ], key[ 4 ] ), { formatLength: 'short' } );
+			formatter: lang.hitch( this, function( key, rowIndex ) {
+				return locale.format( new Date( key[ 0 ], key[ 1 ] - 1, key[ 2 ], key[ 3 ], key[ 4 ] ), { formatLength: 'short' } );
 			} )
 		}];
 
 		// generate the data grid
-		this._grid = new umc.widgets.Grid({
+		this._grid = new Grid({
 			actions: actions,
 			defaultAction: 'view',
 			columns: columns,
@@ -176,30 +183,30 @@ dojo.declare("umc.modules.printermoderation", [ umc.widgets.Module, umc.i18n.Mix
 
 		// add remaining elements of the search form
 		var widgets = [{
-			type: 'ComboBox',
+			type: ComboBox,
 			name: 'school',
-			description: this._('Select the school.'),
-			label: this._( 'School' ),
+			description: _('Select the school.'),
+			label: _( 'School' ),
 			autoHide: true,
 			size: 'TwoThirds',
 			dynamicValues: 'printermoderation/schools'
 		}, {
-			type: 'ComboBox',
+			type: ComboBox,
 			name: 'class',
-			description: this._('Select a class or workgroup.'),
-			label: this._('Class or workroup'),
+			description: _('Select a class or workgroup.'),
+			label: _('Class or workroup'),
 			size: 'TwoThirds',
 			staticValues: [
-				{ 'id' : 'None', 'label' : this._( 'All classes and workgroups' ) }
+				{ 'id' : 'None', 'label' : _( 'All classes and workgroups' ) }
 			],
 			dynamicValues: 'printermoderation/groups',
 			depends: 'school'
 		}, {
-			type: 'TextBox',
+			type: TextBox,
 			name: 'pattern',
 			value: '',
-			description: this._('Specifies the substring pattern which is searched for in the first name, surname and username'),
-			label: this._('Name')
+			description: _('Specifies the substring pattern which is searched for in the first name, surname and username'),
+			label: _('Name')
 		}];
 
 		var layout = [
@@ -207,22 +214,21 @@ dojo.declare("umc.modules.printermoderation", [ umc.widgets.Module, umc.i18n.Mix
 		];
 
 		// generate the search form
-		this._searchForm = new umc.widgets.SearchForm({
+		this._searchForm = new SearchForm({
 			// property that defines the widget's position in a dijit.layout.BorderContainer
 			region: 'top',
 			widgets: widgets,
 			layout: layout,
-			onSearch: dojo.hitch(this, function(values) {
+			onSearch: lang.hitch(this, function(values) {
 				// call the grid's filter function
-				// (could be also done via dojo.connect() and dojo.disconnect() )
 				this._grid.filter(values);
 			})
 		});
 
 		// turn off the standby animation as soon as all form values have been loaded
-		this.connect(this._searchForm, 'onValuesInitialized', function() {
+		this._searchForm.on('ValuesInitialized', lang.hitch(this, function() {
 			this.standby(false);
-		});
+		}));
 
 		// add search form to the title pane
 		titlePane.addChild(this._searchForm);
@@ -231,24 +237,24 @@ dojo.declare("umc.modules.printermoderation", [ umc.widgets.Module, umc.i18n.Mix
 	},
 
 	_deletePrintJobs: function(ids, items) {
-		umc.dialog.confirm( this._( 'Should the selected print jobs be deleted?' ), [ {
-			label: this._( 'Delete' ),
-			callback: dojo.hitch( this, function() {
-				var finished_func = dojo.hitch( this, function() {
-					this._progressInfo.update( items.length, this._( 'Finished' ) );
+		dialog.confirm( _( 'Should the selected print jobs be deleted?' ), [ {
+			label: _( 'Delete' ),
+			callback: lang.hitch( this, function() {
+				var finished_func = lang.hitch( this, function() {
+					this._progressInfo.update( items.length, _( 'Finished' ) );
 					this.moduleStore.onChange();
 					this.standby( false );
 				} );
-				var deferred = new dojo.Deferred();
+				var deferred = new Deferred();
 
 				this._progressInfo.set( 'maximum', items.length );
-				this._progressInfo.update( 0, '', this._( 'Deleting print jobs ...' ) );
+				this._progressInfo.update( 0, '', _( 'Deleting print jobs ...' ) );
 				this.standby( true, this._progressInfo );
 				deferred.resolve();
 
-				dojo.forEach( items, dojo.hitch( this, function( item, i ) {
-					deferred = deferred.then( dojo.hitch( this, function() {
-						this._progressInfo.update( i, dojo.replace( this._( 'Print job {0} from {1}' ), [ item.printjob, item.user ] ) );
+				array.forEach( items, lang.hitch( this, function( item, i ) {
+					deferred = deferred.then( lang.hitch( this, function() {
+						this._progressInfo.update( i, lang.replace( _( 'Print job {0} from {1}' ), [ item.printjob, item.user ] ) );
 						return this.umcpCommand( 'printermoderation/delete', {
 							username: item.username,
 							printjob: item.filename
@@ -258,36 +264,36 @@ dojo.declare("umc.modules.printermoderation", [ umc.widgets.Module, umc.i18n.Mix
 				deferred.then( finished_func, finished_func );
 			} )
 		}, {
-			label: this._( 'Cancel' ),
+			label: _( 'Cancel' ),
 			'default': true
 		} ] );
 	},
 
 	_printJobs: function(ids, items) {
-		var dialog = null, form = null;
+		var _dialog = null, form = null;
 
 		var _cleanup = function() {
-			dialog.hide();
-			dialog.destroyRecursive();
+			_dialog.hide();
+			_dialog.destroyRecursive();
 			form.destroyRecursive();
 		};
 
-		var _print = dojo.hitch( this, function( printer ) {
-			var deferred = new dojo.Deferred();
-			var finished_func = dojo.hitch(this, function() {
+		var _print = lang.hitch( this, function( printer ) {
+			var deferred = new Deferred();
+			var finished_func = lang.hitch(this, function() {
 				this.moduleStore.onChange();
-				this._progressInfo.update( ids.length, this._( 'Finished' ) );
+				this._progressInfo.update( ids.length, _( 'Finished' ) );
 				this.standby( false );
 			} );
 			this._progressInfo.set( 'maximum', ids.length );
-			this._progressInfo.updateTitle( this._( 'Printing ...' ) );
+			this._progressInfo.updateTitle( _( 'Printing ...' ) );
 			this.standby( true, this._progressInfo );
 			deferred.resolve();
 
-			dojo.forEach( items, function( item, i ) {
-				deferred = deferred.then( dojo.hitch( this, function() {
-					this._progressInfo.update( i, dojo.replace( this._( 'Print job <i>{printjob}</i> of <i>{user}</i>' ), item ) );
-					return umc.tools.umcpCommand( 'printermoderation/print', {
+			array.forEach( items, function( item, i ) {
+				deferred = deferred.then( lang.hitch( this, function() {
+					this._progressInfo.update( i, lang.replace( _( 'Print job <i>{printjob}</i> of <i>{user}</i>' ), item ) );
+					return tools.umcpCommand( 'printermoderation/print', {
 						username: item.username,
 						printjob: item.filename,
 						printer: printer
@@ -301,26 +307,26 @@ dojo.declare("umc.modules.printermoderation", [ umc.widgets.Module, umc.i18n.Mix
 
 		var message = '';
 		if ( ids.length == 1 ) {
-			message = dojo.replace( this._( 'A printer must be selected on which the document <i>{printjob}</i> should be printed.' ), items[ 0 ] );
+			message = lang.replace( _( 'A printer must be selected on which the document <i>{printjob}</i> should be printed.' ), items[ 0 ] );
 		} else {
-			message = dojo.replace( this._( 'A printer must be selected on which the {0} documents should be printed.' ), [ items.length ] );
+			message = lang.replace( _( 'A printer must be selected on which the {0} documents should be printed.' ), [ items.length ] );
 		}
 		message = '<p>' + message + '</p>';
-		form = new umc.widgets.Form( {
+		form = new Form( {
 			style: 'max-width: 500px;',
 			widgets: [ {
-				type: 'Text',
+				type: Text,
 				name: 'info',
 				content: message
 			},{
-				type: 'ComboBox',
+				type: ComboBox,
 				name: 'printer',
 				dynamicValues: 'printermoderation/printers',
-				label: this._( 'Printer' )
+				label: _( 'Printer' )
 			} ],
 			buttons: [ {
 				name: 'submit',
-				label: this._( 'Print' ),
+				label: _( 'Print' ),
 				style: 'float: right;',
 				callback: function() {
 					var printer = form.getWidget( 'printer' );
@@ -329,20 +335,19 @@ dojo.declare("umc.modules.printermoderation", [ umc.widgets.Module, umc.i18n.Mix
 				}
 			}, {
 				name: 'cancel',
-				label: this._( 'Cancel' ),
+				label: _( 'Cancel' ),
 				callback: _cleanup
 			}],
 			layout: [ 'info', 'printer' ]
 		});
 
-		dialog = new dijit.Dialog( {
-			title: this._( 'Print' ),
+		_dialog = new Dialog( {
+			title: _( 'Print' ),
 			content: form,
 			'class': 'umcPopup'
 		} );
-		dialog.show();
+		_dialog.show();
 	}
 });
 
-
-
+});
