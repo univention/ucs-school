85fc76e4b24989dfff6dfa72e22437b5af30e715
Jascha Geerds <geerds@univention.de>
2012-12-13 17:22:51 +0000

    ucs-school-umc-helpdesk (7.0.1-1) unstable; urgency=low
      * Migrate module to Dojo 2.0 syntax (Bug #29787)
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.1/ucs-school@37791 1b283449-9f2f-0410-b571-8eb228e1a901

--- ucs-school-umc-helpdesk/umc/js/helpdesk.js
+++ ucs-school-umc-helpdesk/umc/js/helpdesk.js
@@ -26,183 +26,191 @@
  * /usr/share/common-licenses/AGPL-3; if not, see
  * <http://www.gnu.org/licenses/>.
  */
-/*global console dojo dojox dijit umc */
+/*global define*/
 
-dojo.provide("umc.modules.helpdesk");
+define([
+	"dojo/_base/array",
+	"dojo/_base/declare",
+	"dojo/_base/lang",
+	"dojo/on",
+	"dojo/topic",
+	"umc/dialog",
+	"umc/i18n!umc/modules/helpdesk",
+	"umc/tools",
+	"umc/widgets/ComboBox",
+	"umc/widgets/ExpandingTitlePane",
+	"umc/widgets/Form",
+	"umc/widgets/Grid",
+	"umc/widgets/Module",
+	"umc/widgets/Page",
+	"umc/widgets/SearchForm",
+	"umc/widgets/TextArea",
+	"umc/widgets/TextBox"
+], function(array, declare, lang, on, topic, dialog, _, tools, ComboBox,
+            ExpandingTitlePane, Form, Grid, Module, Page, SearchForm, TextArea,
+            TextBox) {
+	return declare("umc.modules.helpdesk", [ Module ], {
+		// summary:
+		//		Template module to ease the UMC module development.
+		// description:
+		//		This module is a template module in order to aid the development of
+		//		new modules for Univention Management Console.
 
-dojo.require("umc.dialog");
-dojo.require("umc.i18n");
-dojo.require("umc.tools");
-dojo.require("umc.widgets.ExpandingTitlePane");
-dojo.require("umc.widgets.Grid");
-dojo.require("umc.widgets.Module");
-dojo.require("umc.widgets.Page");
-dojo.require("umc.widgets.SearchForm");
+		postMixInProperties: function() {
+			// is called after all inherited properties/methods have been mixed
+			// into the object (originates from dijit._Widget)
 
-dojo.declare("umc.modules.helpdesk", [ umc.widgets.Module, umc.i18n.Mixin ], {
-	// summary:
-	//		Template module to ease the UMC module development.
-	// description:
-	//		This module is a template module in order to aid the development of
-	//		new modules for Univention Management Console.
+			// it is important to call the parent's postMixInProperties() method
+			this.inherited(arguments);
 
-	postMixInProperties: function() {
-		// is called after all inherited properties/methods have been mixed
-		// into the object (originates from dijit._Widget)
+			// Set the opacity for the standby animation to 100% in order to mask
+			// GUI changes when the module is opened. Call this.standby(true|false)
+			// to enabled/disable the animation.
+			this.standbyOpacity = 1;
+		},
 
-		// it is important to call the parent's postMixInProperties() method
-		this.inherited(arguments);
+		buildRendering: function() {
+			// is called after all DOM nodes have been setup
+			// (originates from dijit._Widget)
 
-		// Set the opacity for the standby animation to 100% in order to mask
-		// GUI changes when the module is opened. Call this.standby(true|false)
-		// to enabled/disable the animation.
-		this.standbyOpacity = 1;
-	},
+			// it is important to call the parent's buildRendering() method
+			this.inherited(arguments);
 
-	buildRendering: function() {
-		// is called after all DOM nodes have been setup
-		// (originates from dijit._Widget)
+			// start the standby animation in order prevent any interaction before the
+			// form values are loaded
+			this.standby(true);
 
-		// it is important to call the parent's buildRendering() method
-		this.inherited(arguments);
+			// render the page containing search form and grid
+			this.umcpCommand('helpdesk/configuration').then(
+				lang.hitch(this, function(response) {
+					if (response.result.recipient) {
+						this.renderPage(response.result.username, response.result.school);
+						this.standby(false);
+					} else {
+						dialog.alert(_('The helpdesk module is not configured properly. The recipient email address is not set.'));
+						on.once(dialog._alertDialog, 'confirm', lang.hitch(this, function() {
+							topic.publish('/umc/tabs/close', this);
+						}));
+					}
+				})
+			);
+		},
 
-		// start the standby animation in order prevent any interaction before the
-		// form values are loaded
-		this.standby(true);
+		renderPage: function(username, school) {
+			// ExpandingTitlePane is an extension of dijit.layout.BorderContainer
+			var titlePane = new ExpandingTitlePane({
+				title: _('Message to the helpdesk team')
+			} );
 
-		// render the page containing search form and grid
-		this.umcpCommand( 'helpdesk/configuration' ).then( dojo.hitch( this, function( response ) {
-			if ( response.result.recipient ) {
-				this.renderPage( response.result.username, response.result.school );
-				this.standby( false );
-			} else {
-				umc.dialog.alert( this._( 'The helpdesk module is not configured properly. The recipient email address is not set.' ) )
-				var hdl = dojo.connect( umc.dialog._alertDialog, 'onConfirm', dojo.hitch( this, function() {
-					dojo.disconnect( hdl );
-					dojo.publish('/umc/tabs/close', [ this ] );
-				} ) );
-			}
-		} ) );
-	},
+			//
+			// form
+			//
 
-	renderPage: function( username, school ) {
+			// add remaining elements of the search form
+			var widgets = [{
+				type: TextBox,
+				name: 'username',
+				label: _('User name'),
+				value: username,
+				disabled: true
+			}, {
+				type: TextBox,
+				name: 'school',
+				label: _('School'),
+				value: school,
+				disabled: true
+			}, {
+				type: ComboBox,
+				name: 'category',
+				label: _('Category'),
+				dynamicValues: 'helpdesk/categories'
+			}, {
+				type: TextArea,
+				name: 'message',
+				style: 'height: 200px;',
+				label: _('Message')
+			} ];
 
-		// umc.widgets.ExpandingTitlePane is an extension of dijit.layout.BorderContainer
-		var titlePane = new umc.widgets.ExpandingTitlePane( {
-			title: this._( 'Message to the helpdesk team' )
-		} );
+			// the layout is an 2D array that defines the organization of the form elements...
+			// here we arrange the form elements in one row and add the 'submit' button
+			var layout = [
+				'username',
+				'school',
+				'category',
+				'message'
+			];
 
-		//
-		// form
-		//
+			// generate the form
+			this._form = new Form({
+				// property that defines the widget's position in a dijit.layout.BorderContainer
+				region: 'top',
+				widgets: widgets,
+				layout: layout
+			});
 
-		// add remaining elements of the search form
-		var widgets = [ {
-			type: 'TextBox',
-			name: 'username',
-			label: this._('User name'),
-			value: username,
-			disabled: true
-		}, {
-			type: 'TextBox',
-			name: 'school',
-			label: this._('School'),
-			value: school,
-			disabled: true
-		}, {
-			type: 'ComboBox',
-			name: 'category',
-			label: this._( 'Category' ),
-			dynamicValues: 'helpdesk/categories'
-		}, {
-			type: 'TextArea',
-			name: 'message',
-			style: 'height: 200px;',
-			label: this._( 'Message' )
-		} ];
+			// turn off the standby animation as soon as all form values have been loaded
+			this.own(on(this._form, 'valuesInitialized', function() {
+				this.standby(false);
+			}));
 
-		// the layout is an 2D array that defines the organization of the form elements...
-		// here we arrange the form elements in one row and add the 'submit' button
-		var layout = [
-			'username',
-			'school',
-			'category',
-			'message'
-		];
+			// add form to the title pane
+			titlePane.addChild(this._form);
 
-		// generate the form
-		this._form = new umc.widgets.Form({
-			// property that defines the widget's position in a dijit.layout.BorderContainer
-			region: 'top',
-			widgets: widgets,
-			layout: layout
-		});
+			// submit changes
+			var buttons = [{
+				name: 'submit',
+				label: _('Send'),
+				'default': true,
+				callback: lang.hitch(this, function() {
+					var values = this._form.gatherFormValues();
+					if (values.message) {
+						this.onSubmit(this._form.gatherFormValues());
+					} else {
+						dialog.alert(_('The required message is missing. Therefore, no report has been sent to the helpdesk team.'));
+					}
+				})
+			}, {
+				name: 'close',
+				label: _('Close'),
+				callback: lang.hitch(this, function() {
+					var values = this._form.gatherFormValues();
+					if (values.message) {
+						dialog.confirm(_('Should the UMC module be closed? All unsaved modification will be lost.'), [{
+							label: _('Close'),
+							callback: lang.hitch(this, function() {
+								topic.publish('/umc/tabs/close', [ this ] );
+							})
+						}, {
+							label: _('Cancel'),
+							'default': true
+						}]);
+					} else {
+						topic.publish('/umc/tabs/close', [ this ] );
+					}
+				})
+			}];
 
-		// turn off the standby animation as soon as all form values have been loaded
-		this.connect( this._form, 'onValuesInitialized', function() {
-			this.standby( false );
-		});
+			this._page = new Page({
+				headerText: this.description,
+				helpText: '',
+				footerButtons: buttons
+			});
 
-		// add form to the title pane
-		titlePane.addChild(this._form);
+			this.addChild(this._page);
+			this._page.addChild(titlePane);
+		},
 
-
-
-		// submit changes
-		var buttons = [ {
-            name: 'submit',
-            label: this._( 'Send' ),
-            'default': true,
-            callback: dojo.hitch( this, function() {
-				var values = this._form.gatherFormValues();
-				if ( values.message ) {
-					this.onSubmit( this._form.gatherFormValues() );
-				} else {
-					umc.dialog.alert( this._( 'The required message is missing. Therefore, no report has been sent to the helpdesk team.' ) );
-				}
-            } )
-        }, {
-            name: 'close',
-            label: this._('Close'),
-            callback: dojo.hitch(this, function() {
-				var values = this._form.gatherFormValues();
-				if ( values.message ) {
-					umc.dialog.confirm( this._( 'Should the UMC module be closed? All unsaved modification will be lost.' ), [ {
-						label: this._( 'Close' ),
-						callback: dojo.hitch( this, function() {
-							dojo.publish('/umc/tabs/close', [ this ] );
-						} )
-					}, {
-						label: this._( 'Cancel' ),
-						'default': true
-					} ] );
-				} else {
-					dojo.publish('/umc/tabs/close', [ this ] );
-				}
-            } )
-        } ];
-
-		this._page = new umc.widgets.Page({
-			headerText: this.description,
-			helpText: '',
-			footerButtons: buttons
-		});
-
-		this.addChild(this._page);
-		this._page.addChild( titlePane );
-	},
-
-	onSubmit: function( values ) {
-		this.umcpCommand( 'helpdesk/send', values ).then( dojo.hitch( this, function ( response ) {
-			if ( response.result ) {
-				umc.dialog.alert( this._( 'The report has been sent to the helpdesk team' ) );
-				this._form._widgets.message.set( 'value', '' );
-			} else {
-				umc.dialog.alert( this._( 'The message could not be send to the helpdesk team: ' ) + response.message );
-			}
-		} ) );
-	}
+		onSubmit: function(values) {
+			this.umcpCommand('helpdesk/send', values).then(
+				lang.hitch(this, function (response) {
+					if (response.result) {
+						dialog.alert(_('The report has been sent to the helpdesk team'));
+						this._form._widgets.message.set('value', '');
+					} else {
+						dialog.alert(_('The message could not be send to the helpdesk team: ') + response.message);
+					}
+				})
+			);
+		}
+	});
 });
-
-
-
