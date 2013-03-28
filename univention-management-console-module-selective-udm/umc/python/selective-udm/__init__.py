8c84c8040ca80b4a65f34f22e3de6ebfac9ea618
Arvid Requate <requate@univention.de>
2013-03-28 17:52:52 +0000

    * selectiveudm/clone_user_account: Add cloned user to groups (Bug #30832) * selectiveudm/clone_user_account: Allow target container to be specified * selectiveudm/clone_user_account: confirm allocated IDs
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.1/ucs-school-r2@39805 1b283449-9f2f-0410-b571-8eb228e1a901

--- univention-management-console-module-selective-udm/umc/python/selective-udm/__init__.py
+++ univention-management-console-module-selective-udm/umc/python/selective-udm/__init__.py
@@ -149,7 +149,7 @@ class Instance(umcm.Base):
 			return
 		server_dn = server_dn[0]
 			
-		# Search for the OU container
+		## Search for the OU container
 		server_dn_list = ldap.explode_dn(server_dn)
 		idx=None
 		for i in range(1,len(server_dn_list)):
@@ -161,15 +161,21 @@ class Instance(umcm.Base):
 			MODULE.warn(message)
 			self.finished(request.id, {}, message, success=False)
 			return
-		clone_user_container = 'cn=users,cn=temp,%s' % string.join(server_dn_list[idx:], ',')
 
-		clonename = request.options.get('clonename')
-		if not clonename:
-			message = 'Command failed\nNo clonename specified'
+		#### Validate the client specified parameters
+		### Target container
+		clone_user_container = request.options.get('container')
+		if not clone_user_container:
+			clone_user_container = 'cn=users,cn=temp,%s' % string.join(server_dn_list[idx:], ',')
+		try:
+			lo.searchDn(base=clone_user_container, scope='base')
+		except Exception, err:
+			message = 'Command failed\nTarget container does not exists: %s' % clone_user_container
 			MODULE.warn(message)
 			self.finished(request.id, {}, message, success=False)
 			return
-
+		
+		### Origin uid
 		username = request.options.get('username')
 		if not username:
 			message = 'Command failed\nNo username specified'
@@ -177,63 +183,93 @@ class Instance(umcm.Base):
 			self.finished(request.id, {}, message, success=False)
 			return
 
-		# Convert the username into the object DN.
+		## Convert the username into the object DN.
 		user_orig_dn = lo.searchDn('(&(uid=%s)(objectClass=posixAccount))' % username )
 		if len(user_orig_dn) != 1:
-			message = 'Command failed\nDid not find the User DN'
+			message = 'Command failed\nUser %s not found' % username
 			MODULE.warn(message)
 			self.finished(request.id, {}, message, success=False)
 			return
 
-		## Get user_orig attributes
+		### Target uid
+		user_clone_uid = request.options.get('clonename')
+		if not user_clone_uid:
+			message = 'Command failed\nNo clonename specified'
+			MODULE.warn(message)
+			self.finished(request.id, {}, message, success=False)
+			return
+
+		prohibited_objects=univention.admin.handlers.settings.prohibited_username.lookup(co, lo, '')
+		if prohibited_objects and len(prohibited_objects) > 0:
+			for i in range(0,len(prohibited_objects)):
+				if user_clone_uid in prohibited_objects[i]['usernames']:
+					message = 'Command failed\nRequested clonename is not allowed according to settings/prohibited_username object %s' % prohibited_objects[i]['name']
+					MODULE.warn(message)
+					self.finished(request.id, {}, message, success=False)
+					return
+
+		### Get user_orig attributes
 		module_users_user = univention.admin.modules.get('users/user')
 		univention.admin.modules.init(lo, con_position, module_users_user)
 		user_orig = univention.admin.objects.get(module_users_user, co, lo, position=con_position, dn=user_orig_dn[0])
 
-		## Determine new DN
+		### Determine new DN
 		user_clone_position = univention.admin.uldap.position(user_orig.lo.base)
 		user_clone_position.setDn(clone_user_container)
-		user_clone_dn = "uid=%s,%s" % (clonename, user_clone_position.getDn())
+		user_clone_dn = "uid=%s,%s" % (user_clone_uid, user_clone_position.getDn())
 
-		## Allocate new uidNumber
+		### Allocate new uid
 		alloc = []
-		uidNum = univention.admin.allocators.request(lo, con_position, 'uidNumber')
-		alloc.append(('uidNumber', uidNum))
+		try:
+			uid=univention.admin.allocators.request(lo, con_position, 'uid', value=user_clone_uid)
+			alloc.append(('uid', uid))
+		except univention.admin.uexceptions.noLock, e:
+			univention.admin.allocators.release(lo, con_position, 'uid', user_clone_uid)
+			raise univention.admin.uexceptions.uidAlreadyUsed, ': %s' % user_clone_uid
 
+		### Ok, we have a valid target uid, so start cloning the user
+		## deepcopy(user_orig) soes not help much, as we cannot use users.user.object.create()
+		## because it currently cannot be convinced to preserve the password. So we do it manually:
 		try: 
+			## Allocate new uidNumber
+			if 'posix' in user_orig.options:
+				uidNum = univention.admin.allocators.request(lo, con_position, 'uidNumber')
+				alloc.append(('uidNumber', uidNum))
+
 			## Allocate new sambaSID
-			## code copied from users.user.object.__generate_user_sid:
-			if user_orig.s4connector_present:
-				# In this case Samba 4 must create the SID, the s4 connector will sync the
-				# new sambaSID back from Samba 4.
-				userSid='S-1-4-%s' % uidNum
-			else:
-				try:
-					userSid=univention.admin.allocators.requestUserSid(lo, con_position, uidNum)
-				except:
-					pass
-			if not userSid or userSid == 'None':
-				num=uidNum
-				while not userSid or userSid == 'None':
-					num = str(int(num)+1)
+			if 'samba' in user_orig.options:
+				## code copied from users.user.object.__generate_user_sid:
+				if user_orig.s4connector_present:
+					# In this case Samba 4 must create the SID, the s4 connector will sync the
+					# new sambaSID back from Samba 4.
+					userSid='S-1-4-%s' % uidNum
+				else:
 					try:
-						userSid=univention.admin.allocators.requestUserSid(lo, con_position, num)
-					except univention.admin.uexceptions.noLock, e:
+						userSid=univention.admin.allocators.requestUserSid(lo, con_position, uidNum)
+					except:
+						pass
+				if not userSid or userSid == 'None':
+					num=uidNum
+					while not userSid or userSid == 'None':
 						num = str(int(num)+1)
-				alloc.append(('sid', userSid))
+						try:
+							userSid=univention.admin.allocators.requestUserSid(lo, con_position, num)
+						except univention.admin.uexceptions.noLock, e:
+							num = str(int(num)+1)
+					alloc.append(('sid', userSid))
 
 
-			## Create the addlist, fixing up attributes as we go
+			## Now create the addlist, fixing up attributes as we go
 			new_description = "Exam user for %s" % user_orig.oldattr['uid']
 			al=[]
 			for (key, value) in user_orig.oldattr.items():
 				if key == 'uid':
-					value = [clonename]
+					value = [user_clone_uid]
 				elif key == 'homeDirectory':
-					value = ["/home/%s" % clonename]
+					value = ["/home/%s" % user_clone_uid]
 				elif key == 'krb5PrincipalName':
 					user_orig_krb5PrincipalName = value[0]
-					value = ["%s%s" % (clonename, user_orig_krb5PrincipalName[user_orig_krb5PrincipalName.find("@"):])]
+					value = ["%s%s" % (user_clone_uid, user_orig_krb5PrincipalName[user_orig_krb5PrincipalName.find("@"):])]
 				elif key == 'uidNumber':
 					value = [uidNum]
 				elif key == 'sambaSID':
@@ -250,7 +286,7 @@ class Instance(umcm.Base):
 			lo.add(user_clone_dn, al)
 
 		except Exception, err:
-			for i, j in user_orig.alloc:
+			for i, j in alloc:
 				univention.admin.allocators.release(lo, con_position, i, j)
 
 			message = 'ERROR: Command failed\n%s' % traceback.format_exc()
@@ -258,5 +294,24 @@ class Instance(umcm.Base):
 			self.finished(request.id, {}, message, success=False)
 			return
 
-		self.finished(request.id, {}, message, success=True)
+		## Add user_clone to groups
+		if 'posix' in user_orig.options:
+			## Now simply open the original user object to load the groups
+			user_orig.open()
+			module_groups_group = univention.admin.modules.get('groups/group')
+			grpobj = module_groups_group.object(None, lo, con_position, user_orig['primaryGroup'])
+			grpobj.fast_member_add( [ user_clone_dn ], [ user_clone_uid ] )
+
+			for group in user_orig.info.get('groups', []):
+				grpobj = module_groups_group.object(None, lo, con_position, group)
+				grpobj.fast_member_add( [ user_clone_dn ], [ user_clone_uid ] )
+
+		## finally confirm allocated IDs
+		univention.admin.allocators.confirm(lo, con_position, 'uid', user_clone_uid)
+		if 'samba' in user_orig.options:
+			univention.admin.allocators.confirm(lo, con_position, 'sid', userSid)
+		if 'posix' in user_orig.options:
+			univention.admin.allocators.confirm(lo, con_position, 'uidNumber', uidNum)
+
+		self.finished(request.id, {}, success=True)
 		return
