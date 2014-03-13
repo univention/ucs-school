039e409cc60c41592f2676de13788f0ce356c5f0
Sönke Schwardt-Krummrich <schwardt@univention.de>
2014-03-13 17:00:52 +0000

    Bug #34280: base class for testing UCS@school environments
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.2/ucs-school-3.2r2@48537 1b283449-9f2f-0410-b571-8eb228e1a901

--- ucs-test-ucsschool/univention/testing/ucsschool.py
+++ ucs-test-ucsschool/univention/testing/ucsschool.py
@@ -31,52 +31,220 @@ API for testing UCS@school and cleaning up after performed tests
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <http://www.gnu.org/licenses/>.
 
+import tempfile
 import ldap
+import random
 import subprocess
 import univention.testing.utils as utils
 import univention.testing.ucr
 import univention.testing.udm as utu
+import univention.testing.strings as uts
 
-def remove_udm_object(module, dn, fail_if_missing=False):
-	"""
-		Tries to remove UDM object specified by given dn.
-		Return None on success or error message.
-	"""
-	try:
-		dn = utils.get_ldap_connection().searchDn(base=dn)[0]
-	except (ldap.NO_SUCH_OBJECT, IndexError):
-		if fail_if_missing:
-			raise
-		return 'missing object'
-
-	msg = None
-	cmd = [ utu.UCSTestUDM.PATH_UDM_CLI_CLIENT_WRAPPED, module, 'remove', '--dn', dn ]
-	retval = subprocess.call(cmd)
-	if retval:
-		msg = 'ERROR: failed to remove UCS@school %s object: %s' % (module, dn)
-		print msg
-	return msg
+class UCSTestSchool_Exception(Exception):
+	pass
+class UCSTestSchool_MissingOU(UCSTestSchool_Exception):
+	pass
+class UCSTestSchool_OU_Name_Too_Short(UCSTestSchool_Exception):
+	pass
 
 
-def cleanup_ou(ou_name):
-	""" Removes the given school ou and all it's corresponding objects like groups """
+class UCSTestSchool(object):
+	_lo = utils.get_ldap_connection()
+	_ucr = univention.testing.ucr.UCSTestConfigRegistry()
+	_ucr.load()
 
-	ucr = univention.testing.ucr.UCSTestConfigRegistry()
-	ucr.load()
+	LDAP_BASE = _ucr['ldap/base']
 
-	print '*** Purging OU %s and related objects' % ou_name
-	# remove OU specific groups
-	for grpdn in ('cn=OU%(ou)s-Member-Verwaltungsnetz,cn=ucsschool,cn=groups,%(basedn)s',
-				  'cn=OU%(ou)s-Member-Edukativnetz,cn=ucsschool,cn=groups,%(basedn)s',
-				  'cn=OU%(ou)s-Klassenarbeit,cn=ucsschool,cn=groups,%(basedn)s',
-				  'cn=OU%(ou)s-DC-Verwaltungsnetz,cn=ucsschool,cn=groups,%(basedn)s',
-				  'cn=OU%(ou)s-DC-Edukativnetz,cn=ucsschool,cn=groups,%(basedn)s',
-				  'cn=admins-%(ou)s,cn=ouadmins,cn=groups,%(basedn)s',
-				  ):
-		grpdn = grpdn % { 'ou': ou_name, 'basedn': ucr.get('ldap/base') }
-		remove_udm_object('groups/group', grpdn)
+	PATH_CMD_BASE = '/usr/share/ucs-school-import/scripts'
+	PATH_CMD_CREATE_OU = PATH_CMD_BASE + '/create_ou'
+	PATH_CMD_IMPORT_USER = PATH_CMD_BASE + '/import_user'
 
-	# remove OU recursively
-	oudn = 'ou=%(ou)s,%(basedn)s' % { 'ou': ou_name, 'basedn': ucr.get('ldap/base') }
-	remove_udm_object('container/ou', oudn)
-	print '*** Purging OU %s and related objects: done' % ou_name
+	CN_STUDENT = _ucr.get('ucsschool/ldap/default/container/pupils', 'schueler')
+	CN_TEACHERS = _ucr.get('ucsschool/ldap/default/container/teachers', 'lehrer')
+	CN_TEACHERS_STAFF = _ucr.get('ucsschool/ldap/default/container/teachers-and-staff', 'lehrer und mitarbeiter')
+	CN_ADMINS = _ucr.get('ucsschool/ldap/default/container/admins', 'admins')
+	CN_STAFF = _ucr.get('ucsschool/ldap/default/container/staff', 'mitarbeiter')
+
+
+	def __init__(self):
+		self._cleanup_ou_names = set()
+
+	def __enter__(self):
+		return self
+
+	def __exit__(self, exc_type, exc_value, traceback):
+		if exc_type:
+			print 'Cleanup after exception: %s %s' % (exc_type, exc_value)
+		self.cleanup()
+
+
+	def _remove_udm_object(self, module, dn, raise_exceptions=False):
+		"""
+			Tries to remove UDM object specified by given dn.
+			Return None on success or error message.
+		"""
+		try:
+			dn = self._lo.searchDn(base=dn)[0]
+		except (ldap.NO_SUCH_OBJECT, IndexError):
+			if raise_exceptions:
+				raise
+			return 'missing object'
+
+		msg = None
+		cmd = [utu.UCSTestUDM.PATH_UDM_CLI_CLIENT_WRAPPED, module, 'remove', '--dn', dn]
+		retval = subprocess.call(cmd)
+		if retval:
+			msg = 'ERROR: failed to remove UCS@school %s object: %s' % (module, dn)
+			print msg
+		return msg
+
+
+	def cleanup(self):
+		""" Cleanup all objects created by the UCS@school test environment """
+		for ou_name in self._cleanup_ou_names:
+
+			self.cleanup_ou(ou_name)
+
+	def cleanup_ou(self, ou_name):
+		""" Removes the given school ou and all its corresponding objects like groups """
+
+		print '*** Purging OU %s and related objects' % ou_name
+		# remove OU specific groups
+		for grpdn in ('cn=OU%(ou)s-Member-Verwaltungsnetz,cn=ucsschool,cn=groups,%(basedn)s',
+					  'cn=OU%(ou)s-Member-Edukativnetz,cn=ucsschool,cn=groups,%(basedn)s',
+					  'cn=OU%(ou)s-Klassenarbeit,cn=ucsschool,cn=groups,%(basedn)s',
+					  'cn=OU%(ou)s-DC-Verwaltungsnetz,cn=ucsschool,cn=groups,%(basedn)s',
+					  'cn=OU%(ou)s-DC-Edukativnetz,cn=ucsschool,cn=groups,%(basedn)s',
+					  'cn=admins-%(ou)s,cn=ouadmins,cn=groups,%(basedn)s',
+					  ):
+			grpdn = grpdn % {'ou': ou_name, 'basedn': self._ucr.get('ldap/base')}
+			self._remove_udm_object('groups/group', grpdn)
+
+		# remove OU recursively
+		oudn = 'ou=%(ou)s,%(basedn)s' % {'ou': ou_name, 'basedn': self._ucr.get('ldap/base')}
+		self._remove_udm_object('container/ou', oudn)
+		print '*** Purging OU %s and related objects: done' % ou_name
+
+
+	def create_ou(self, ou_name=None, name_edudc=None, displayName=None):
+		"""
+		Creates a new OU with random or specified name. The function also sets a random display name
+		if no display name has been specified. name_edudc may contain the optional name for an
+		educational dc slave.
+
+		Return value: (ou_name, ou_dn)
+			ou_name: name of the created OU
+			ou_dn:   DN of the created OU object
+		"""
+		# create random display name for OU
+		charset = uts.STR_ALPHANUMDOTDASH + uts.STR_ALPHA.upper() + '()[]/,;:_#"+*@<>~ßöäüÖÄÜ$%&!     '
+		if displayName is None:
+			displayName = uts.random_string(length=random.randint(5, 50), charset=charset)
+			ou_displayName = displayName
+
+		# create random OU name
+		if not ou_name:
+			ou_name = uts.random_string(length=random.randint(3, 12))
+
+		# remember OU name for cleanup
+		self._cleanup_ou_names.add(ou_name)
+
+		# build command line
+		cmd = [self.PATH_CMD_CREATE_OU]
+		if displayName:
+			cmd += ['--displayName', ou_displayName]
+		cmd += [ou_name]
+		if name_edudc:
+			cmd += [name_edudc]
+
+		print 'Calling following command: %r' % cmd
+		retval = subprocess.call(cmd)
+		if retval:
+			utils.fail('create_ou failed with exitcode %s' % retval)
+
+		ou_dn = 'ou=%s,%s' % (ou_name, self.LDAP_BASE)
+		return ou_name, ou_dn
+
+	def _get_district(self, ou_name):
+		try:
+			return ou_name[:2]
+		except IndexError:
+			raise UCSTestSchool_OU_Name_Too_Short('The OU name "%s" is too short for district mode' % ou_name)
+
+	def _get_ou_base_dn(self, ou_name):
+		"""
+		Returns the LDAP DN for the given school OU name (the district mode will be considered).
+		"""
+		dn = '%(school)s,%(district)s%(basedn)s'
+		values = {'school':'ou=%s' % ou_name,
+				  'district':'',
+				  'basedn': self.LDAP_BASE,
+				  }
+		if self._ucr.is_true('ucsschool/ldap/district/enable'):
+			values['district'] = 'ou=%s,' % self._get_district(ou_name)
+		return dn % values
+
+	def _get_user_container(self, ou_name, is_teacher=False, is_staff=False):
+		"""
+		Returns user container for specified user role and ou_name.
+		"""
+		if is_teacher and is_staff:
+			return 'cn=%s,cn=users,%s' % (self.CN_TEACHERS_STAFF, self._get_ou_base_dn(ou_name))
+		if is_teacher:
+			return 'cn=%s,cn=users,%s' % (self.CN_TEACHERS, self._get_ou_base_dn(ou_name))
+		if is_staff:
+			return 'cn=%s,cn=users,%s' % (self.CN_STAFF, self._get_ou_base_dn(ou_name))
+		return 'cn=%s,cn=users,%s' % (self.CN_STUDENT, self._get_ou_base_dn(ou_name))
+
+
+	def create_user(self, ou_name, username=None, firstname=None, lastname=None, classes=None,
+					mailaddress=None, is_teacher=False, is_staff=False, is_active=True):
+		"""
+		Create a user in specified OU with given attributes. If attributes are not specified, random
+		values will be used for username, firstname and lastname.
+		"""
+		if not ou_name:
+			raise UCSTestSchool_MissingOU('No OU name specified')
+
+		# set default values
+		if username is None:
+			username = uts.random_username()
+		if firstname is None:
+			firstname = uts.random_string(length=10, numeric=False)
+		if lastname is None:
+			lastname = uts.random_string(length=10, numeric=False)
+		if classes is None:
+			classes = ''
+		if mailaddress is None:
+			mailaddress = ''
+
+		# create import file
+		line = 'A\t%s\t%s\t%s\t%s\t%s\t\t%s\t%d\t%d\t%d\n' % (username, lastname, firstname, ou_name, classes,
+															mailaddress, int(is_teacher), int(is_active), int(is_staff))
+		with tempfile.NamedTemporaryFile() as tmp_file:
+			tmp_file.write(line)
+			tmp_file.flush()
+
+			cmd = [self.PATH_CMD_IMPORT_USER, tmp_file.name]
+			print 'Calling following command: %r' % cmd
+			retval = subprocess.call(cmd)
+			if retval:
+				utils.fail('create_ou failed with exitcode %s' % retval)
+
+		user_dn = 'uid=%s,%s' % (username, self._get_user_container(ou_name, is_teacher, is_staff))
+		return username, user_dn
+
+
+
+if __name__ == '__main__':
+	with UCSTestSchool() as schoolenv:
+		# create ou
+		ou_name, ou_dn = schoolenv.create_ou(displayName='') # FIXME: displayName has been disabled for backward compatibility
+		print 'NEW OU'
+		print '  ', ou_name
+		print '  ', ou_dn
+		# create user
+		user_name, user_dn = schoolenv.create_user(ou_name)
+		print 'NEW USER'
+		print '  ', user_name
+		print '  ', user_dn
