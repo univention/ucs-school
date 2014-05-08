8222d1189ac762f7a79a7be02c231a5aac961158
Sönke Schwardt-Krummrich <schwardt@univention.de>
2014-05-08 13:13:39 +0000

    Bug #34773: updated copyrights and fixed bashism
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.2/ucs-school-3.2r2@50019 1b283449-9f2f-0410-b571-8eb228e1a901

--- ucs-school-webproxy/pupilgroups.py
+++ ucs-school-webproxy/pupilgroups.py
@@ -3,7 +3,7 @@
 # Univention Directory Listener Module Pupilgroups
 #  listener module: pupilgroups
 #
-# Copyright 2008-2013 Univention GmbH
+# Copyright 2008-2014 Univention GmbH
 #
 # http://www.univention.de/
 #
