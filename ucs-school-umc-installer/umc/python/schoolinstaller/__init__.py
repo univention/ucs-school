c26539fe2c86444d89c801fe2782e0e71c35f185
Sönke Schwardt-Krummrich <schwardt@univention.de>
2014-05-08 12:35:52 +0000

    Bug #34166: updated copyright entries / added missing dependency
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.2/ucs-school-3.2r2@50012 1b283449-9f2f-0410-b571-8eb228e1a901

--- ucs-school-umc-installer/umc/python/schoolinstaller/__init__.py
+++ ucs-school-umc-installer/umc/python/schoolinstaller/__init__.py
@@ -3,7 +3,7 @@
 # Univention Management Console
 #  This installation wizard guides the installation of UCS@school in the domain
 #
-# Copyright 2013 Univention GmbH
+# Copyright 2013-2014 Univention GmbH
 #
 # http://www.univention.de/
 #
