e9497b356b38c584017128ccbbf6bcb3c9439491
Dirk Wiesenthal <wiesenthal@univention.de>
2014-05-09 12:31:06 +0000

    * Bug #34337: Improve handling of situations without schools
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.2/ucs-school-3.2r2@50089 1b283449-9f2f-0410-b571-8eb228e1a901

--- ucs-school-umc-wizards/umc/js/schoolwizards.js
+++ ucs-school-umc-wizards/umc/js/schoolwizards.js
@@ -33,14 +33,16 @@ define([
 	"dojo/_base/declare",
 	"dojo/_base/lang",
 	"dojo/promise/all",
+	"dojo/topic",
 	"umc/tools",
+	"umc/dialog",
 	"umc/widgets/Module",
 	"umc/modules/schoolwizards/UserGrid",
 	"umc/modules/schoolwizards/ClassGrid",
 	"umc/modules/schoolwizards/ComputerGrid",
 	"umc/modules/schoolwizards/SchoolGrid",
 	"umc/i18n!umc/modules/schoolwizards"
-], function(declare, lang, all, tools, Module, UserGrid, ClassGrid, ComputerGrid, SchoolGrid, _) {
+], function(declare, lang, all, topic, tools, dialog, Module, UserGrid, ClassGrid, ComputerGrid, SchoolGrid, _) {
 	var grids = {
 		'schoolwizards/users': UserGrid,
 		'schoolwizards/classes': ClassGrid,
@@ -55,9 +57,31 @@ define([
 
 		buildRendering: function() {
 			this.inherited(arguments);
-			var schools = this.umcpCommand('schoolwizards/schools', {'all_option_if_appropriate' : true}).then(lang.hitch(this, function(data) {
-				this.schools = data.result;
-			}));
+			var schools = this.umcpCommand('schoolwizards/schools', {}, false).then(
+				lang.hitch(this, function(data) {
+					this.schools = data.result;
+				}),
+				lang.hitch(this, function() {
+					// error. most probably no schools found
+					if (this.moduleFlavor == 'schoolwizards/schools') {
+						// goto school grid. no need for error message
+						return;
+					}
+					dialog.confirm(_('Would you like to create a first school?'), [{
+						name: 'cancel',
+						label: _('Cancel')
+					}, {
+						name: 'add',
+						'default': true,
+						label: _('Create school')
+					}], _('No school found')).then(lang.hitch(this, function(response) {
+						if (response == 'add') {
+							topic.publish('/umc/modules/open', 'schoolwizards', 'schoolwizards/schools');
+							topic.publish('/umc/tabs/close', this);
+						}
+					}));
+				})
+			);
 			var ucrVariables = tools.ucr(['ucsschool/wizards/udmlink', 'ucsschool/wizards/autosearch', 'ucsschool/wizards/' + this.moduleFlavor + '/autosearch']).then(lang.hitch(this, lang.hitch(this, function(ucr) {
 				this.autoSearch = tools.isTrue(ucr['ucsschool/wizards/' + this.moduleFlavor + '/autosearch'] || ucr['ucsschool/wizards/autosearch'] || true);
 				var udmLink = ucr['ucsschool/wizards/udmlink'];
