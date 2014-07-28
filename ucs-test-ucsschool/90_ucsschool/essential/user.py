a6838f7556bf384cd78343e2c32586d54ef64755
Ammar Najjar <najjar@univention.de>
2014-07-28 08:38:27 +0000

    Bug #35415: inherit from class Person in essential.importusers
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.2/ucs-school-3.2r2@52267 1b283449-9f2f-0410-b571-8eb228e1a901

--- ucs-test-ucsschool/90_ucsschool/essential/user.py
+++ ucs-test-ucsschool/90_ucsschool/essential/user.py
@@ -9,6 +9,7 @@ import univention.testing.utils as utils
 from univention.lib.umc_connection import UMCConnection
 import univention.testing.ucr as ucr_test
 from univention.testing.ucsschool import UCSTestSchool
+from essential.importusers import Person
 
 class GetFail(Exception):
 	pass
@@ -29,53 +30,29 @@ class EditFail(Exception):
 	pass
 
 
-class User(object):
+class User(Person):
 
-	"""Contains the needed functuality for users in the UMC module schoolwizards/users.
-	By default they are randomly formed.\n
-	:param umc_connection:
-	:type umc_connection: UMC connection object
-	:param ucr:
-	:type ucr: UCR object
-	:param name: name of the user to be created later
-	:type name: str
-	:param display_name: display_name of the user to be created later
-	:type display_name: str
+	"""Contains the needed functuality for users in the UMC module schoolwizards/users.\n
+	:param school: school name of the user
+	:type school: str
+	:param role: role of the user
+	:type role: str ['student', 'teacher', 'staff', 'teacherAndStaff']
+	:param school_class: name of the class which contain the user
+	:type school_class: str
 	"""
 
-	# Initialization (Random by default)
-	# types = ['student', 'teacher', 'staff', teacherAndStaff']
-	def __init__(self,
-				 school,
-				 typ=None,
-				 name=None,
-				 firstname=None,
-				 lastname=None,
-				 school_class=None,
-				 email=None,
-				 password=None,
-				 ucr=None,
-				 umc_connection=None):
-		self.school = school
-		self.typ = typ if typ else 'student'
-		self.password = password if password else 'univention'
-		self.name = name if name else uts.random_string()
-		self.firstname = firstname if firstname else uts.random_string()
-		self.lastname = lastname if lastname else uts.random_string()
-		self.email = email if email else '%s@%s.de' % (uts.random_string(), uts.random_string())
-		self.school_class = school_class if school_class else uts.random_string()
-
-		self.ucr = ucr if ucr else ucr_test.UCSTestConfigRegistry()
+	def __init__(self, school, role, school_class):
+		Person.__init__(self, school, role)
+		self.classes = [school_class]
+		self.school_class = school_class
+		self.ucr = ucr_test.UCSTestConfigRegistry()
 		self.ucr.load()
-		if umc_connection:
-			self.umc_connection = umc_connection
-		else:
-			host = self.ucr.get('ldap/master')
-			self.umc_connection = UMCConnection(host)
-			account = utils.UCSTestDomainAdminCredentials()
-			admin = account.username
-			passwd = account.bindpw
-			self.umc_connection.auth(admin, passwd)
+		host = self.ucr.get('ldap/master')
+		self.umc_connection = UMCConnection(host)
+		account = utils.UCSTestDomainAdminCredentials()
+		admin = account.username
+		passwd = account.bindpw
+		self.umc_connection.auth(admin, passwd)
 
 	def __enter__(self):
 		return self
@@ -91,9 +68,9 @@ class User(object):
 					'object':{
 						'school': self.school,
 						'school_class': self.school_class,
-						'email': self.email,
-						'name': self.name,
-						'type': self.typ,
+						'email': self.mail,
+						'name': self.username,
+						'type': self.role,
 						'firstname': self.firstname,
 						'lastname': self.lastname,
 						'password': self.password
@@ -101,30 +78,12 @@ class User(object):
 					'options': None
 					}
 				]
-		print 'Creating user %s' % (self.name,)
+		print 'Creating user %s' % (self.username,)
 		print 'param = %s' % (param,)
 		reqResult = self.umc_connection.request(
-				'schoolwizards/users/add',
-				param,
-				flavor)
+				'schoolwizards/users/add', param, flavor)
 		if not reqResult[0]:
 			raise CreateFail('Unable to create user (%r)' % (param,))
-
-	# create list of random users returns list of user objects
-	def createList(self, count):
-		"""Create a list of users
-		with random names and display_name\n
-		:param count: number of wanted users
-		:type count: int
-		:returns: [school_object]
-		"""
-		print 'Creating schoolsList'
-		cList = []
-		for i in xrange(count):
-			c = self.__school__(self.school, umc_connection=self.umc_connection, ucr=self.ucr)
-			c.create()
-			cList.append(c)
-		return cList
 
 	def get(self):
 		"""Get user"""
@@ -132,7 +91,7 @@ class User(object):
 		param =	[
 				{
 					'object':{
-						'$dn$': self.dn(),
+						'$dn$': self.dn,
 						'school': self.school
 						}
 					}
@@ -140,15 +99,15 @@ class User(object):
 		reqResult = self.umc_connection.request(
 				'schoolwizards/users/get',param,flavor)
 		if not reqResult[0]:
-			raise GetFail('Unable to get user (%s)' % self.name)
+			raise GetFail('Unable to get user (%s)' % self.username)
 		else:
 			return reqResult[0]
 
 	def check_get(self):
 		info = {
-				'$dn$': self.dn(),
+				'$dn$': self.dn,
 				'display_name': ' '.join([self.firstname,self.lastname]),
-				'name': self.name,
+				'name': self.username,
 				'firstname': self.firstname,
 				'lastname': self.lastname,
 				'type_name': self.type_name(),
@@ -157,8 +116,8 @@ class User(object):
 				'disabled' : 'none',
 				'birthday': None,
 				'password': None,
-				'type': self.typ,
-				'email': self.email,
+				'type': self.role,
+				'email': self.mail,
 				'objectType': 'users/user'
 				}
 		get_result = self.get()
@@ -166,16 +125,16 @@ class User(object):
 		info['type_name'] = get_result['type_name']
 		if get_result != info:
 			raise GetCheckFail('Failed get request for user %s. Returned result: %r. Expected result: %r' % (
-				self.name, get_result, info))
+				self.username, get_result, info))
 
 	def type_name(self):
-		if self.typ == 'student':
+		if self.role == 'student':
 			return 'Student'
-		elif self.typ == 'teacher':
+		elif self.role == 'teacher':
 			return 'Teacher'
-		elif self.typ == 'staff':
+		elif self.role == 'staff':
 			return 'Staff'
-		elif self.typ == 'teacherAndStaff':
+		elif self.role == 'teacherAndStaff':
 			return 'Teacher and Staff'
 
 	def query(self):
@@ -197,24 +156,15 @@ class User(object):
 			raise QueryCheckFail('users from query do not contain the existing users, found (%r), expected (%r)' % (
 				k, users))
 
-	def dn(self):
-		if self.typ == 'student':
-			return 'uid=%s,cn=schueler,cn=users,%s' % (self.name, UCSTestSchool().get_ou_base_dn(self.school))
-		elif self.typ == 'teacher':
-			return 'uid=%s,cn=lehrer,cn=users,%s' % (self.name, UCSTestSchool().get_ou_base_dn(self.school))
-		elif self.typ == 'staff':
-			return 'uid=%s,cn=mitarbeiter,cn=users,%s' % (self.name, UCSTestSchool().get_ou_base_dn(self.school))
-		elif self.typ == 'teacherAndStaff':
-			return 'uid=%s,cn=lehrer und mitarbeiter,cn=users,%s' % (self.name, UCSTestSchool().get_ou_base_dn(self.school))
-
 	def remove(self):
 		"""Remove user"""
+		print 'Removing User (%s)' % self.username
 		flavor = 'schoolwizards/users'
 		param =	[
 				{
 					'object':{
 						'school': self.school,
-						'$dn$': self.dn(),
+						'$dn$': self.dn,
 						},
 					'options': None
 					}
@@ -222,7 +172,9 @@ class User(object):
 		reqResult = self.umc_connection.request(
 				'schoolwizards/users/remove',param,flavor)
 		if not reqResult[0]:
-			raise RemoveFail('Unable to remove user (%s)' % self.name)
+			raise RemoveFail('Unable to remove user (%s)' % self.username)
+		else:
+			self.set_mode_to_delete()
 
 
 	def edit(self, new_attributes):
@@ -233,31 +185,30 @@ class User(object):
 					'object':{
 						'school': self.school,
 						'school_class': new_attributes.get('school_class') if new_attributes.get('school_class') else self.school_class,
-						'email': new_attributes.get('email') if new_attributes.get('email') else self.email,
-						'name': self.name,
-						'type': self.typ,
+						'email': new_attributes.get('email') if new_attributes.get('email') else self.mail,
+						'name': self.username,
+						'type': self.role,
 						'firstname': new_attributes.get('firstname') if new_attributes.get('firstname') else self.firstname,
 						'lastname': new_attributes.get('lastname') if new_attributes.get('lastname') else self.lastname,
 						'password': new_attributes.get('password') if new_attributes.get('password') else self.password,
-						'$dn$':  self.dn(),
+						'$dn$':  self.dn,
 						},
 					'options': None
 					}
 				]
-		print 'Editing user %s' % (self.name,)
+		print 'Editing user %s' % (self.username,)
 		print 'param = %s' % (param,)
 		reqResult = self.umc_connection.request(
 				'schoolwizards/users/put',
 				param,
 				flavor)
 		if not reqResult[0]:
-			raise EditFail('Unable to edit user (%s) with the parameters (%r)' % (self.name , param))
+			raise EditFail('Unable to edit user (%s) with the parameters (%r)' % (self.username , param))
 		else:
+			self.set_mode_to_modify()
 			self.school_class = new_attributes['school_class']
-			self.email = new_attributes['email']
+			self.classes = [self.school_class]
+			self.mail = new_attributes['email']
 			self.firstname = new_attributes['firstname']
 			self.lastname = new_attributes['lastname']
 			self.password = new_attributes['password']
-
-	def verify_ldap(self, should_exist):
-		utils.verify_ldap_object(self.dn(), should_exist=should_exist)
