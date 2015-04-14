e823b6a634d051ffa4c31ddd358990cbad193f38
Florian Best <best@univention.de>
2015-04-14 13:25:55 +0000

    Bug #38198: autopep8
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.0/ucs-school-4.0r2@59784 1b283449-9f2f-0410-b571-8eb228e1a901

--- ucs-school-umc-helpdesk/umc/python/helpdesk/__init__.py
+++ ucs-school-umc-helpdesk/umc/python/helpdesk/__init__.py
@@ -44,18 +44,20 @@ import notifier.popen
 
 import smtplib
 
-_ = Translation( 'ucs-school-umc-helpdesk' ).translate
+_ = Translation('ucs-school-umc-helpdesk').translate
 
-class Instance( SchoolBaseModule ):
+
+class Instance(SchoolBaseModule):
+
 	@simple_response
 	@LDAP_Connection()
-	def configuration( self, ldap_user_read = None, ldap_position = None, search_base = None ):
-		MODULE.process( 'return configuration' )
-		username = _( 'unknown' )
+	def configuration(self, ldap_user_read=None, ldap_position=None, search_base=None):
+		MODULE.process('return configuration')
+		username = _('unknown')
 		if self._username:
 			username = self._username
 
-		MODULE.info( 'username=%s  school=%s' % ( self._username, search_base.school ) )
+		MODULE.info('username=%s  school=%s' % (self._username, search_base.school))
 
 		school = School.from_dn(School(search_base.school).dn, None, ldap_user_read)
 		return {
@@ -64,19 +66,18 @@ class Instance( SchoolBaseModule ):
 			'recipient': ucr.get('ucsschool/helpdesk/recipient')
 		}
 
-
-	def send( self, request ):
-		def _send_thread( sender, recipients, username, school, category, message ):
-			MODULE.info( 'sending mail: thread running' )
+	def send(self, request):
+		def _send_thread(sender, recipients, username, school, category, message):
+			MODULE.info('sending mail: thread running')
 
 			msg = u'From: ' + sender + u'\r\n'
 			msg += u'To: ' + (', '.join(recipients)) + u'\r\n'
 			msg += u'Subject: %s (%s: %s)\r\n' % (category, _('School'), school)
 			msg += u'\r\n'
-			msg += u'%s: %s\r\n' % ( _( 'Sender' ), username )
-			msg += u'%s: %s\r\n' % ( _( 'School' ), school )
-			msg += u'%s: %s\r\n' % ( _( 'Category' ), category )
-			msg += u'%s:\r\n' % _( 'Message' )
+			msg += u'%s: %s\r\n' % (_('Sender'), username)
+			msg += u'%s: %s\r\n' % (_('School'), school)
+			msg += u'%s: %s\r\n' % (_('Category'), category)
+			msg += u'%s:\r\n' % _('Message')
 			msg += message + u'\r\n'
 			msg += u'\r\n'
 
@@ -87,48 +88,44 @@ class Instance( SchoolBaseModule ):
 			server.sendmail(sender, recipients, msg)
 			server.quit()
 
-		def _send_return( thread, result, request ):
+		def _send_return(thread, result, request):
 			import traceback
 
-			if not isinstance( result, BaseException ):
-				MODULE.info( 'sending mail: completed successfully' )
-				self.finished( request.id, True )
+			if not isinstance(result, BaseException):
+				MODULE.info('sending mail: completed successfully')
+				self.finished(request.id, True)
 			else:
-				msg = '%s\n%s: %s\n' % ( ''.join( traceback.format_tb( thread.exc_info[ 2 ] ) ), thread.exc_info[ 0 ].__name__, str( thread.exc_info[ 1 ] ) )
-				MODULE.process( 'sending mail:An internal error occurred: %s' % msg )
-				self.finished( request.id, False, msg, False )
+				msg = '%s\n%s: %s\n' % (''.join(traceback.format_tb(thread.exc_info[2])), thread.exc_info[0].__name__, str(thread.exc_info[1]))
+				MODULE.process('sending mail:An internal error occurred: %s' % msg)
+				self.finished(request.id, False, msg, False)
 
-
-		keys = [ 'username', 'school', 'category', 'message' ]
-		self.required_options( request, *keys )
+		keys = ['username', 'school', 'category', 'message']
+		self.required_options(request, *keys)
 		for key in keys:
-			if request.options[ key ]:
-				MODULE.info( 'send ' + key + '=' + request.options[ key ].replace('%','_') )
+			if request.options[key]:
+				MODULE.info('send ' + key + '=' + request.options[key].replace('%', '_'))
 
 		if ucr.get('ucsschool/helpdesk/recipient'):
 			if ucr.get('hostname') and ucr.get('domainname'):
-				sender = 'ucsschool-helpdesk@%s.%s' % ( ucr[ 'hostname' ], ucr[ 'domainname' ] )
+				sender = 'ucsschool-helpdesk@%s.%s' % (ucr['hostname'], ucr['domainname'])
 			else:
 				sender = 'ucsschool-helpdesk@localhost'
 
-			func = notifier.Callback( _send_thread,
-									  sender,	ucr[ 'ucsschool/helpdesk/recipient' ].split( ' ' ),
-									  request.options[ 'username' ], request.options[ 'school' ],
-									  request.options[ 'category' ], request.options[ 'message' ] )
-			MODULE.info( 'sending mail: starting thread' )
-			cb = notifier.Callback( _send_return, request )
-			thread = notifier.threads.Simple( 'HelpdeskMessage', func, cb )
+			func = notifier.Callback(_send_thread, sender, ucr['ucsschool/helpdesk/recipient'].split(' '), request.options['username'], request.options['school'], request.options['category'], request.options['message'])
+			MODULE.info('sending mail: starting thread')
+			cb = notifier.Callback(_send_return, request)
+			thread = notifier.threads.Simple('HelpdeskMessage', func, cb)
 			thread.run()
 		else:
-			MODULE.error( 'HELPDESK: cannot send mail - config-registry variable "ucsschool/helpdesk/recipient" is not set' )
-			self.finished( request.id, False, _( 'The email address for the helpdesk team is not configured.' ) )
+			MODULE.error('HELPDESK: cannot send mail - config-registry variable "ucsschool/helpdesk/recipient" is not set')
+			self.finished(request.id, False, _('The email address for the helpdesk team is not configured.'))
 
 	@LDAP_Connection()
-	def categories( self, request, ldap_user_read = None, ldap_position = None, search_base = None ):
+	def categories(self, request, ldap_user_read=None, ldap_position=None, search_base=None):
 		categories = []
-		res = ldap_user_read.searchDn( filter = 'objectClass=univentionUMCHelpdeskClass', base = ldap_position.getBase() )
+		res = ldap_user_read.searchDn(filter='objectClass=univentionUMCHelpdeskClass', base=ldap_position.getBase())
 		# use only first object found
-		if res and res[ 0 ]:
-			categories = ldap_user_read.getAttr( res[ 0 ], 'univentionUMCHelpdeskCategory' )
+		if res and res[0]:
+			categories = ldap_user_read.getAttr(res[0], 'univentionUMCHelpdeskCategory')
 
-		self.finished( request.id, map( lambda x: { 'id' : x, 'label' : x }, categories ) )
+		self.finished(request.id, map(lambda x: {'id': x, 'label': x}, categories))
