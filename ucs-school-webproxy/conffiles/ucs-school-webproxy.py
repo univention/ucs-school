732ae3a8402a27b7c089984bab2652cf33f23058
Sönke Schwardt-Krummrich <schwardt@univention.de>
2016-01-07 20:56:54 +0000

    Bug #40082: updated copyright string
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-school-4.1@66657 1b283449-9f2f-0410-b571-8eb228e1a901

--- ucs-school-webproxy/conffiles/ucs-school-webproxy.py
+++ ucs-school-webproxy/conffiles/ucs-school-webproxy.py
@@ -3,7 +3,7 @@
 # Univention Config Registry
 #  enable/disable internet access in squidguard config
 #
-# Copyright 2007-2015 Univention GmbH
+# Copyright 2007-2016 Univention GmbH
 #
 # http://www.univention.de/
 #
