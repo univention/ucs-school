a1e7c25a18e9faa38a98420a052dd77f7ac18c57
Eduard Mai <mai@univention.de>
2016-11-07 13:28:53 +0000

    Bug #41777: remove obsolete source package ucs-school-translation-fr
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-school-4.1r2@74175 1b283449-9f2f-0410-b571-8eb228e1a901

--- ucs-school-translation-fr/additional_targets.mk
+++
@@ -1,5 +0,0 @@
-ADDITIONAL = $(DESTDIR)/usr/share/locale/fr/LC_MESSAGES/python-ucs-school.mo \
-	$(DESTDIR)/usr/share/locale/fr/LC_MESSAGES/univention-admin-handlers-settings-helpdesk.mo
-
-$(DESTDIR)/usr/share/locale/fr/LC_MESSAGES/python-ucs-school.mo: fr/ucs-school-lib/python/fr.po
-$(DESTDIR)/usr/share/locale/fr/LC_MESSAGES/univention-admin-handlers-settings-helpdesk.mo: fr/ucs-school-umc-helpdesk/modules/univention/admin/handlers/settings/fr.po
