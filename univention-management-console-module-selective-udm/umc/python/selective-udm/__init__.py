95d36811a44dca4cb8e7d2b55108990b73fe7da5
Arvid Requate <requate@univention.de>
2016-11-16 19:10:21 +0000

    * Bug #40435: Make selective-udm return the DN * Plus other fixes by fbest
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-school-4.1r2@74514 1b283449-9f2f-0410-b571-8eb228e1a901

--- univention-management-console-module-selective-udm/umc/python/selective-udm/__init__.py
+++ univention-management-console-module-selective-udm/umc/python/selective-udm/__init__.py
@@ -31,8 +31,8 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <http://www.gnu.org/licenses/>.
 
-import traceback
 import base64
+import ldap.filter
 
 from univention.lib.i18n import Translation
 import univention.config_registry
@@ -44,7 +44,9 @@ import univention.admin.handlers.users.user
 import univention.admin.handlers.computers.windows
 
 from univention.management.console.log import MODULE
-from univention.management.console.modules import UMC_Error
+from univention.management.console.base import UMC_Error
+from univention.management.console.modules.sanitizers import StringSanitizer
+from univention.management.console.modules.decorators import sanitize
 from ucsschool.lib.schoolldap import LDAP_Connection, SchoolBaseModule, ADMIN_WRITE, USER_READ
 
 from univention.management.console.config import ucr
@@ -57,81 +59,67 @@ univention.admin.syntax.update_choices()
 _ = Translation('univention-management-console-selective-udm').translate
 
 
-class CreationDenied(Exception):
-	pass
-
-
 class Instance(SchoolBaseModule):
 
 	def _check_usersid_join_permissions(self, lo, usersid):
-
 		allowed_groups = ucr.get('ucsschool/windows/join/groups', 'Domain Admins').split(',')
 
-		result = lo.search('sambaSID=%s' % usersid, attr=['dn'])
+		result = lo.searchDn(ldap.filter.filter_format('sambaSID=%s', [usersid]))
 		if not result:
-			raise CreationDenied('SID %s was not found' % usersid)
+			raise UMC_Error('SID %s was not found.' % (usersid,))
 
-		user_dn = result[0][0]
-		MODULE.info("Found user with DN %s" % user_dn)
+		user_dn = result[0]
+		MODULE.info("Found user with DN %s" % (user_dn,))
 
-		result = lo.search('uniqueMember=%s' % user_dn, attr=['cn'])
+		result = lo.search(ldap.filter.filter_format('uniqueMember=%s', [user_dn]), attr=['cn'])
 		if not result:
-			raise CreationDenied('No group memberships for SID %s found' % usersid)
+			raise UMC_Error('No group memberships for SID %s found.' % (usersid,))
 
 		for dn, attr in result:
 			if attr.get('cn', [])[0] in allowed_groups:
 				return
 
-		raise CreationDenied('SID %s is not member of one of the following groups: %s. The allowed groups can be modified by setting the UCR variable ucsschool/windows/join/groups.' % (usersid, allowed_groups))
+		raise UMC_Error('SID %s is not member of one of the following groups: %s. The allowed groups can be modified by setting the UCR variable ucsschool/windows/join/groups.' % (usersid, allowed_groups))
 
+	@sanitize(name=StringSanitizer(required=True))
 	@LDAP_Connection(USER_READ, ADMIN_WRITE)
 	def create_windows_computer(self, request, ldap_user_read=None, ldap_admin_write=None, ldap_position=None, search_base=None):
 
-		self.required_options(request, 'name')
-
 		if not search_base.school:
-			raise UMC_Error(_('Could not determine schoolOU'))
+			raise UMC_Error(_('Could not determine schoolOU.'))
 
-		try:
-			# Set new position
-			ldap_position.setDn(search_base.computers)
+		# Set new position
+		ldap_position.setDn(search_base.computers)
 
-			usersid = request.options.get('usersid')
-			self._check_usersid_join_permissions(ldap_user_read, usersid)
+		self._check_usersid_join_permissions(ldap_user_read, request.options.get('usersid'))
 
-			# Create the computer account
-			computer = univention.admin.handlers.computers.windows.object(None, ldap_admin_write, position=ldap_position, superordinate=None)
-			computer.open()
-			name = request.options.get('name')
-			if name[-1] == '$':
-				# Samba 3 calls the name in this way
-				name = name[:-1]
+		# Create the computer account
+		computer = univention.admin.handlers.computers.windows.object(None, ldap_admin_write, position=ldap_position, superordinate=None)
+		computer.open()
+		name = request.options.get('name')
+		if name[-1] == '$':
+			# Samba 3 calls the name in this way
+			name = name[:-1]
 
-			# In Samba 3 the samba attributes must be set by Samba itself
-			samba3_mode = request.options.get('samba3_mode')
-			if samba3_mode and samba3_mode.lower() in ['true', 'yes']:
-				computer.options = ['posix']
+		# In Samba 3 the samba attributes must be set by Samba itself
+		samba3_mode = request.options.get('samba3_mode')
+		if samba3_mode and samba3_mode.lower() in ['true', 'yes']:
+			computer.options = ['posix']
 
-			computer['name'] = name
+		computer['name'] = name
 
-			password = request.options.get('password')
-			if password:
-				decode_password = request.options.get('decode_password')
-				if decode_password and decode_password.lower() in ['true', 'yes']:
-					# the password is base64 encoded
-					password = base64.decodestring(password)
-					# decode from utf-16le
-					password = password.decode('utf-16le')
-					# and remove the quotes
-					password = password.strip('"')
-				computer['password'] = password
+		password = request.options.get('password')
+		if password:
+			decode_password = request.options.get('decode_password')
+			if decode_password and decode_password.lower() in ['true', 'yes']:
+				# the password is base64 encoded
+				password = base64.decodestring(password)
+				# decode from utf-16le
+				password = password.decode('utf-16le')
+				# and remove the quotes
+				password = password.strip('"')
+			computer['password'] = password
 
-			computer['description'] = request.options.get('description')
-			computer_dn = computer.create()
-
-		except Exception, err:
-			message = 'Failed to create windows computer\n%s' % traceback.format_exc()
-			MODULE.warn(message)
-			self.finished(request.id, {}, message, success=False)
-		else:
-			self.finished(request.id, {}, success=True)
+		computer['description'] = request.options.get('description')
+		computer_dn = computer.create()
+		self.finished(request.id, {'dn': computer_dn})
