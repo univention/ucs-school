7475b801f817a7bc274384bd074bcd7c7f7c2ded
Sönke Schwardt-Krummrich <schwardt@univention.de>
2017-07-11 11:18:00 +0000

    Bug #44916: add test with station id
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.1/ucs-school-4.1r2@81034 1b283449-9f2f-0410-b571-8eb228e1a901

--- ucs-test-ucsschool/90_ucsschool/essential/radius.py
+++ ucs-test-ucsschool/90_ucsschool/essential/radius.py
@@ -17,20 +17,19 @@ def write_peap_config_file(conf_file, username, password):
 	conf_file.write(content)
 
 
-def peap_auth(username, password, radius_secret):
+def peap_auth(username, password, radius_secret, station_id=None):
 	peap_conf_file = tempfile.NamedTemporaryFile(suffix='.conf', dir='/tmp')
 	print ' ** Creating temp config file %s' % peap_conf_file.name
 	write_peap_config_file(peap_conf_file, username, password)
 	peap_conf_file.flush()
 	peap_auth_cmd = ['eapol_test', '-c', '%(peap_conf_file)s', '-s', '%(radius_secret)s']
 	result = run_commands([peap_auth_cmd], {'peap_conf_file': peap_conf_file.name, 'radius_secret': radius_secret})
-	return_value = True if result[0] == 0 else False
-	return return_value
+	return result[0] == 0
 
 
-def test_peap_auth(username, password, radius_secret, should_succeed=True):
-	print '*** PEAP AUTH: user: %s, password: %s, should_succeed: %r' % (username, password, should_succeed), '-' * 40
-	auth_result = peap_auth(username, password, radius_secret)
+def test_peap_auth(username, password, radius_secret, station_id=None, should_succeed=True):
+	print '*** PEAP AUTH: user=%r, password=%r, should_succeed=%r, station_id=%r' % (username, password, should_succeed, station_id), '-' * 40
+	auth_result = peap_auth(username, password, radius_secret, station_id)
 	if auth_result != should_succeed:
-		raise TestFail('PEAP authentication unexpected result (%r), while the expected is (%r)\nUser=%s' % (
-			auth_result, should_succeed, username))
+		raise TestFail('PEAP authentication unexpected result (%r), while the expected is (%r)\nuser=%r, password=%r, should_succeed=%r, station_id=%r' % (
+			auth_result, should_succeed, username, password, should_succeed, station_id))
