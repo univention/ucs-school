f114866afa82ea184c3eea8818190e1bf0a9b30d
Florian Best <best@univention.de>
2017-07-27 12:05:57 +0000

    Bug #45026: add object class, attributes for UCS@school groups
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-school-4.2@81432 1b283449-9f2f-0410-b571-8eb228e1a901

--- ucs-school-import/syntax.ucs-school-import.py
+++ ucs-school-import/syntax.ucs-school-import.py
@@ -1,3 +1,6 @@
+from univention.admin.syntax import UDM_Objects, UDM_Attribute, select
+
+
 class UCSSchool_Server_DN(UDM_Objects):
 	udm_modules = ('computers/domaincontroller_master', 'computers/domaincontroller_backup', 'computers/domaincontroller_slave', 'computers/memberserver')
 	label = '%(fqdn)s'
@@ -10,3 +13,12 @@ class ucsschoolSchools(UDM_Attribute):
 	udm_filter = 'objectClass=ucsschoolOrganizationalUnit'
 	attribute = 'name'
 	label_format = '%(displayName)s'
+
+
+class ucsschoolTypes(select):
+	choices = [
+		('student', _('Student')),
+		('teacher', _('Teacher')),
+		('staff', _('Staff')),
+		('teacher_and_staff', _('Teacher and Staff')),
+	]
