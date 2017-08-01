b73c13eeeab353c59095d3175a4d089d2ef860b4
Daniel Tröder <troeder@univention.de>
2017-08-01 09:36:37 +0000

    Bug #45022: complete makeover of client API
    * fix module installation path
    * handle resources in separate classes
    * accept ID instead of URL as object identifier
    * add exception classes for most common errors
    * add support for user type
    * make SSL certificate verification optional
    
    
    
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-4.2/ucs-school-4.2@81582 1b283449-9f2f-0410-b571-8eb228e1a901

--- ucs-school-import/modules/ucsschool/http_api/client.py
+++ ucs-school-import/modules/ucsschool/http_api/client.py
@@ -33,6 +33,7 @@ HTTP API Client
 # <http://www.gnu.org/licenses/>.
 
 from __future__ import absolute_import, unicode_literals
+import copy
 import os.path
 try:
 	from urlparse import urljoin
@@ -40,6 +41,7 @@ except ImportError:
 	# Python 3
 	from urllib.parse import urljoin
 import logging
+import inspect
 
 import requests
 import magic
@@ -50,20 +52,71 @@ ucr = ConfigRegistry()
 ucr.load()
 MIME_TYPE = magic.open(magic.MAGIC_MIME_TYPE)
 MIME_TYPE.load()
+__resource_class_registry = list()
+
+
+def register_resource_class(cls):
+	if cls not in __resource_class_registry:
+		__resource_class_registry.append(cls)
+
+
+def get_resource_classes():
+	return __resource_class_registry
 
 
 class ApiError(Exception):
+	def __init__(self, msg, status_code=None):
+		super(ApiError, self).__init__(msg)
+		self.status_code = status_code
+
+
+class BadRequest(ApiError):
+	"""
+	HTTP 400
+	"""
 	pass
+
+class LoginError(ApiError):
+	"""
+	HTTP 403
+	"""
+	pass
+
+
+class ObjectNotFound(ApiError):
+	"""
+	HTTP 404
+	"""
+	pass
+
+
+class ServerError(ApiError):
+	"""
+	HTTP 5xx
+	"""
+	pass
+
+class ConnectionError(ApiError):
+	"""
+	Cannot establish / lost connection to server.
+	"""
+	pass
+
+
+class _ResourceMetaClass(type):
+	def __new__(cls, clsname, bases, attrs):
+		kls = super(_ResourceMetaClass, cls).__new__(cls, clsname, bases, attrs)
+		register_resource_class(kls)
+		return kls
 
 
 class Client(object):
 	LOG_REQUEST = 5
 	LOG_RESPONSE = 4
-	SCHOOLS_URL = 'schools/'
-	IMPORT_USERS_URL = 'imports/users/'
 
-	def __init__(self, name, password, server=None, version=1, log_level=logging.INFO, *args, **kwargs):
+	def __init__(self, name, password, server=None, version=1, log_level=logging.INFO, ssl_verify=True, *args, **kwargs):
 		"""
+		UCS@School HTTP API client.
 
 		:param name:
 		:param password:
@@ -75,11 +128,17 @@ class Client(object):
 		self.password = password
 		self.server = server or '{}.{}'.format(ucr['hostname'], ucr['domainname'])
 		self.version = version
-		self.session = None
-		self.cookies = None
-		self._lo = None
+		self.ssl_verify = ssl_verify
 		self.base_url = 'https://{}/api/v{}/'.format(self.server, self.version)
 		self.logger = self._setup_logging(log_level)
+		self.logger.debug('Registering resources and methods:')
+		for kls in get_resource_classes():
+			setattr(self, kls.__name__, kls(self))
+			self.logger.debug(
+				'  %s: %s',
+				kls.__name__,
+				', '.join([m[0] for m in inspect.getmembers(kls, predicate=inspect.ismethod) if m[0] != '__init__'])
+			)
 
 	@classmethod
 	def _setup_logging(cls, log_level):
@@ -104,7 +163,7 @@ class Client(object):
 			logger.setLevel(log_level)
 		return logger
 
-	def _call_api(self, method, url_end, data=None, files=None, params=None, **kwargs):
+	def call_api(self, method, url_end, data=None, files=None, params=None, **kwargs):
 		if not url_end.endswith('/'):
 			url_end += '/'
 		url = urljoin(self.base_url, url_end)
@@ -113,72 +172,107 @@ class Client(object):
 			data=data,
 			files=files,
 			params=params,
-			verify=False,
 			auth=(self.username, self.password),
 			**kwargs)
-		self.logger.request('%s(%s)', method, ', '.join('{}={!r}'.format(k, v) for k, v in request_kwargs.items()))
+		if not self.ssl_verify:
+			request_kwargs['verify'] = False
+		log_request_kwargs = copy.deepcopy(request_kwargs)
+		log_request_kwargs['auth'] = (log_request_kwargs['auth'][0], '*' * len(log_request_kwargs['auth'][1]))
+		self.logger.request('%s(%s)', method, ', '.join('{}={!r}'.format(k, v) for k, v in log_request_kwargs.items()))
 		meth = getattr(requests, method)
-		response = meth(**request_kwargs)
-		self.logger.response('%s (%r): %r', response.reason, response.status_code, response.content)
+		try:
+			response = meth(**request_kwargs)
+		except requests.ConnectionError as exc:
+			raise ConnectionError(str(exc))
+		self.logger.response('%s -> %s (%r): %r', response.url, response.reason, response.status_code, response.content)
 		if not response.ok:
-			raise ApiError('Received status_code={!r} with reason={!r} for requests.{}(**{}).'.format(response.status_code, response.reason, method, ', '.join('{}={!r}'.format(k, v) for k, v in request_kwargs.items())))
+			msg = 'Received status_code={!r} with reason={!r} for requests.{}(**{}).'.format(response.status_code, response.reason, method, ', '.join('{}={!r}'.format(k, v) for k, v in log_request_kwargs.items()))
+			if response.status_code == 400:
+				exc = BadRequest
+			elif response.status_code == 403:
+				exc = LoginError
+			elif response.status_code == 404:
+				exc = ObjectNotFound
+			elif 499 < response.status_code < 600:
+				exc = ServerError
+			else:
+				exc = ApiError
+			raise exc(msg, status_code=response.status_code)
 		return response.json()
 
-	@staticmethod
-	def _get_mime_type(data):
-		return MIME_TYPE.buffer(data)
+	class _Resource(object):
+		RESOURCE_URL = ''
 
-	def create_importjob(self, filename, source_uid, school, dryrun=True, file_obj=None):
-		"""
-		Create an ImportJob.
+		def __init__(self, client):
+			self.client = client
 
-		:param filename: str: path to a CSV file
-		:param source_uid: str: unique sourceUID of school management software database
-		:param school: URL: to one of those returned by get_schools()
-		:param dryrun: bool: False to start a real import
-		:param file_obj: optional file like object to read CSV data from, instead of opening 'filename'
-		:return: dict: the created ImportJob resource
-		"""
-		assert (isinstance(filename, basestring) or filename is None)
-		assert isinstance(source_uid, basestring)
-		assert isinstance(school, basestring)
-		assert isinstance(dryrun, bool)
-		assert (isinstance(file_obj, file) or file_obj is None)
+		def get(self, pk):
+			"""
+			Read Resource.
 
-		data = {
-			"source_uid": source_uid,
-			"dryrun": dryrun,
-			"school": school
-		}
-		filename = filename or 'noname'
-		if not file_obj:
-			file_obj = open(filename, 'rb')
-		file_data = file_obj.read(32)
-		mime_type = self._get_mime_type(file_data)
-		file_obj.seek(os.SEEK_SET)
-		files = {'input_file': (os.path.basename(filename), file_obj, mime_type)}
-		return self._call_api('post', self.IMPORT_USERS_URL, data=data, files=files)
+			:param pk: str: primary key (name, id, ..)
+			:return: dict
+			"""
+			url = urljoin(self.RESOURCE_URL, str(pk))
+			return self.client.call_api('get', url)
 
-	def get_importjobs(self, importjob_id=None):
-		"""
-		Read ImportJob(s).
+		def list(self):
+			"""
+			List all Resource this user has access to.
 
-		:param importjob_id: int or None
-		:return: list of dicts or dict if importjob_id is given
-		"""
-		url = self.IMPORT_USERS_URL
-		if importjob_id:
-			url += '{}/'.format(importjob_id)
-		return self._call_api('get', url)
+			:return: list of dicts
+			"""
+			return self.client.call_api('get', self.RESOURCE_URL)
 
-	def get_schools(self, ou_name=None):
-		"""
-		Read School(s).
 
-		:param ou_name: str: OU
-		:return: list of dicts or dict if ou_name is given
-		"""
-		url = self.SCHOOLS_URL
-		if ou_name:
-			url += '{}/'.format(ou_name)
-		return self._call_api('get', url)
+	class School(_Resource):
+		__metaclass__ = _ResourceMetaClass
+		RESOURCE_URL = 'schools/'
+
+
+	class UserImportJob(_Resource):
+		__metaclass__ = _ResourceMetaClass
+		RESOURCE_URL = 'imports/users/'
+
+		def create(self, filename, source_uid, school, user_role=None, dryrun=True, file_obj=None):
+			"""
+			Create a UserImportJob.
+
+			:param filename: str: path to a CSV file, or just a filename and read from 'file_obj'
+			:param source_uid: str: unique sourceUID of school management software database
+			:param school: str: name of a School
+			:param user_role: str: optional role of user, one of staff, student, teacher, teacher_and_staff
+			:param dryrun: bool: False to start a real import
+			:param file_obj: optional file like object to read CSV data from, instead of opening 'filename'
+			:return: dict: the created UserImportJob resource
+			"""
+			assert isinstance(filename, basestring)
+			assert isinstance(source_uid, basestring)
+			assert isinstance(school, basestring)
+			assert (isinstance(user_role, basestring) or user_role is None)
+			assert isinstance(dryrun, bool)
+			assert (isinstance(file_obj, file) or file_obj is None)
+
+			try:
+				school_resource = self.client.School.get(school)
+			except ObjectNotFound:
+				raise ObjectNotFound('School {!r} is unknown.'.format(school))
+
+			data = {
+				'source_uid': source_uid,
+				'dryrun': dryrun,
+				'school': school_resource['url'],
+				'user_role': user_role,
+			}
+			filename = filename or 'noname'
+			if not file_obj:
+				file_obj = open(filename, 'rb')
+			file_data = file_obj.read(32)
+			mime_type = self._get_mime_type(file_data)
+			file_obj.seek(os.SEEK_SET)
+			files = {'input_file': (os.path.basename(filename), file_obj, mime_type)}
+			return self.client.call_api('post', self.RESOURCE_URL, data=data, files=files)
+
+		@staticmethod
+		def _get_mime_type(data):
+			return MIME_TYPE.buffer(data)
