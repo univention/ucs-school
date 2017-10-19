53877bea5b9442ee15ad8b0a553dcc524832ab59
Daniel Troeder <troeder@univention.de>
2017-10-19 18:16:46 +0200

    Bug #45524: fix TypeError

--- ucs-test-ucsschool/90_ucsschool/essential/importusers_cli_v2.py
+++ ucs-test-ucsschool/90_ucsschool/essential/importusers_cli_v2.py
@@ -376,7 +376,7 @@ class CLI_Import_v2_Tester(object):
 		try:
 			with univention.testing.udm.UCSTestUDM() as udm, utu.UCSTestSchool() as schoolenv:
 				self.udm = udm
-				if self.maildomain not in self.ucr.get('mail/hosteddomains'):
+				if self.maildomain not in self.ucr.get('mail/hosteddomains', ''):
 					self.logger.info("\n\n*** Creating mail domain %r...\n", self.maildomain)
 					udm.create_object(
 						'mail/domain',
