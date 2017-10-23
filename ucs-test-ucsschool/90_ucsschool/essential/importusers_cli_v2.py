d94ab1da0908b68762fc03b0abb435f7f64126e4
Daniel Troeder <troeder@univention.de>
2017-10-23 19:01:22 +0200

    Bug #45524: fix AttributeError

--- ucs-test-ucsschool/90_ucsschool/essential/importusers_cli_v2.py
+++ ucs-test-ucsschool/90_ucsschool/essential/importusers_cli_v2.py
@@ -377,7 +377,7 @@ class CLI_Import_v2_Tester(object):
 			with univention.testing.udm.UCSTestUDM() as udm, utu.UCSTestSchool() as schoolenv:
 				self.udm = udm
 				if self.maildomain not in self.ucr.get('mail/hosteddomains', ''):
-					self.logger.info("\n\n*** Creating mail domain %r...\n", self.maildomain)
+					self.log.info("\n\n*** Creating mail domain %r...\n", self.maildomain)
 					udm.create_object(
 						'mail/domain',
 						position='cn=domain,cn=mail,{}'.format(self.ucr['ldap/base']),
