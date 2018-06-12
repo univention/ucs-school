838e8b0aef3a778efd0e1fedadf2733972197a20
Daniel Troeder <troeder@univention.de>
2018-06-12 11:17:04 +0200

    Bug #47181: create custom django-admin commands
    (cherry picked from commit 5bb9e5d)

--- ucs-school-import/modules/ucsschool/http_api/import_api/views.py
+++ ucs-school-import/modules/ucsschool/http_api/import_api/views.py
@@ -51,7 +51,7 @@ from rest_framework.permissions import BasePermission, IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend, FilterSet
 from django_filters import CharFilter, MultipleChoiceFilter
 from ucsschool.importer.utils.ldap_connection import get_machine_connection
-from ucsschool.http_api.import_api.models import JOB_CHOICES, Role,School, TextArtifact, UserImportJob
+from ucsschool.http_api.import_api.models import JOB_CHOICES, Role, School, TextArtifact, UserImportJob
 from ucsschool.http_api.import_api.serializers import (
 	UserImportJobCreationValidator,
 	UserImportJobSerializer,
