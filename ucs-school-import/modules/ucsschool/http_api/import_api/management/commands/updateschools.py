5bb9e5d0e415db5b7750eab020a233e49299bd03
Daniel Troeder <troeder@univention.de>
2018-06-12 11:17:04 +0200

    Bug #45163: create custom django-admin commands

---
+++ ucs-school-import/modules/ucsschool/http_api/import_api/management/commands/updateschools.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+#
+# Univention UCS@school
+#
+# Copyright 2018 Univention GmbH
+#
+# http://www.univention.de/
+#
+# All rights reserved.
+#
+# The source code of this program is made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation.
+#
+# Binary versions of this program provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention and not subject to the GNU AGPL V3.
+#
+# In the case you use this program under the terms of the GNU AGPL V3,
+# the program is provided in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <http://www.gnu.org/licenses/>.
+
+"""
+Django manage.py command to update one/some/all School object(s).
+"""
+
+from __future__ import unicode_literals
+from django.core.management.base import BaseCommand, CommandError
+from ucsschool.http_api.import_api.models import School
+
+
+class Command(BaseCommand):
+	def add_arguments(self, parser):
+		parser.add_argument(
+			'-a', '--all',
+			help='Update all school OU objects and delete not existing ones [default off].',
+			action='store_true'
+		)
+		parser.add_argument(
+			'--ou',
+			nargs='+',
+			type=str,
+			help='OU to update.'
+		)
+
+	def handle(self, *args, **options):
+		if not options['all'] and not options['ou']:
+			raise CommandError('Either --all or --ou must be used.')
+		if options['all']:
+			School.update_from_ldap()
+		else:
+			for ou in options['ou']:
+				try:
+					School.update_from_ldap(ou)
+				except RuntimeError as exc:
+					raise CommandError(str(exc))
+		self.stdout.write('Known schools now: {}.'.format(', '.join(s.name for s in School.objects.all())))
