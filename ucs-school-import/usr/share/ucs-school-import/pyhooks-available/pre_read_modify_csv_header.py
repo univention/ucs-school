ea4b83060d4e1d70a45629cea99d426fbc317374
Daniel Troeder <troeder@univention.de>
2019-03-07 16:22:35 +0100

    Bug #48189: WIP: pre-read hook that modifies the header of a CSV file

--- ucs-school-import/usr/share/ucs-school-import/pyhooks-available/pre_read_modify_csv_header.py
+++ ucs-school-import/usr/share/ucs-school-import/pyhooks-available/pre_read_modify_csv_header.py
@@ -29,42 +29,76 @@
 # <http://www.gnu.org/licenses/>.
 
 """
-Base class for all Python based pre-read hooks.
+A pre-read hooks that changes the header of a input CSV file.
+
+A mapping is read from the configuration file (csv:header_swap), the original
+CSV file is backuped and the header is changed.
+
+If a mapping key is not found in the input file, it is ignored.
+
+Example::
+	{
+		"csv": {
+			"header_swap": {
+				"Schulen Ost": "Schulen",
+				"Schulname": "Schulen",
+				"Familienname": "Nachname"
+			}
+		}
+	}
+	header 1 before: "ID","Vorname","Nachname","Email","Schulen Ost"
+	header 1 after : "ID","Vorname","Nachname","Email","Schulen"
+	header 2 before: "ID","Familienname,"Vorname","Schulname"
+	header 2 after : "ID","Nachname,"Vorname","Schulen"
 """
 
-from ucsschool.importer.utils.import_pyhook import ImportPyHook
-from ucsschool.importer.configuration import Configuration
+import csv
+import codecs
+import shutil
+import datetime
+from ucsschool.importer.exceptions import ConfigurationError
+from ucsschool.importer.reader.csv_reader import CsvReader, UTF8Recoder
+from ucsschool.importer.utils.pre_read_pyhook import PreReadPyHook
 try:
 	import typing
-	from ucsschool.importer.configuration import ReadOnlyDict
 except ImportError:
 	pass
 
 
-class PreReadPyHook(ImportPyHook):
+class ModifyCsvHeader(PreReadPyHook):
 	"""
-	Hook that is called before starting to read the input file.
-
-	* self.lo          # LDAP object
-	* self.logger      # Python logging instance
-	* self.config      # read-only import configuration
-
-	If multiple hook classes are found, hook functions with higher
-	priority numbers run before those with lower priorities. None disables
-	a function.
+	Hook is called before starting to read the input file to change the header
+	of the CSV input file.
 	"""
 	priority = {
-		'pre_read': None,
+		'pre_read': 1000,
 	}
-
-	def __init__(self, lo=None, dry_run=False, *args, **kwargs):
-		super(PreReadPyHook, self).__init__(lo, dry_run, *args, **kwargs)
-		self.config = Configuration()  # type: ReadOnlyDict
 
 	def pre_read(self):  # type: () -> None
 		"""
-		Run code before starting to read the input file.
+		* get mapping from the configuration file (csv:header_swap)
+		* backup original CSV file to {input:filename}.$date.bak.csv
+		* modify CSV file (input:filename)
 
 		:return: None
 		"""
-		return None
+		if not self.config['csv'].get('header_swap'):
+			raise ConfigurationError('Missing configuration key csv:header_swap.')
+
+		ori_file_name = self.config['input']['filename']
+		backup_file_name = '{}.{:%Y-%m-%d_%H:%M:%S}.bak.csv'.format(ori_file_name, datetime.datetime.now())
+		self.logger.info('Copying %r to %r...', ori_file_name, backup_file_name)
+		shutil.copy2(ori_file_name, backup_file_name)
+
+		encoding = CsvReader.get_encoding(backup_file_name)
+		with open(backup_file_name, 'r') as fp:
+			dialect = csv.Sniffer().sniff(fp.readline())
+
+		self.logger.info('Rewriting %r...', ori_file_name)
+		with open(backup_file_name, 'r') as fpr, open(ori_file_name, 'w') as fpw:
+			fpru = UTF8Recoder(fpr, encoding)
+			encoder = codecs.getincrementalencoder(encoding)()
+			writer = csv.writer(fpw, dialect=dialect)
+			for row in csv.reader(fpru, dialect=dialect):
+				writer.write([encoder.encode(r) for r in row])
+		self.logger.info('Done.')
