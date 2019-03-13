48523accb07f4e0a5b0ad222c87c6550ae8c85fb
Daniel Troeder <troeder@univention.de>
2019-03-13 16:38:05 +0100

    Bug #49268: improve type hints and py3 compatibility

--- ucs-school-lib/python/pyhooks/pyhooks_loader.py
+++ ucs-school-lib/python/pyhooks/pyhooks_loader.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # Univention UCS@school
-"""
-Loader for Python based hooks.
-"""
+#
 # Copyright 2016-2019 Univention GmbH
 #
 # http://www.univention.de/
@@ -31,11 +29,16 @@ Loader for Python based hooks.
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <http://www.gnu.org/licenses/>.
 
+"""
+Loader for Python based hooks.
+"""
+
 import imp
 import inspect
 from os import listdir
 import os.path
 from collections import defaultdict
+from six import iteritems
 
 try:
 	from typing import Any, Callable, Dict, List, Optional, Tuple, Type, TypeVar
@@ -54,8 +57,7 @@ class PyHooksLoader(object):
 	Use get_hook_classes() if you want to initialize them yourself.
 	"""
 
-	# Dict[base_class_name, List[Class]]
-	_hook_classes = {}  # type: Dict[str, List[PyHookTV]]
+	_hook_classes = {}  # type: Dict[str, List[Type[PyHookTV]]]
 
 	def __init__(self, base_dir, base_class, logger, filter_func=None):
 		# type: (str, Type[PyHookTV], logging.Logger, Optional[Callable[[Type[PyHookTV]], bool]]) -> None
@@ -81,7 +83,7 @@ class PyHooksLoader(object):
 		if filter_func:
 			assert callable(filter_func), "'filter_func' must be a callable, got {!r}.".format(filter_func)
 		self._filter_func = filter_func
-		self._pyhook_obj_cache = None
+		self._pyhook_obj_cache = None  # type: Dict[str, List[Callable[[...], Any]]]
 
 	def drop_cache(self):  # type: () -> None
 		"""
@@ -95,7 +97,7 @@ class PyHooksLoader(object):
 		if self.base_class_name in self._hook_classes:
 			del self._hook_classes[self.base_class_name]
 
-	def get_hook_classes(self):  # type: () -> List[PyHookTV]
+	def get_hook_classes(self):  # type: () -> List[Type[PyHookTV]]
 		"""
 		Search hook files in filesystem and load classes.
 		No objects are initialized, no sorting is done.
@@ -122,25 +124,26 @@ class PyHooksLoader(object):
 			self.logger.info("Found hook classes: %s", ", ".join(c.__name__ for c in self._hook_classes[self.base_class_name]))
 		return self._hook_classes[self.base_class_name]
 
-	def get_hook_objects(self, *args, **kwargs):  # type: (*Any, **Any) -> List[PyHookTV]
+	def get_hook_objects(self, *args, **kwargs):  # type: (*Any, **Any) -> Dict[str, List[Callable[[...], Any]]]
 		"""
-		Get hook classes, initialize objects and sort by method and priority.
+		Get initialized hook objects, sorted by method and priority.
 
 		:param tuple args: arguments to pass to __init__ of hooks
-		:param  dict kwargs: arguments to pass to __init__ of hooks
-		:return: initialized hook objects, sorted by method and method-priority
-		:rtype: Dict[List[ucsschool.lib.pyhooks.PyHook]]
+		:param dict kwargs: arguments to pass to __init__ of hooks
+		:return: mapping from method names to list of methods of initialized
+			hook objects, sorted by method priority
+		:rtype: Dict[str, List[Callable]]
 		"""
 		if self._pyhook_obj_cache is None:
 			pyhook_objs = [pyhook_cls(*args, **kwargs) for pyhook_cls in self.get_hook_classes()]
 
 			# fill cache: find all enabled hook methods
-			methods = defaultdict(list)
+			methods = defaultdict(list)  # type: Dict[str, List[Tuple[Callable[[...], Any], int]]]
 			for pyhook_obj in pyhook_objs:
 				if not hasattr(pyhook_obj, "priority") or not isinstance(pyhook_obj.priority, dict):
 					self.logger.warn('Ignoring hook %r without/invalid "priority" attribute.', pyhook_obj)
 					continue
-				for meth_name, prio in pyhook_obj.priority.items():
+				for meth_name, prio in iteritems(pyhook_obj.priority):
 					if hasattr(pyhook_obj, meth_name) and isinstance(pyhook_obj.priority.get(meth_name), int):
 						methods[meth_name].append((getattr(pyhook_obj, meth_name), pyhook_obj.priority[meth_name]))
 					elif hasattr(pyhook_obj, meth_name) and pyhook_obj.priority.get(meth_name) is None:
@@ -149,10 +152,15 @@ class PyHooksLoader(object):
 						self.logger.warn('Ignoring invalid priority item (%r : %r).', meth_name, prio)
 			# sort by priority
 			self._pyhook_obj_cache = dict()
-			for meth_name, meth_list in methods.items():
+			for meth_name, meth_list in iteritems(methods):
 				self._pyhook_obj_cache[meth_name] = [x[0] for x in sorted(meth_list, key=lambda x: x[1], reverse=True)]
 
-			self.logger.info("Loaded hooks: %r.", dict([(meth_name, ["{}.{}".format(m.im_class.__name__, m.im_func.func_name) for m in meths]) for meth_name, meths in self._pyhook_obj_cache.items()]))
+			self.logger.info(
+				"Loaded hooks: %r.",
+				dict([
+					(meth_name, ["{}.{}".format(m.im_class.__name__, m.im_func.func_name) for m in meths])
+					for meth_name, meths in iteritems(self._pyhook_obj_cache)
+				]))
 		return self._pyhook_obj_cache
 
 	@staticmethod
