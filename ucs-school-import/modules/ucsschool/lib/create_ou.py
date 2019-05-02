8840e5e80b6f4fc4685682a5b4037068a345da2a
Daniel Troeder <troeder@univention.de>
2019-05-02 17:33:22 +0200

    Bug #48141: remove duplicate hook execution

--- ucs-school-import/modules/ucsschool/lib/create_ou.py
+++ ucs-school-import/modules/ucsschool/lib/create_ou.py
@@ -30,94 +30,34 @@
 # <http://www.gnu.org/licenses/>.
 
 """
-Temporary class and function to create an OU using shell hooks.
-Remove once Bug #48141 has been fixed.
+Class to create an OU.
 Used by create_ou script and customer single user HTTP API.
 """
 
-import os
-import sys
-import subprocess
-import tempfile
 import logging
-from ucsschool.lib.models.school import School
-import univention.config_registry
 from ldap.filter import filter_format
-
-config_registry = univention.config_registry.ConfigRegistry()
-config_registry.load()
-_hooks = None
-
-
-# TODO: REMOVE WITH Bug #48141
-class Hooks(object):
-	OBJECTS = ('user', 'group', 'printer', 'computer', 'network', 'router', 'ou')
-	OPERATIONS = {'A': 'create', 'M': 'modify', 'D': 'remove'}
-
-	PATH = '/usr/share/ucs-school-import/hooks/'
-
-	def _create_temp_file(self, line):
-		tmpfile = tempfile.NamedTemporaryFile()
-		tmpfile.write(line)
-		tmpfile.flush()
-		return tmpfile
-
-	def __run(self, phase, module, action, **kwargs):
-		# verify phase
-		if action not in Hooks.OPERATIONS:
-			return False
-
-		# verify path
-		path = os.path.join(Hooks.PATH, '%s_%s_%s.d' % (module, Hooks.OPERATIONS[action], phase))
-		if not os.path.isdir(path) or not os.listdir(path):
-			return False
-
-		# create temporary file with data
-		if 'line' in kwargs:
-			kwargs['line'] = kwargs['line'].strip() + "\n"
-			tmpfile = self._create_temp_file(kwargs.get('line'))
-
-		# invoke hook scripts
-		# <script> <temporary file> [<ldap dn>]
-		command = ['run-parts', path]
-		if 'line' in kwargs:
-			command.extend(('--arg', tmpfile.name))
-		if 'dn' in kwargs:
-			command.extend(('--arg', kwargs['dn']))
-
-		sys.stdout.flush()
-		sys.stderr.flush()
-		ret_code = subprocess.call(command)
-
-		# close temporary file (also deletes the file)
-		if 'line' in kwargs:
-			tmpfile.close()
-
-		return ret_code == 0
-
-	def pre(self, module, action, **kwargs):
-		return self.__run('pre', module, action, **kwargs)
-
-	def post(self, module, action, **kwargs):
-		return self.__run('post', module, action, **kwargs)
+from ucsschool.lib.models.school import School
+from ucsschool.lib.models.utils import ucr
 
 
 def create_ou(ou_name, display_name, edu_name, admin_name, share_name, lo, baseDN, hostname, is_single_master, alter_dhcpd_base=None):
-	"""Raises ValueError, uidAlreadyUsed"""
-	global _hooks
-	# TODO: REMOVE WITH Bug #48141
-	if not _hooks:
-		_hooks = Hooks()
-	hooks = _hooks
+	"""
+	Create a ucsschool OU.
 
-	# invoke pre hooks
-	if edu_name:
-		dccn = edu_name
-	else:
-		dccn = ''
-	myline = '%s\t%s' % (ou_name, dccn)
-	hooks.pre('ou', 'A', line=myline)
-
+	:param str ou_name: name for the OU
+	:param str display_name: display name for the OU
+	:param str edu_name: host name of educational school server
+	:param str admin_name: host name of administrative school server
+	:param str share_name: host name
+	:param univention.uldap.acceess lo: LDAP connection object
+	:param str baseDN: base DN
+	:param str hostname: hostname of master in case of singlemaster
+	:param bool is_single_master: whther it is a singlemaster
+	:param bool alter_dhcpd_base: if the DHCP base should be modified
+	:return bool: whether the OU was sucessfully created (or already existed)
+	:raises ValueError: on validation errors
+	:raises uidAlreadyUsed:
+	"""
 	if not edu_name and is_single_master:
 		edu_name = hostname
 	elif not edu_name and not is_single_master:
@@ -135,15 +75,15 @@ def create_ou(ou_name, display_name, edu_name, admin_name, share_name, lo, baseD
 	share_dn = ''
 	if share_name is None:
 		share_name = edu_name
-	objects = lo.searchDn(filter='(&(objectClass=univentionHost)(cn={}))'.format(share_name), base=baseDN)
+	objects = lo.searchDn(filter=filter_format('(&(objectClass=univentionHost)(cn=%s))', (share_name,)), base=baseDN)
 	if not objects:
 		if share_name == 'dc{}-01'.format(ou_name) or (edu_name and share_name == edu_name):
-			share_dn = filter_format('cn=%s,cn=dc,cn=server,cn=computers,%s', [share_name, new_school.dn])
+			share_dn = filter_format('cn=%s,cn=dc,cn=server,cn=computers,%s', (share_name, new_school.dn))
 		else:
 			logger.warn(
 				'WARNING: share file server name %r not found! Using %r as share file server.',
-				share_name, config_registry.get('hostname'))
-			share_dn = config_registry.get('ldap/hostdn')
+				share_name, ucr.get('hostname'))
+			share_dn = ucr.get('ldap/hostdn')
 	else:
 		share_dn = objects[0]
 
@@ -160,10 +100,10 @@ def create_ou(ou_name, display_name, edu_name, admin_name, share_name, lo, baseD
 		for key, value in new_school.errors.items():
 			error_str += '{}: {}\n'.format(key, value)
 		raise ValueError(error_str)
-	new_school.create(lo)
-	if new_school.exists(lo):
-		logger.info('OU %r exists', new_school.name)
-		# TODO: REMOVE WITH Bug #48141
-		# invoke post hooks
-		# for OUs the temporary file contains the LDAP DN
-		hooks.post('ou', 'A', dn=new_school.dn, line=myline)
+
+	res = new_school.create(lo)
+	if res:
+		logger.info('OU %r created successfully.', new_school.name)
+	else:
+		logger.error('Error creating OU %r.', new_school.name)
+	return res
