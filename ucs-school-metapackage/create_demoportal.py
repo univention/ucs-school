73c7af78ab7196016e33ecbbc1d4c1ee39c7df0c
Daniel Troeder <troeder@univention.de>
2019-07-16 10:32:31 +0200

    Bug #49531: create separate staff and admin users, created admin from teacher

--- ucs-school-metapackage/create_demoportal.py
+++ ucs-school-metapackage/create_demoportal.py
@@ -113,14 +113,17 @@ def create_school():
 	student.create(lo)
 	teacher = Teacher(firstname='Demo', lastname='Teacher', name='demo_teacher', password=demo_password, school=SCHOOL[0])
 	teacher.create(lo)
-	school_staff = Staff(firstname='Demo', lastname='Staff', name='demo_admin', password=demo_password, school=SCHOOL[0])
-	school_staff.create(lo)
-	# make staff user a school admin
+	staff = Staff(firstname='Demo', lastname='Staff', name='demo_staff', password=demo_password, school=SCHOOL[0])
+	staff.create(lo)
+	# create school admin from teacher
+	admin = Teacher(firstname='Demo', lastname='Admin', name='demo_admin', password=demo_password, school=SCHOOL[0])
+	admin.create(lo)
 	admin_group = module_groups.lookup(None, lo, 'name=admins-{}'.format(SCHOOL[0]), pos.getBase())[0].dn
-	school_staff_udm = school_staff.get_udm_object(lo)
-	school_staff_udm.options.append('ucsschoolAdministrator')
-	school_staff_udm['groups'].append(admin_group)
-	school_staff_udm.modify()
+	admin_udm = admin.get_udm_object(lo)
+	admin_udm.options.append('ucsschoolAdministrator')
+	admin_udm['groups'].append(admin_group)
+	admin_udm['description'] = 'School Admin for {} created from teacher account.'.format(SCHOOL[0])
+	admin_udm.modify()
 
 
 def create_portal():
