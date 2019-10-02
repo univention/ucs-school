1e010f9a1c9f2e3881d8ea38be5a80cc838a6ed6
Daniel Troeder <troeder@univention.de>
2019-10-02 10:25:09 +0200

    code linter, formatter, coverage, test samples, packaging

--- kelvin-api/ucsschool/kelvin/tests/test_utils.py
+++ kelvin-api/ucsschool/kelvin/tests/test_utils.py
@@ -1,6 +1,15 @@
-import packaging.version
-import ucsschool.kelvin.constants
+import logging
+import ucsschool.kelvin.utils
 
 
-def test_parse_version():
-    packaging.version.parse(ucsschool.kelvin.constants.__version__)
+def test_get_logger():
+    logger = ucsschool.kelvin.utils.get_logger(__name__)
+    assert logger.level == logging.DEBUG
+    assert "kelvin-api" in (handler.name for handler in logger.handlers)
+
+
+def test_get_logger_no_double_handlers():
+    logger = ucsschool.kelvin.utils.get_logger(__name__)
+    num1 = len(logger.handlers)
+    logger = ucsschool.kelvin.utils.get_logger(__name__)
+    assert len(logger.handlers) == num1
