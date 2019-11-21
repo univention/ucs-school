3fdfdb449cabaa99a8a7fc389293580ebed56589
Daniel Troeder <troeder@univention.de>
2019-11-21 13:56:07 +0100

    switch to alpine:latest, improve Docker build process, don't read UCR at import time

--- kelvin-api/ucsschool/kelvin/token_auth.py
+++ kelvin-api/ucsschool/kelvin/token_auth.py
@@ -114,7 +114,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme)) -> LdapUser:
 
 
 async def get_current_active_user(
-    current_user: LdapUser = Depends(get_current_user)
+    current_user: LdapUser = Depends(get_current_user),
 ) -> LdapUser:
     if current_user.disabled:
         raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail="Inactive user")
