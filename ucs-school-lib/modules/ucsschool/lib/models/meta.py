afc517512adc7560772692d97ef5a96205553922
Daniel Troeder <troeder@univention.de>
2019-11-29 17:01:00 +0100

    simplify metaclass

--- ucs-school-lib/modules/ucsschool/lib/models/meta.py
+++ ucs-school-lib/modules/ucsschool/lib/models/meta.py
@@ -29,12 +29,10 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <http://www.gnu.org/licenses/>.
 
-from functools import partial
 import inspect
 import logging
 import six
 import lazy_object_proxy
-import univention.admin.modules as udm_modules
 from .attributes import Attribute
 
 
@@ -50,40 +48,6 @@ class UCSSchoolHelperOptions(object):
 		if self.udm_module:
 			udm_module_short = self.udm_module.split('/')[1]
 		self.set_from_meta_object(meta, 'udm_module_short', udm_module_short)
-		# if self.udm_module:
-		# 	module = udm_modules.get(self.udm_module)
-		# 	if not module:
-		# 		# happens when the udm_module is not in the standard package
-		# 		#   i.e. computers/ucc
-		# 		return
-		# 	for key, attr in six.iteritems(klass._attributes):
-		# 		# sanity checks whether we specified everything correctly
-		# 		if attr.udm_name and not attr.extended:
-		# 			# extended? only available after module_init(lo)
-		# 			#   we have to trust ourselved here
-		# 			if attr.udm_name not in module.property_descriptions:
-		# 				raise RuntimeError('%s\'s attribute "%s" has no counterpart in the %s\'s property_descriptions ("%s")!' % (klass.__name__, key, self.udm_module, attr.udm_name))
-		# 	udm_name = klass._attributes['name'].udm_name
-		# 	# ldap_name = module.mapping.mapName(udm_name)
-		# 	# self.ldap_name_part = ldap_name
-		# 	# ldap_map_function = partial(module.mapping.mapValue, udm_name)
-		# 	# self.ldap_map_function = ldap_map_function
-		# 	# ldap_unmap_function = partial(module.mapping.unmapValue, module.mapping.mapName(udm_name))
-		# 	# self.ldap_unmap_function = ldap_unmap_function
-		# 	self.ldap_name_part = 'cn'
-		# 	self.ldap_map_function = lambda name: name
-		# 	self.ldap_unmap_function = lambda name: name
-		# else:
-		# 	# this is to not let models fail when accessing obj.dn
-		# 	#   note that without an udm_module it is not possible
-		# 	#   to save an object
-		# 	self.ldap_name_part = 'cn'
-		# 	self.ldap_map_function = lambda name: name
-		# 	self.ldap_unmap_function = lambda name: name
-		# self.ldap_name_part = 'cn'
-		self.ldap_map_function = lambda name: name
-		self.ldap_unmap_function = lambda name: name
-
 
 	def set_from_meta_object(self, meta, name, default):
 		setattr(self, name, getattr(meta, name, default))
@@ -107,5 +71,5 @@ class UCSSchoolHelperMetaClass(type):
 		cls = super(UCSSchoolHelperMetaClass, mcs).__new__(mcs, cls_name, bases, dict(attrs))
 		cls._attributes = attributes
 		cls._meta = UCSSchoolHelperOptions(cls, meta)
-		cls.logger = lazy_object_proxy.Proxy(lambda: logging.getLogger(inspect.getmodule(cls).__name__))  # type: logging.Logger
+		cls.logger: logging.Logger = lazy_object_proxy.Proxy(lambda: logging.getLogger(inspect.getmodule(cls).__name__))
 		return cls
