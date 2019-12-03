ac76b30698cd158017708fbb0c6e9d85c3e2b79d
Daniel Troeder <troeder@univention.de>
2019-12-03 19:37:25 +0100

    improve tests

--- kelvin-api/tests/test_ldap_access.py
+++ kelvin-api/tests/test_ldap_access.py
@@ -1,11 +1,60 @@
 import collections
+from unittest.mock import patch
 
 import pytest
 
+import ucsschool.kelvin.constants
 import ucsschool.kelvin.ldap_access
+import ucsschool.lib.models.utils
 from udm_rest_client import UDM
 
+must_run_in_container = pytest.mark.skipif(
+    not ucsschool.kelvin.constants.CN_ADMIN_PASSWORD_FILE.exists(),
+    reason="Must run inside Docker container started by appcenter.",
+)
 
+
+@pytest.mark.asyncio
+async def test_ldap_access_props(temp_file_func, random_name):
+    tmp_file1 = temp_file_func()
+    txt1 = random_name()
+    tmp_file1.write_text(txt1)
+    tmp_file2 = temp_file_func()
+    txt2 = random_name()
+    tmp_file2.write_text(txt2)
+    with patch.object(
+        ucsschool.kelvin.ldap_access, "CN_ADMIN_PASSWORD_FILE", tmp_file1
+    ), patch("ucsschool.kelvin.ldap_access.MACHINE_PASSWORD_FILE", tmp_file2), patch(
+        "ucsschool.kelvin.ldap_access.get_logger"
+    ), patch(
+        "ucsschool.kelvin.ldap_access._udm_kwargs", {}
+    ):
+        ldap_access = ucsschool.kelvin.ldap_access.LDAPAccess()
+        assert ldap_access.cn_admin == "cn=admin"
+        assert txt1 == await ldap_access.cn_admin_password
+        assert txt2 == await ldap_access.machine_password
+
+
+@pytest.mark.asyncio
+async def test_udm_kwargs_fake(temp_file_func, random_name):
+    tmp_file1 = temp_file_func()
+    txt1 = random_name()
+    tmp_file1.write_text(txt1)
+    tmp_file2 = temp_file_func()
+    with patch("ucsschool.kelvin.ldap_access.CN_ADMIN_PASSWORD_FILE", tmp_file1), patch(
+        "ucsschool.kelvin.ldap_access.MACHINE_PASSWORD_FILE", tmp_file2
+    ), patch("ucsschool.kelvin.ldap_access.get_logger"), patch(
+        "ucsschool.kelvin.ldap_access._udm_kwargs", {}
+    ):
+        udm_kwargs = await ucsschool.kelvin.ldap_access.udm_kwargs()
+    assert udm_kwargs["username"] == "cn=admin"
+    assert udm_kwargs["password"] == txt1
+    host = ucsschool.lib.models.utils.env_or_ucr("ldap/master")
+    assert udm_kwargs["url"] == f"https://{host}/univention/udm/"
+    assert udm_kwargs["ssl_ca_cert"] == ucsschool.kelvin.constants.UCS_SSL_CA_CERT
+
+
+@must_run_in_container
 @pytest.mark.asyncio
 async def test_get_user():
     username = "Administrator"
@@ -15,6 +64,7 @@ async def test_get_user():
     assert user_obj.dn == f"uid={username},cn=users,{ldap_access.ldap_base}"
 
 
+@must_run_in_container
 @pytest.mark.asyncio
 async def test_admin_group_members():
     username = "Administrator"
@@ -25,8 +75,9 @@ async def test_admin_group_members():
     assert administrator_dn in members
 
 
+@must_run_in_container
 @pytest.mark.asyncio
-async def test_udm_kwargs():
+async def test_udm_kwargs_real():
     ldap_access = ucsschool.kelvin.ldap_access.LDAPAccess()
     udm_kwargs = await ucsschool.kelvin.ldap_access.udm_kwargs()
     async with UDM(**udm_kwargs) as udm:
