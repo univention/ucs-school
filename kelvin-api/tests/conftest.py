db1fd301a75d8f2ad73a2623ad5445f1900a813b
Ole Schwiegert <schwiegert@univention.de>
2019-12-09 14:46:47 +0100

    Add tests for user resource create,search,get,delete

--- kelvin-api/tests/conftest.py
+++ kelvin-api/tests/conftest.py
@@ -2,15 +2,33 @@ import os
 import shutil
 from pathlib import Path
 from tempfile import mkdtemp, mkstemp
-from typing import Callable, List
+from typing import Callable, List, Dict
 from unittest.mock import patch
+import requests
 
 import pytest
 from faker import Faker
 
 import ucsschool.kelvin.utils
+from ucsschool.kelvin.routers.user import UserCreateModel
 
 faker = Faker()
+
+
+@pytest.fixture(scope="session")
+def url_fragment():
+    return f"http://{os.environ['DOCKER_HOST_NAME']}/kelvin/api/v1"
+
+
+@pytest.fixture(scope="session")
+def auth_header(url_fragment):
+    response_json = requests.post(
+        f"http://{os.environ['DOCKER_HOST_NAME']}/kelvin/api/token",
+        headers={"Content-Type": "application/x-www-form-urlencoded"},
+        data=dict(username="Administrator", password="univention"),
+    ).json()
+    auth_header = {"Authorization": f"Bearer {response_json['access_token']}"}
+    return auth_header
 
 
 @pytest.fixture
@@ -101,3 +119,62 @@ def setup_logging(temp_dir_session):
 @pytest.fixture
 def random_name() -> Callable[[], str]:
     return faker.first_name
+
+
+@pytest.fixture
+def create_random_user_data(
+    url_fragment,
+):  # TODO: Extend with schools and school classes if ressources are done
+    def _create_random_user_data(role: str):
+        f_name = faker.first_name()
+        l_name = faker.last_name()
+        name = f"{f_name}-{l_name}"
+        data = dict(
+            email="",
+            record_uid=name,
+            source_uid="KELVIN",
+            birthday=faker.date(),
+            disabled=False,
+            name=name,
+            firstname=f_name,
+            lastname=l_name,
+            udm_properties={},
+            school=f"{url_fragment}/schools/DEMOSCHOOL",
+            schools=[f"{url_fragment}/schools/DEMOSCHOOL"],
+            school_classes={"DEMOSCHOOL": ["DEMOSCHOOL-Democlass"]},
+            role=f"{url_fragment}/roles/{role}",
+        )
+        return UserCreateModel(**data)
+
+    return _create_random_user_data
+
+
+@pytest.fixture
+def create_random_users(
+    create_random_user_data, url_fragment, auth_header
+):  # TODO: Extend with schools and school_classes if resources are done
+    usernames = list()
+
+    def _create_random_users(roles: Dict[str, int]) -> Dict[str, List[str]]:
+        users = {}
+        for role, amount in roles.items():
+            users[role] = []
+            for i in range(amount):
+                user_data = create_random_user_data(role)
+                response = requests.post(
+                    f"{url_fragment}/users/",
+                    headers={"Content-Type": "application/json", **auth_header},
+                    data=user_data.json(),
+                )
+                assert response.status_code == 201
+                users[role].append(user_data.name)
+                usernames.append(user_data.name)
+        return users
+
+    yield _create_random_users
+
+    for username in usernames:
+        response = requests.delete(
+            f"{url_fragment}/users/{username}", headers=auth_header
+        )
+        assert response.status_code == 204
