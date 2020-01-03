b8724336f605558e8dc9bcab4281d713e189c41e
Daniel Troeder <troeder@univention.de>
2020-01-03 12:28:08 +0100

    Bug #50677: resolve DNs to names in school resource

--- kelvin-api/ucsschool/kelvin/routers/school.py
+++ kelvin-api/ucsschool/kelvin/routers/school.py
@@ -1,12 +1,12 @@
 import logging
 from functools import lru_cache
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
 import ujson
-from fastapi import APIRouter, Depends, HTTPException, Query
+from fastapi import APIRouter, Depends, Query
 from ldap.filter import escape_filter_chars
 from starlette.requests import Request
-from starlette.status import HTTP_201_CREATED, HTTP_404_NOT_FOUND
+from starlette.status import HTTP_201_CREATED
 
 from ucsschool.lib.models.school import School
 from udm_rest_client import UDM
@@ -21,8 +21,7 @@ def get_logger() -> logging.Logger:
     return logging.getLogger(__name__)
 
 
-class SchoolModel(BaseModel):
-    dn: str = None
+class SchoolModel(APIAttributesMixin, LibModelHelperMixin):
     name: str
     display_name: str = None
     administrative_servers: List[str] = []
@@ -31,23 +30,62 @@ class SchoolModel(BaseModel):
     dc_name_administrative: str = None
     educational_servers: List[str] = []
     home_share_file_server: str = None
-    ucsschool_roles: List[str] = []
 
-    class Config:
+    _dn2name: Dict[str, str] = {}
+
+    class Config(LibModelHelperMixin.Config):
         json_loads = ujson.loads
+
+    @classmethod
+    async def _from_lib_model_kwargs(
+        cls, obj: School, request: Request, udm: UDM
+    ) -> Dict[str, Any]:
+        kwargs = await super()._from_lib_model_kwargs(obj, request, udm)
+        kwargs["url"] = cls.scheme_and_quote(
+            request.url_for("get", school_name=kwargs["name"])
+        )
+        kwargs["administrative_servers"] = [
+            await cls.computer_dn2name(udm, dn) for dn in obj.administrative_servers
+        ]
+        kwargs["class_share_file_server"] = await cls.computer_dn2name(
+            udm, obj.class_share_file_server
+        )
+        kwargs["educational_servers"] = [
+            await cls.computer_dn2name(udm, dn) for dn in obj.educational_servers
+        ]
+        kwargs["home_share_file_server"] = await cls.computer_dn2name(
+            udm, obj.home_share_file_server
+        )
+        return kwargs
+
+    @classmethod
+    async def computer_dn2name(cls, udm: UDM, dn: str) -> Optional[str]:
+        if not dn:
+            return None
+        if dn not in cls._dn2name:
+            obj = await udm.obj_by_dn(dn)
+            cls._dn2name[dn] = obj.props.name
+        return cls._dn2name[dn]
 
 
 @router.get("/", response_model=List[SchoolModel])
 async def search(
+    request: Request,
     name_filter: str = Query(
         None,
         alias="name",
-        description="List schools with this name. '*' can be used for an inexact search.",
+        description="List schools with this name. '*' can be used for an inexact search. (optional)",
         title="name",
     ),
     logger: logging.Logger = Depends(get_logger),
     udm: UDM = Depends(udm_ctx),
 ) -> List[SchoolModel]:
+    """
+    Search for schools (OUs).
+
+    The **name** parameter is optional and supports the use of ``*`` for wildcard
+    searches. No other properties can be used to filter.
+    """
     logger.debug("Searching for schools with: name_filter=%r", name_filter)
     if name_filter:
         filter_str = "ou={}".format(
@@ -55,30 +93,32 @@ async def search(
         )
     else:
         filter_str = None
-    schools = [
-        school.to_dict() for school in await School.get_all(udm, filter_str=filter_str)
+    schools = await School.get_all(udm, filter_str=filter_str)
+    return [
+        await SchoolModel.from_lib_model(school, request, udm) for school in schools
     ]
-    for school in schools:
-        school["dn"] = school.pop("$dn$")
-    return [SchoolModel(**school) for school in schools]
 
 
 @router.get("/{school_name}", response_model=SchoolModel)
 async def get(
+    request: Request,
     school_name: str = Query(
         None, alias="name", description="School (OU) with this name.", title="name",
     ),
     udm: UDM = Depends(udm_ctx),
 ) -> SchoolModel:
-    school_udm = await School(name=school_name).get_udm_object(udm)
-    if not school_udm:
-        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="School not found.")
-    school = await School.from_udm_obj(school_udm, school_name, udm)
-    school_dict = school.to_dict()
-    school_dict["dn"] = school_dict.pop("$dn$")
-    return SchoolModel(**school_dict)
+    """
+    Fetch a specific school (OU).
+
+    - **name**: name of the school (required)
+    """
+    school = await School.from_dn(School(name=school_name).dn, None, udm)
+    return await SchoolModel.from_lib_model(school, request, udm)
 
 
 @router.post("/", status_code=HTTP_201_CREATED, response_model=SchoolModel)
 async def create(school: SchoolModel) -> SchoolModel:
+    """
+    **Not implemented yet!**
+    """
     raise NotImplementedError
