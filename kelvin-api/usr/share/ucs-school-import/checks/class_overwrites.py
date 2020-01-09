eb78a5b3f3f4292f851aa228970be126da0a98cb
Ole Schwiegert <schwiegert@univention.de>
2020-01-09 10:46:27 +0100

    Bug #50685: Fix error in check

--- kelvin-api/usr/share/ucs-school-import/checks/class_overwrites.py
+++ kelvin-api/usr/share/ucs-school-import/checks/class_overwrites.py
@@ -39,18 +39,20 @@ class ClassOverwriteConfigurationCheck(ConfigurationChecks):
         classes = self.config.get("classes", {})
         for k, v in classes.items():
             self.logger.info(f"Checking class override for {k}: {v}")
+            if v == "":  # Empty module names are being ignored by the Import Framework
+                continue
             module_name, class_name = v.rsplit(".", 1)
             try:
                 imported_module = importlib.import_module(module_name)
             except ModuleNotFoundError:
                 raise InitialisationError(
                     f"Overwriting the class for {k} is not possible, because the module {module_name} "
-                    f"could not be found. You might deactivating the override for Kelvin by setting the "
-                    f"class in the kelvin.json to ''."
+                    f"could not be found. You might deactivating the override for Kelvin API by setting"
+                    f" the class in the kelvin.json to ''."
                 )
             if not inspect.isclass(getattr(imported_module, class_name)):
                 raise InitialisationError(
                     f"Overwriting the class for {k} is not possible, because the class {class_name} "
                     f"could not be found in module {module_name}. You might deactivating the override "
-                    f"for Kelvin by setting the class in the kelvin.json to ''."
+                    f"for Kelvin API by setting the class in the kelvin.json to ''."
                 )
