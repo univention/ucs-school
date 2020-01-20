6b49e1855209f3ea0f2bfc07e17fd0f1d41fc157
Tobias Wenzel <wenzel@univention.de>
2020-01-20 15:08:02 +0100

    Bug #50678: Check school attribute vs. multi-value ldap object.

--- kelvin-api/tests/test_route_user.py
+++ kelvin-api/tests/test_route_user.py
@@ -123,10 +123,8 @@ async def compare_lib_api_user(  # noqa: C901
 
 def compare_ldap_json_obj(dn, json_resp, url_fragment):  # noqa: C901
     import univention.admin.uldap
-
     lo, pos = univention.admin.uldap.getAdminConnection()
     ldap_obj = lo.get(dn)
-    # assert True is False
     for attr, value in json_resp.items():
         if attr == "record_uid" and "ucsschoolRecordUID" in ldap_obj:
             assert value == ldap_obj["ucsschoolRecordUID"][0].decode("utf-8")
@@ -143,9 +141,7 @@ def compare_ldap_json_obj(dn, json_resp, url_fragment):  # noqa: C901
         elif attr == "lastname" and "sn" in ldap_obj:
             assert value == ldap_obj["sn"][0].decode("utf-8")
         elif attr == "school" and "ucsschoolSchool" in ldap_obj:
-            assert value.split("/")[-1] == ldap_obj["ucsschoolSchool"][0].decode(
-                "utf-8"
-            )
+            assert value.split("/")[-1] in [s.decode('utf-8') for s in ldap_obj["ucsschoolSchool"]]
         elif attr == "udm_properties":
             for k, v in json_resp["udm_properties"].items():
                 if k == "organisation" and "o" in ldap_obj:
