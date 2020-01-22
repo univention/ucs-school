36b3f05e24c0d112b69069a29468a6037b37ddb5
Daniel Troeder <troeder@univention.de>
2020-01-22 12:07:10 +0100

    Bug #50721: add kelvin logo

--- doc/kelvin/docs/conf.py
+++ doc/kelvin/docs/conf.py
@@ -87,7 +87,7 @@ html_use_index = False
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {"logo": "kelvin_rest_api_logo.svg"}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
