c147faec7352feab4c895a8d7414fa5b0edfbead
Tobias Wenzel <wenzel@univention.de>
2020-05-08 12:28:56 +0200

    Bug #42182: Add very simple test.

--- ucs-test-ucsschool/90_ucsschool/133_check_share_permissions.py
+++ ucs-test-ucsschool/90_ucsschool/133_check_share_permissions.py
@@ -1,9 +1,9 @@
 #!/usr/share/ucs-test/runner python
-## desc: Test if share-access is not full control for class-users.
+## desc: Test if share-access don't leave permission change open for class members.
 ## roles: [domaincontroller_master]
 ## tags: [apptest,ucsschool,ucsschool_base1]
 ## exposure: dangerous
-## packages: [ucs-school-lib]
+## packages: []
 ## bugs: [42182]
 
 import re
@@ -21,19 +21,18 @@ def main():
 				if share.name in ["Marktplatz", "iTALC-Installation"]:
 					print("*** Ignoring //{}/{} (Bug #42514)".format(school.name, share.name))
 				else:
-					# better test if permissions can be altered
-					# -> how?
+					# better test -> if permissions can be altered
 					proc = subprocess.Popen(['samba-tool', 'ntacl', 'get', '--as-sddl', share_udm["path"]], stdout=subprocess.PIPE)
 					stdout, stderr = proc.communicate()
 					sid_map = [
 						('S-1-1-0', 'everyone'),
 						('S-1-5-21', 'group'),
-						('CG', 'creator group'), # this might be ok.
-						('CO', 'creator user'), # this might be ok.
 					]
 					for sid, name in sid_map:
-						if re.match(r'.*?\(A.+?0x001f01ff[^\)]+?{}.*?\).*?'.format(sid), stdout):
-							utils.fail("Shares {} has full access for {}".format(share_udm["path"], name))
+						if re.match(r'.*?(A.+?0x001f01ff[^)]+?{}.*?).*'.format(sid), stdout):
+							# Full control is ok, if it is stripped by the permission to change permissions.
+							if not re.match(r'.*?(D.+?0x00140000[^)]+?{}.*?).*'.format(sid), stdout):
+								utils.fail("The permissions of share {} can be changed.".format(share_udm["path"], name))
 
 
 if __name__ == '__main__':
