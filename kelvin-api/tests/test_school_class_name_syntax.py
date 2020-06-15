5df7738f7e375f53331dd74fe3c113d77c96a0dc
Daniel Troeder <troeder@univention.de>
2020-06-15 08:07:05 +0200

    Bug #51363: fix formatting

--- kelvin-api/tests/test_school_class_name_syntax.py
+++ kelvin-api/tests/test_school_class_name_syntax.py
@@ -25,17 +25,18 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <http://www.gnu.org/licenses/>.
 
-from typing import List
 import random
 import string
+from typing import List
+
 import pytest
 import requests
 from pydantic import ValidationError
 
-from udm_rest_client import UDM
-from ucsschool.lib.models.utils import ucr
-from udm_rest_client.exceptions import CreateError
 from ucsschool.kelvin.routers.school_class import SchoolClass
+from ucsschool.lib.models.utils import ucr
+from udm_rest_client import UDM
+from udm_rest_client.exceptions import CreateError
 
 lower_case_chars = f"{string.ascii_lowercase}"
 digits = "".join(str(i) for i in range(10))
@@ -44,16 +45,18 @@ digits = "".join(str(i) for i in range(10))
 def random_names(name_lengths: list, chars: str) -> List:
     names = []
     for n in name_lengths:
-        names.append(''.join(random.choice(chars) for _ in range(n)))
+        names.append("".join(random.choice(chars) for _ in range(n)))
     return names
 
 
-@pytest.mark.parametrize("name", random_names(random.choices(range(1, 33), k=100),
-                                              f"{lower_case_chars}{digits}"))
+@pytest.mark.parametrize(
+    "name",
+    random_names(random.choices(range(1, 33), k=100), f"{lower_case_chars}{digits}"),
+)
 @pytest.mark.asyncio
 async def test_schoolclass_module(name):
     try:
-        SchoolClass(name=name, school='DEMOSCHOOL')
+        SchoolClass(name=name, school="DEMOSCHOOL")
     except ValidationError as e:
         raise e
 
@@ -65,20 +68,20 @@ async def test_check_class_name(auth_header, url_fragment, udm_kwargs):
         "school": school_name,
     }
     names = []
-    names.append('1a')
-    names.append('1-a')
-    name_lengths = random.sample(range(1, 33), 3) + [1]*3
+    names.append("1a")
+    names.append("1-a")
+    name_lengths = random.sample(range(1, 33), 3) + [1] * 3
     names.extend(random_names(name_lengths, lower_case_chars))
     names.extend(random_names(name_lengths, digits))
     class_names = names
     names = [f"DEMOSCHOOL-{name}" for name in names]
 
     async with UDM(**udm_kwargs) as udm:
-        group_mod = udm.get('groups/group')
+        group_mod = udm.get("groups/group")
         for name in names:
             obj = await group_mod.new()
             obj.props.name = name
-            obj.props.ucsschoolRole = [f'school_class:school:{school_name}']
+            obj.props.ucsschoolRole = [f"school_class:school:{school_name}"]
             obj.props.school = [school_name]
             obj.position = f"cn=klassen,cn=schueler,cn=groups,ou={school_name},{ucr.get('ldap/base')}"
             try:
@@ -95,8 +98,5 @@ async def test_check_class_name(auth_header, url_fragment, udm_kwargs):
         assert response.status_code == 200
         # make sure all classes were created.
         expected = set(class_names)
-        received = set([r['name'] for r in json_resp if r['name'] in class_names])
+        received = set([r["name"] for r in json_resp if r["name"] in class_names])
         assert expected == received
-
-
-
