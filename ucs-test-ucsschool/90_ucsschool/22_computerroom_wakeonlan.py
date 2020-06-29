62e458f3b33c64ba58e115fee4e7aba437345f7c
Tobias Wenzel <wenzel@univention.de>
2020-06-29 15:50:18 +0200

    Bug #51074: ucs-test retry before giving up

--- ucs-test-ucsschool/90_ucsschool/22_computerroom_wakeonlan.py
+++ ucs-test-ucsschool/90_ucsschool/22_computerroom_wakeonlan.py
@@ -21,64 +21,83 @@ from univention.testing.ucsschool.computerroom import UmcComputer
 def main():
     logger = utu.get_ucsschool_logger()
     target_broadcast_ips = ["255.255.255.255", "10.200.47.254"]
-    t_shark_timeout = 10
-    t_shark_duration = t_shark_timeout * 5
+    max_iterations = 10
+    t_shark_duration = max_iterations * 5
+    hostname = socket.gethostname()
+    server_ip = socket.gethostbyname(hostname)
+    proc = subprocess.Popen(
+        [
+            "tshark",
+            "-i",
+            "any",
+            "-a",
+            "duration:{}".format(t_shark_duration),
+            "src",
+            "host",
+            server_ip,
+        ],
+        stdout=subprocess.PIPE,
+        close_fds=True,
+    )
+    time.sleep(1)
     with utu.UCSTestSchool() as schoolenv, ucr_test.UCSTestConfigRegistry() as ucr:
         school, _ = schoolenv.create_ou(name_edudc=ucr.get("hostname"))
         computer = UmcComputer(school, "windows")
         computer.create()
         mac_address = computer.mac_address
-        hostname = socket.gethostname()
-        server_ip = socket.gethostbyname(hostname)
-
-        proc = subprocess.Popen(
-            [
-                "tshark",
-                "-i",
-                "any",
-                "-a",
-                "duration:{}".format(t_shark_duration),
-                "src",
-                "host",
-                server_ip,
-            ],
-            stdout=subprocess.PIPE,
-            close_fds=True,
-        )
-        logger.info("Wait {}s for tshark to get ready (...)".format(t_shark_timeout))
-        time.sleep(t_shark_timeout)
-        logger.info(
-            "Send WoL signals to {} to broadcast-ips {}".format(mac_address, target_broadcast_ips)
-        )
-        try:
-            computerroom.wakeonlan.send_wol_packet(
-                mac_address, target_broadcast_ips=target_broadcast_ips
+        for i in range(max_iterations):
+            wol_received = [True for _ in range(len(target_broadcast_ips))]
+            logger.info(
+                "Send WoL signals to {} to broadcast-ips {}".format(
+                    mac_address, target_broadcast_ips
+                )
             )
-        except socket.error:
-            # Non-existing ips raise errors,
-            # thus they have to be put last in the list.
-            # A more extensive test would have multiple machines with
-            # different broadcast-ips. We decided this would produce too much overhead.
-            pass
-        stdout, stderr = proc.communicate()
-
-        # I simply check if a WoL signal was sent. Alternatively, we
-        # can search for the expected payload. tshark must then have the argument -x.
-        # addr = re.sub(r'[.:-]', '', mac_address)
-        # payload_hex = 'FFFFFFFFFFFF' + (addr * 16).encode()
-        # payload = bytes(bytearray.fromhex(payload_hex))
-        assert computer.mac_address in stdout
-        for b_ip in target_broadcast_ips:
-            regex = r".*{}.+?{} WOL \d+ MagicPacket for {}.*".format(server_ip, b_ip, mac_address)
-            successful_send = re.match(regex, str(stdout), re.DOTALL)
-            if successful_send:
-                logger.info("Packages were successfully sent to {}".format(b_ip))
-            elif b_ip in str(stdout):
-                logger.info("Could not send WoL signal to {}".format(b_ip))
-                logger.info("This is the expected behaviour, since it is not reachable.")
-            else:
-                logger.info("tshark output was\n{}".format(stdout))
-                utils.fail("Did not succeed to send WoL to {}".format(b_ip))
+            try:
+                computerroom.wakeonlan.send_wol_packet(
+                    mac_address, target_broadcast_ips=target_broadcast_ips
+                )
+            except socket.error:
+                # Non-existing ips raise errors,
+                # thus they have to be put last in the list.
+                # A more extensive test would have multiple machines with
+                # different broadcast-ips. We decided this would produce too much overhead.
+                pass
+            stdout, stderr = proc.communicate()
+            # I simply check if a WoL signal was sent. Alternatively, we
+            # can search for the expected payload. tshark must then have the argument -x.
+            # addr = re.sub(r'[.:-]', '', mac_address)
+            # payload_hex = 'FFFFFFFFFFFF' + (addr * 16).encode()
+            # payload = bytes(bytearray.fromhex(payload_hex))
+            assert computer.mac_address in stdout
+            for j, b_ip in enumerate(target_broadcast_ips):
+                regex = r".*{}.+?{} WOL \d+ MagicPacket for {}.*".format(
+                    server_ip, b_ip, mac_address
+                )
+                successful_send = re.match(regex, str(stdout), re.DOTALL)
+                if successful_send:
+                    logger.info("Packages were successfully sent to {}".format(b_ip))
+                elif b_ip in str(stdout):
+                    logger.info("Could not send WoL signal to {}".format(b_ip))
+                    logger.info(
+                        "This is the expected behaviour, since it is not reachable."
+                    )
+                else:
+                    wol_received[j] = False
+                    logger.info("tshark output was\n{}".format(stdout))
+                    logger.info(
+                        "Did not succeed to send WoL to {} in iteration {}/{}".format(
+                            b_ip, j, max_iterations
+                        )
+                    )
+                    break
+            if all(wol_received):
+                break
+        if not all(wol_received):
+            utils.fail(
+                "Did not succeed to send WoL all of the following addresses {}".format(
+                    target_broadcast_ips
+                )
+            )
 
 
 if __name__ == "__main__":
