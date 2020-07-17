f62a1b8ba91f3750b72483623e87d9f83048e37c
Daniel Troeder <troeder@univention.de>
2020-07-17 16:21:22 +0200

    Bug #51606: test Newton client server name not matching hosts fqdn regarding case

---
+++ ucs-test-ucsschool/90_ucsschool/conftest.py
@@ -0,0 +1,39 @@
+import os
+
+import pytest
+
+import univention.testing.ucr as ucr_test
+
+
+@pytest.fixture(scope="session")
+def ucr():
+    """Instance of UCSTestConfigRegistry"""
+    with ucr_test.UCSTestConfigRegistry() as ucr:
+        yield ucr
+
+
+@pytest.fixture(scope="session")
+def fqdn(ucr):
+    """hostname.domainname"""
+    ret = os.environ.get("UCS_TEST_HOSTNAME")
+    return ret or "{hostname}.{domainname}".format(**ucr)
+
+
+@pytest.fixture(scope="session")
+def admin_username(ucr):
+    """Username of the Admin account"""
+    ret = os.environ.get("UCS_TEST_ADMIN_USERNAME")
+    if not ret:
+        ret = ucr.get("tests/domainadmin/account")
+        if ret:
+            ret = ret.split(",")[0].split("=")[-1]
+        else:
+            ret = "Administrator"
+    return ret
+
+
+@pytest.fixture(scope="session")
+def admin_password(ucr):
+    """Password of the Admin account"""
+    ret = os.environ.get("UCS_TEST_ADMIN_PASSWORD")
+    return ret or ucr.get("tests/domainadmin/pwd", "univention")
