bb02c3afdf286a4bc48c937903a7790097863379
Tobias Wenzel <wenzel@univention.de>
2020-09-10 14:51:36 +0200

    Bug #42182: add class to set custom nt acls on shares

--- ucs-school-lib/python/models/share.py
+++ ucs-school-lib/python/models/share.py
@@ -40,13 +40,125 @@ from univention.udm import UDM
 
 from ..roles import role_marketplace_share, role_school_class_share, role_workgroup_share
 from .attributes import Roles, SchoolClassAttribute, ShareName, WorkgroupAttribute
-from .base import RoleSupportMixin, UCSSchoolHelperAbstractClass
+from .base import RoleSupportMixin, UCSSchoolHelperAbstractClass, WrongObjectType
 from .utils import _, ucr
 
 try:
-    from typing import Any, Optional
+    from typing import Any, Optional, List
+    from .base import LoType, UdmObject
 except ImportError:
     pass
+
+
+class NoSID(Exception):
+    pass
+
+
+class NoSchoolGroup(Exception):
+    pass
+
+
+class SetNTACLsMixin(object):
+    """
+    Mixin for setting NTACLs of UCS@school Share (sub)classes. For example to
+    to prevent students from changing the permissions in a share (Bug #42182).
+
+    D ~ deny, OI/ OBJECT_INHERIT_ACE ~ Object inheritance, CI/ CONTAINER_INHERIT_ACE ~ container inheritance
+    RC/ READ_CONTROL ~ display security attributes WO/ WRITE_OWNER ~ take ownership
+    WD/ WRITE_DAC ~ write security permissions
+    To make sure, students can edit folders&files in subfolders, they need to inherit edit
+    or full control, since they are denied first.
+    For a complete overview of all options, see https://docs.microsoft.com/en-us/windows/win32/secauthz/ace-strings
+    """
+
+    def get_nt_acls(self, lo):  # type: (LoType) -> List[str]
+        return []
+
+    @staticmethod
+    def get_groups_samba_sid(lo, dn):  # type: (LoType, str) -> str
+        try:
+            return lo.get(dn)["sambaSID"][0]
+        except (IndexError, KeyError):
+            raise NoSID("Group {!r} has no/empty 'sambaSID' attribute.".format(dn))
+
+    def get_ou_admin_full_control(self, lo):  # type: (LoType) -> List[str]
+        admin_dn = "cn=admins-{},cn=ouadmins,cn=groups,{}".format(
+            self.school.lower(), ucr.get("ldap/base")
+        )
+        samba_sid = self.get_groups_samba_sid(lo, admin_dn)
+        return ["(A;OICI;0x001f01ff;;;{})".format(samba_sid)]
+
+    def get_aces_deny_students_change_permissions(self, lo):  # type: (LoType) -> List[str]
+        """
+        Get the schueler-ou sid to deny all students the
+        permissions to modify permissions and take ownership.
+        Derived classes may add more NTACLS.
+
+        Sets NT ACLs to disallow students to deny students to change the permission of
+        folders, subfolder and files or to take ownership of them as well as
+        displaying them (RC).
+        """
+        search_base = self.get_search_base(self.school)
+        student_group_dn = "cn={}{},cn=groups,{}".format(
+            search_base.group_prefix_students, self.school, search_base.schoolDN
+        )
+        samba_sid = self.get_groups_samba_sid(lo, student_group_dn)
+        return ["(D;OICI;WOWD;;;{})".format(samba_sid)]
+
+    def get_aces_work_group(self, lo):  # type: (LoType) -> List[str]
+        """
+            ACE: deny schueler to change permissions & take ownership
+            ACE: allow workgroup-members to read/write/modify
+            ACE: allow ou-admins full control
+        """
+        res = self.get_aces_deny_students_change_permissions(lo)
+        if self.school_group:
+            group_dn = self.school_group.dn
+        else:
+            raise NoSchoolGroup("No schoolgroup set.")
+        samba_sid = self.get_groups_samba_sid(lo, group_dn)
+        res.append("(A;OICI;0x001f01ff;;;{})".format(samba_sid))
+        res.extend(self.get_ou_admin_full_control(lo))
+        return res
+
+    def get_aces_market_place(self, lo):  # type: (LoType) -> List[str]
+        """
+            ACE: deny schueler to change permissions & take ownership
+            ACE: allow Domain Users to read/write/modify
+            ACE: allow ou-admins full control
+        """
+        res = self.get_aces_deny_students_change_permissions(lo)
+        search_base = self.get_search_base(self.school)
+        domain_users_dn = "cn=Domain Users %s,%s" % (self.school.lower(), search_base.groups)
+        samba_sid = self.get_groups_samba_sid(lo, domain_users_dn)
+        res.append("(A;OICI;0x001f01ff;;;{})".format(samba_sid))
+        res.extend(self.get_ou_admin_full_control(lo))
+        return res
+
+    def get_aces_class_group(self, lo):  # type: (LoType) -> List[str]
+        """
+            ACE: deny schueler to change permissions & take ownership
+            ACE: allow class-members to read/write/modify
+            ACE: allow ou-admins full control
+        """
+        res = self.get_aces_deny_students_change_permissions(lo)
+        if self.school_group:
+            group_dn = self.school_group.dn
+        else:
+            raise NoSchoolGroup("No schoolgroup set.")
+        samba_sid = self.get_groups_samba_sid(lo, group_dn)
+        res.append("(A;OICI;0x001f01ff;;;{})".format(samba_sid))
+        res.extend(self.get_ou_admin_full_control(lo))
+        return res
+
+    def set_nt_acls(self, udm_obj, lo):  # type: (UdmObject, LoType) -> None
+        try:
+            udm_obj["appendACL"] = self.get_nt_acls(lo)
+        except NoSID as exc:
+            self.logger.warning("Not setting NTACLs for %s: %s", self.__class__.__name__, exc)
+            return
+        udm_obj["sambaInheritOwner"] = "1"
+        udm_obj["sambaInheritPermissions"] = "1"
 
 
 class Share(UCSSchoolHelperAbstractClass):
@@ -138,7 +250,7 @@ class Share(UCSSchoolHelperAbstractClass):
             alternative_server_dn = ou_attr_ldap_access_write["univentionLDAPAccessWrite"][0]
             if len(ou_attr_ldap_access_write) > 1:
                 self.logger.warning(
-                    "more than one corresponding univentionLDAPAccessWrite found at ou=%s", self.school
+                    "more than one corresponding univentionLDAPAccessWrite found at ou=%s", self.school,
                 )
 
         # build fqdn of alternative server and set serverfqdn
@@ -157,11 +269,15 @@ class Share(UCSSchoolHelperAbstractClass):
         udm_module = "shares/share"
 
 
-class GroupShare(Share):
+class GroupShare(SetNTACLsMixin, Share):
     school_group = SchoolClassAttribute(_("School class"), required=True, internal=True)
 
     @classmethod
     def from_school_group(cls, school_group):
+        from .group import Group  # isort:skip  # prevent cyclic import
+
+        if not isinstance(school_group, Group):
+            raise WrongObjectType(dn=getattr(school_group, "dn", "<no 'dn' attribute>"), cls=Group)
         return cls(name=school_group.name, school=school_group.school, school_group=school_group)
 
     from_school_class = from_school_group  # legacy
@@ -172,6 +288,10 @@ class GroupShare(Share):
     def get_share_path(self, school=None):
         school = school or self.school_group.school
         return super(GroupShare, self).get_share_path(school)
+
+    def do_create(self, udm_obj, lo):
+        self.set_nt_acls(udm_obj, lo)
+        return super(GroupShare, self).do_create(udm_obj, lo)
 
 
 class WorkGroupShare(RoleSupportMixin, GroupShare):
@@ -207,6 +327,9 @@ class WorkGroupShare(RoleSupportMixin, GroupShare):
                 filtered_shares.append(share)
         return filtered_shares
 
+    def get_nt_acls(self, lo):  # type: (LoType) -> List[str]
+        return self.get_aces_work_group(lo)
+
 
 class ClassShare(RoleSupportMixin, GroupShare):
     school_group = SchoolClassAttribute(_("School class"), required=True, internal=True)
@@ -229,8 +352,11 @@ class ClassShare(RoleSupportMixin, GroupShare):
 
         return ClassShare
 
+    def get_nt_acls(self, lo):  # type: (LoType) -> List[str]
+        return self.get_aces_class_group(lo)
 
-class MarketplaceShare(RoleSupportMixin, Share):
+
+class MarketplaceShare(RoleSupportMixin, SetNTACLsMixin, Share):
     ucsschool_roles = Roles(_("Roles"), aka=["Roles"])
     default_roles = [role_marketplace_share]
     _school_in_name_prefix = False
@@ -273,4 +399,11 @@ class MarketplaceShare(RoleSupportMixin, Share):
         self.create_defaults.pop("sambaForceGroup", None)
         self.create_defaults.pop("sambaCreateMode", None)
         self.create_defaults.pop("sambaDirectoryMode", None)
+        self.set_nt_acls(udm_obj, lo)
         return super(MarketplaceShare, self).do_create(udm_obj, lo)
+
+    def get_nt_acls(self, lo):  # type: (LoType) -> List[str]
+        return self.get_aces_market_place(lo)
+
+    class Meta(Share.Meta):
+        udm_filter = "(&(univentionObjectType=shares/share)(cn=Marktplatz))"
