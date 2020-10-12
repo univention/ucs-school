5a8f717992c0c2fc5851c26d1347ff70a6166798
Ole Schwiegert <schwiegert@univention.de>
2020-10-12 11:37:32 +0200

    Bug #52204: Implement auth handling in veyon lib

--- ucs-school-veyon-client/veyon_client/utils.py
+++ ucs-school-veyon-client/veyon_client/utils.py
@@ -30,45 +30,19 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <http://www.gnu.org/licenses/>.
 
-import enum as enum
-from collections import namedtuple
+import sys
+
+import six
+
+from .models import VeyonError
 
 
-class VeyonError(Exception):
-    def __init__(self, message, code):
-        super(VeyonError, self).__init__(message)
-        self.code = code
-
-
-class ScreenshotFormat(enum.Enum):
-    PNG = "png"
-    JPEG = "jpeg"
-
-    def __str__(self):
-        return str(self.value)
-
-
-class AuthenticationMethod(enum.Enum):
-    AUTH_KEYS = "0c69b301-81b4-42d6-8fae-128cdd113314"
-    AUTH_LDAP = "6f0a491e-c1c6-4338-8244-f823b0bf8670"
-    AUTH_LOGON = "63611f7c-b457-42c7-832e-67d0f9281085"
-    AUTH_SIMPLE = "73430b14-ef69-4c75-a145-ba635d1cc676"
-
-    def __str__(self):
-        return str(self.value)
-
-
-class Feature(enum.Enum):
-    SCREEN_LOCK = "ccb535a2-1d24-4cc1-a709-8b47d2b2ac79"
-    INPUT_DEVICE_LOCK = "e4a77879-e544-4fec-bc18-e534f33b934c"
-    USER_LOGOFF = "7311d43d-ab53-439e-a03a-8cb25f7ed526"
-    REBOOT = "4f7d98f0-395a-4fff-b968-e49b8d0f748c"
-    POWER_DOWN = "6f5a27a0-0e2f-496e-afcc-7aae62eede10"
-
-    def __str__(self):
-        return str(self.value)
-
-
-VeyonUser = namedtuple("VeyonUser", ["login", "fullname", "session"])
-VeyonSession = namedtuple("VeyonSession", ["connection_uid", "valid_until"])
-Dimension = namedtuple("Dimension", ["width", "height"])
+def check_veyon_error(response):  # type: (requests.Response) -> None
+    if response.status_code == 200:
+        return
+    data = response.json()
+    error = data.get("error", {})
+    error_code = error.get("code", -1)
+    error_message = error.get("message", "")
+    if error_code != 0:
+        raise VeyonError(error_message, error_code)
