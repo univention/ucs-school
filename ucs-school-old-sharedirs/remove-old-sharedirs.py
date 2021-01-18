b6c7c6a71be57b34b7cb502fbaad2bc66de26ba2
Florian Best <best@univention.de>
2021-01-18 19:39:37 +0100

    Bug #52611: Migrate remove-old-sharedirs.py listener to Python 3

--- ucs-school-old-sharedirs/remove-old-sharedirs.py
+++ ucs-school-old-sharedirs/remove-old-sharedirs.py
@@ -29,19 +29,25 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <http://www.gnu.org/licenses/>.
 
-__package__ = ""  # workaround for PEP 366
+from __future__ import absolute_import
+
 import os
+import pipes
 import shutil
 import time
 
-import commands
-import listener
+try:
+    from subprocess import getstatusoutput
+except ImportError:
+    from commands import getstatusoutput
 
-import univention.debug
+from listener import configRegistry, setuid, unsetuid
 
-hostname = listener.configRegistry["hostname"]
-domainname = listener.configRegistry["domainname"]
-ip = listener.configRegistry["interfaces/eth0/address"]
+import univention.debug as ud
+
+hostname = configRegistry["hostname"]
+domainname = configRegistry["domainname"]
+ip = configRegistry["interfaces/eth0/address"]  # FIXME: ...
 name = "remove-old-sharedirs"
 description = "moves directories of removed group shares to backup folder"
 filter = "(&(objectClass=univentionShare)(|(univentionShareHost=%s.%s)(univentionShareHost=%s)))" % (
@@ -55,76 +61,71 @@ modrdn = "1"
 DEFAUL_FS = "ext2/ext3:ext2:ext3:ext4:xfs:btrfs"
 TARGET_BLACKLIST = "/:/boot:/sys:/proc:/etc:/dev"
 
-target_dir = listener.configRegistry.get("ucsschool/listener/oldsharedir/targetdir")
-prefixlist = listener.configRegistry.get("ucsschool/listener/oldsharedir/prefixes", "").split(":")
-fs_types = listener.configRegistry.get("ucsschool/listener/oldsharedir/fs_types", DEFAUL_FS).split(":")
+target_dir = configRegistry.get("ucsschool/listener/oldsharedir/targetdir")
+prefixlist = configRegistry.get("ucsschool/listener/oldsharedir/prefixes", "").split(":")
+fs_types = configRegistry.get("ucsschool/listener/oldsharedir/fs_types", DEFAUL_FS).split(":")
 
 # either returns "" if everything is ok, or returns an error message
 
 
-def check_target_dir(dir):
-
-    if not dir:
+def check_target_dir(directory):
+    if not directory:
         return "targetdir is not set"
 
     # check target blacklist
-    tmp = dir.rstrip("/")
+    tmp = directory.rstrip("/")
     for i in TARGET_BLACKLIST.split(":"):
         if not tmp or tmp == i:
-            return "%s as target dir is invalid" % dir
+            return "%s as target directory is invalid" % directory
 
-    if os.path.exists(dir) and not os.path.isdir(dir):
-        return "%s is not a directory" % dir
+    if os.path.exists(directory) and not os.path.isdir(directory):
+        return "%s is not a directory" % directory
 
     # create directory
-    if not os.path.isdir(dir):
-        listener.setuid(0)
+    if not os.path.isdir(directory):
+        setuid(0)
         try:
-            os.makedirs(dir)
+            os.makedirs(directory)
         except EnvironmentError:
-            return "failed to create target directory %s" % dir
+            return "failed to create target directory %s" % directory
         finally:
-            listener.unsetuid()
+            unsetuid()
 
     # check fs
-    ret = check_filesystem(dir)
+    ret = check_filesystem(directory)
     if ret:
         return ret
 
     return ""
 
 
-# either returns "" if everything is ok, or returns an error message
+def check_source_dir(prefixlist, directory):
+    """either returns "" if everything is ok, or returns an error message"""
 
-
-def check_source_dir(prefixlist, dir):
-
-    # check dir
-    if not os.path.isdir(dir):
-        return "%s is not a directory" % dir
+    # check directory
+    if not os.path.isdir(directory):
+        return "%s is not a directory" % directory
 
     # check fs
-    ret = check_filesystem(dir)
+    ret = check_filesystem(directory)
     if ret:
         return ret
 
     # check allowed prefix
     for prefix in prefixlist:
-        if dir.startswith(prefix):
+        if directory.startswith(prefix):
             return ""
 
-    return "%s does not match any value in ucsschool/listener/oldsharedir/prefixes" % dir
+    return "%s does not match any value in ucsschool/listener/oldsharedir/prefixes" % directory
 
 
-# make sure that we are dealing with a known filesystem
+def check_filesystem(directory):
+    """make sure that we are dealing with a known filesystem"""
 
-
-def check_filesystem(dir):
-
-    ret, out = commands.getstatusoutput("LC_ALL=C stat -f '%s'" % dir)  # nosec
+    ret, out = getstatusoutput("LC_ALL=C stat -f %s" % pipes.quote(directory))
     myFs = ""
-    for line in out.split("\n"):
-        tmp = line.split("Type: ")
+    for line in out.splitlines():
+        tmp = line.split("Type: ", 1)
         if len(tmp) == 2:
             myFs = tmp[1].strip()
             for fs in fs_types:
@@ -132,74 +133,61 @@ def check_filesystem(dir):
                     # ok,
                     return ""
             break
-    return "%s for %s is not on a known filesystem" % (myFs, dir)
+    return "%s for %s is not on a known filesystem" % (myFs, directory)
 
 
-# move directory
-
-
-def move_dir(src, dst, listener):
-
+def move_dir(src, dst):
+    """move directory"""
     newName = os.path.basename(src) + ".%s" % int(time.time())
     dst = os.path.join(dst, newName)
     ret = ""
 
-    listener.setuid(0)
+    setuid(0)
     try:
         shutil.move(src, dst)
     except Exception as exc:
         ret = str(exc)
     finally:
-        listener.unsetuid()
+        unsetuid()
 
     return ret
 
 
 def handler(dn, new, old, command):
-
-    # remove empty share directories
-    # if object is really removed (not renamed)
+    """remove empty share directories
+    if object is really removed (not renamed)"""
     if old and not new and not command == "r":
-
-        name = old["cn"][0]
+        name = old["cn"][0].decode("UTF-8")
 
         # check object
         if not old.get("univentionShareHost") or not old.get("univentionSharePath"):
-            univention.debug.debug(
-                univention.debug.LISTENER,
-                univention.debug.WARN,
+            ud.debug(
+                ud.LISTENER,
+                ud.WARN,
                 "not removing directory of share %s: univentionShareHost(Path) ist not set" % name,
             )
             return
 
-        share_dir = old["univentionSharePath"][0]
+        share_dir = old["univentionSharePath"][0].decode("UTF-8")
 
         # check if target directory is okay
         ret = check_target_dir(target_dir)
         if ret:
-            univention.debug.debug(
-                univention.debug.LISTENER,
-                univention.debug.WARN,
-                "not removing directory of share %s: %s" % (name, ret),
-            )
+            ud.debug(ud.LISTENER, ud.WARN, "not removing directory of share %s: %s" % (name, ret))
             return
 
         # check source (share) directory
         ret = check_source_dir(prefixlist, share_dir)
         if ret:
-            univention.debug.debug(
-                univention.debug.LISTENER,
-                univention.debug.WARN,
-                "not removing share directory of share %s: %s" % (name, ret),
-            )
+            ud.debug(ud.LISTENER, ud.WARN, "not removing share directory of share %s: %s" % (name, ret))
             return
 
         # move it
-        ret = move_dir(share_dir, target_dir, listener)
+        ret = move_dir(share_dir, target_dir)
         if ret:
-            univention.debug.debug(
-                univention.debug.LISTENER,
-                univention.debug.ERROR,
+            ud.debug(
+                ud.LISTENER,
+                ud.ERROR,
                 "failed to move directory of share %s from %s to %s: %s"
                 % (name, share_dir, target_dir, ret),
             )
