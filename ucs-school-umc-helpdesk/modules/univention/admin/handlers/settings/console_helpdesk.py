62f18e2c6dcd098db9e4ec990cb9a0b576d951c9
Florian Best <best@univention.de>
2021-01-18 14:16:30 +0100

    Bug #52658: migrate settings/console_helpdesk to Python 3

--- ucs-school-umc-helpdesk/modules/univention/admin/handlers/settings/console_helpdesk.py
+++ ucs-school-umc-helpdesk/modules/univention/admin/handlers/settings/console_helpdesk.py
@@ -30,13 +30,10 @@
 # License with the Debian GNU/Linux or Univention distribution in file
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <http://www.gnu.org/licenses/>.
-import ldap
 
-import univention.admin.filter
 import univention.admin.handlers
 import univention.admin.localization
 import univention.admin.syntax
-import univention.debug
 from univention.admin.layout import Tab
 
 translation = univention.admin.localization.translation("univention.admin.handlers.settings.helpdesk")
@@ -46,10 +43,16 @@ module = "settings/console_helpdesk"
 operations = ["add", "edit", "remove", "search", "move"]
 superordinate = "settings/cn"
 
-childs = 0
+childs = False
 short_description = _("Settings: Console Helpdesk")
 long_description = _("Settings for Univention Console Helpdesk Module")
-options = {}
+options = {
+    'default': univention.admin.option(
+        short_description=short_description,
+        default=True,
+        objectClasses=["top", "univentionUMCHelpdeskClass"]
+    )
+}
 
 default_containers = ["cn=config,cn=console,cn=univention"]
 
@@ -59,8 +62,6 @@ property_descriptions = {
         short_description=_("Name"),
         long_description=_("Name of Console-Helpdesk-Settings-Object"),
         syntax=univention.admin.syntax.string_numbers_letters_dots,
-        multivalue=False,
-        options=[],
         required=True,
         may_change=False,
         identifies=True,
@@ -69,22 +70,13 @@ property_descriptions = {
         short_description=_("Description"),
         long_description=_("Description"),
         syntax=univention.admin.syntax.string,
-        multivalue=False,
-        options=[],
         dontsearch=True,
-        required=False,
-        may_change=True,
-        identifies=False,
     ),
     "category": univention.admin.property(
         short_description=_("Category"),
         long_description=_("Helpdesk Category"),
         syntax=univention.admin.syntax.string,
         multivalue=True,
-        options=[],
-        required=False,
-        may_change=True,
-        identifies=False,
     ),
 }
 
@@ -103,64 +95,6 @@ mapping.register("category", "univentionUMCHelpdeskCategory")
 class object(univention.admin.handlers.simpleLdap):
     module = module
 
-    def __init__(self, co, lo, position, dn="", superordinate=None, attributes=[]):
-        global mapping
-        global property_descriptions
 
-        self.co = co
-        self.lo = lo
-        self.dn = dn
-        self.position = position
-        self._exists = 0
-        self.mapping = mapping
-        self.descriptions = property_descriptions
-
-        super(object, self).__init__(co, lo, position, dn, superordinate, attributes)
-
-    def exists(self):
-        return self._exists
-
-    def _ldap_pre_create(self):
-        self.dn = "%s=%s,%s" % (
-            mapping.mapName("name"),
-            mapping.mapValue("name", self.info["name"]),
-            self.position.getDn(),
-        )
-
-    def _ldap_addlist(self):
-        return [("objectClass", ["top", "univentionUMCHelpdeskClass"])]
-
-
-def lookup(
-    co,
-    lo,
-    filter_s,
-    base="",
-    superordinate=None,
-    scope="sub",
-    unique=False,
-    required=False,
-    timeout=-1,
-    sizelimit=0,
-):
-
-    filter = univention.admin.filter.conjunction(
-        "&", [univention.admin.filter.expression("objectClass", "univentionUMCHelpdeskClass")]
-    )
-
-    if filter_s:
-        filter_p = univention.admin.filter.parse(filter_s)
-        univention.admin.filter.walk(filter_p, univention.admin.mapping.mapRewrite, arg=mapping)
-        filter.expressions.append(filter_p)
-
-    res = []
-    try:
-        for dn in lo.searchDn(unicode(filter), base, scope, unique, required, timeout, sizelimit):
-            res.append(object(co, lo, None, dn))
-    except ldap.LDAPError:
-        pass
-    return res
-
-
-def identify(dn, attr, canonical=0):
-    return "univentionUMCHelpdeskClass" in attr.get("objectClass", [])
+lookup = object.lookup
+identify = object.identify
