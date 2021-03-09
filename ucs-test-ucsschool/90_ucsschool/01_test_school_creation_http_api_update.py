423769344875d1efe42f6b1419e2884779c5f34f
Joerg Baach <baach@univention.de>
2021-03-09 15:54:59 +0100

    Bug #52853: working test

--- ucs-test-ucsschool/90_ucsschool/01_test_school_creation_http_api_update.py
+++ ucs-test-ucsschool/90_ucsschool/01_test_school_creation_http_api_update.py
@@ -6,41 +6,24 @@
 ## packages:
 ##   - python-ucs-school
 ##   - ucs-school-selfservice-support
+import os
+
+import django
+
 import univention.testing.strings as uts
 import univention.testing.ucsschool.ucs_test_school as utu
-from ucsschool.http_api.import_api.models import School
 
-# import pytest
-# import sys
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ucsschool.http_api.app.settings")
+django.setup()
+from ucsschool.http_api.import_api.models import School  # noqa: E402
 
 
 def test_school_creation():
-
     schoolenv = utu.UCSTestSchool()
     name = uts.random_name()
     schoolenv.create_ou(ou_name=name, use_cache=False)
-    print(School.objects.all().values_list("name", flat=True))
-    # TODO test for removing the school. Does not work  - somehow either
-    # the school does not seem to get deleted, or the listener does not seem
-    # to get called on remove.
-    #
-    #
-    # schoolenv.cleanup_ou(ou_name=name)
-    # # schoolenv.cleanup(wait_for_replication=True)
-    # # schoolenv.udm.cleanup()
-    # wait_for_listener_replication()
-    # res = udm.list_objects("container/ou")
-    # # res = mod.get(dn)
-    # print('z'*60)
-    # print(dn)
-    # print(res)
-    # ucr.load()
-    # ucr_value = ucr.get(ucrv, "")
-    # print("+"*100)
-    # print('yyy', ucr_value)
-    # assert value not in ucr_value
+    assert name in School.objects.all().values_list("name", flat=True)
 
 
 if __name__ == "__main__":
-    # pytest.main(sys.argv)
     test_school_creation()
