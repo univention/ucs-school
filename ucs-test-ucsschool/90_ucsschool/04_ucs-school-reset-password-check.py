93c74d0b221c4a418cb3521351ccec3672fc71cc
Arvid Requate <requate@univention.de>
2021-08-29 18:57:41 +0200

    Bug #53718: Sort existing test cases

--- ucs-test-ucsschool/90_ucsschool/04_ucs-school-reset-password-check.py
+++ ucs-test-ucsschool/90_ucsschool/04_ucs-school-reset-password-check.py
@@ -104,43 +104,43 @@ def main():
 
         utils.wait_for_replication_and_postrun()
 
-        print("#1 test if teacher is unable to reset teacher password (chgPwdNextLogin=True)")
-        test_pwd_reset(
-            host, teachers[0], "teacher", teachers[1], teachersDn[1], True, Forbidden, 200, 401
-        )
-
-        print("#2 test if student is unable to reset teacher password (chgPwdNextLogin=True)")
-        test_pwd_reset(
-            host, students[0], "teacher", teachers[1], teachersDn[1], True, Forbidden, 200, 401
-        )
-
-        print("#3 test if student is unable to reset student password (chgPwdNextLogin=True)")
-        test_pwd_reset(
-            host, students[0], "student", students[1], studentsDn[1], True, Forbidden, 200, 401
-        )
-
-        print("#4 test if teacher is unable to reset teacher password (chgPwdNextLogin=False)")
+        print("#1 test if teacher is unable to reset teacher password (chgPwdNextLogin=False)")
         test_pwd_reset(
             host, teachers[0], "teacher", teachers[1], teachersDn[1], False, Forbidden, 200, 401
         )
 
-        print("#5 test if student is unable to reset teacher password (chgPwdNextLogin=False)")
+        print("#2 test if teacher is unable to reset teacher password (chgPwdNextLogin=True)")
+        test_pwd_reset(
+            host, teachers[0], "teacher", teachers[1], teachersDn[1], True, Forbidden, 200, 401
+        )
+
+        print("#3 test if student is unable to reset teacher password (chgPwdNextLogin=False)")
         test_pwd_reset(
             host, students[0], "teacher", teachers[1], teachersDn[1], False, Forbidden, 200, 401
         )
 
-        print("#6 test if student is unable to reset student password (chgPwdNextLogin=False)")
+        print("#4 test if student is unable to reset teacher password (chgPwdNextLogin=True)")
+        test_pwd_reset(
+            host, students[0], "teacher", teachers[1], teachersDn[1], True, Forbidden, 200, 401
+        )
+
+        print("#5 test if student is unable to reset student password (chgPwdNextLogin=False)")
         test_pwd_reset(
             host, students[0], "student", students[1], studentsDn[1], False, Forbidden, 200, 401
         )
 
-        print("#7 test if teacher is able to reset student password (chgPwdNextLogin=True)")
+        print("#6 test if student is unable to reset student password (chgPwdNextLogin=True)")
+        test_pwd_reset(
+            host, students[0], "student", students[1], studentsDn[1], True, Forbidden, 200, 401
+        )
+
+        print("#7 test if teacher is able to reset student password (chgPwdNextLogin=False)")
+        test_pwd_reset(host, teachers[0], "student", students[0], studentsDn[0], False, True, 401, 200)
+
+        print("#8 test if teacher is able to reset student password (chgPwdNextLogin=True)")
         test_pwd_reset(
             host, teachers[0], "student", students[1], studentsDn[1], True, True, 401, 401, True
         )
-
-        print("#8 test if teacher is able to reset student password (chgPwdNextLogin=False)")
-        test_pwd_reset(host, teachers[0], "student", students[0], studentsDn[0], False, True, 401, 200)
 
         print("#9 test if schooladmin is able to reset student password (chgPwdNextLogin=False)")
         test_pwd_reset(host, admins[0], "student", students[0], studentsDn[0], False, True, 401, 200)
