cc73262742f74da51865a701107a54fd8b58779c
Florian Best <best@univention.de>
2021-09-02 12:11:23 +0200

    Bug #40709: temporarily disable - requires ucs-test >= 9.0.7-64

--- ucs-test-ucsschool/90_ucsschool/conftest.py
+++ ucs-test-ucsschool/90_ucsschool/conftest.py
@@ -1,1 +1,6 @@
-pytest_plugins = ["univention.testing.conftest", "univention.testing.ucsschool.conftest"]
+try:
+    import univention.testing.conftest  # noqa: F401
+except ImportError:
+    pytest_plugins = ["univention.testing.ucsschool.conftest"]
+else:
+    pytest_plugins = ["univention.testing.conftest", "univention.testing.ucsschool.conftest"]
