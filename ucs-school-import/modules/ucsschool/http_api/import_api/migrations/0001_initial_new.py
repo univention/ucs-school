9ac49f4ddf3dbd4a72fe481e66fd2a49cbee56dc
Florian Best <best@univention.de>
2021-09-15 17:55:08 +0200

    fixup! fixup! fixup! Bug #53451: replace djcelery with django-celery-results

--- ucs-school-import/modules/ucsschool/http_api/import_api/migrations/0001_initial_new.py
+++ ucs-school-import/modules/ucsschool/http_api/import_api/migrations/0001_initial_new.py
@@ -32,9 +32,9 @@
 
 from __future__ import unicode_literals
 
+import django.db.models.deletion
 from django.conf import settings
 from django.db import migrations, models
-import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
