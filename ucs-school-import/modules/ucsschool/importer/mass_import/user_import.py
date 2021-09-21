8de9875f4f3469e9b4a5ca01a5231138290d0038
Jürn Brodersen <brodersen@univention.de>
2021-09-21 13:37:15 +0200

    Bug #53816: get_imported_udm_property_names is now mandatory

--- ucs-school-import/modules/ucsschool/importer/mass_import/user_import.py
+++ ucs-school-import/modules/ucsschool/importer/mass_import/user_import.py
@@ -467,12 +467,7 @@ class UserImport(object):
                 # additional_udm_properties are loaded from ldap, so that remove hooks can
                 # work with them the same way as other hooks. For "A" and "M" operations,
                 # udm_properties are set by the reader class.
-                try:
-                    additional_udm_properties = self.reader.get_imported_udm_property_names(a_user)
-                except AttributeError:
-                    # For the unlikely case a reader does not inherit from the `BaseReader`
-                    # and has this function not yet implemented
-                    additional_udm_properties = []
+                additional_udm_properties = self.reader.get_imported_udm_property_names(a_user)
                 user = a_user.get_by_import_id(
                     self.connection, source_uid, record_uid, udm_properties=additional_udm_properties
                 )
