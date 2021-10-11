8d6dfc39eeb43d9e0770eb63c0c1a71026216a62
Daniel Troeder <troeder@univention.de>
2021-10-11 08:41:55 +0200

    Bug #53841: execute test in Python 3

--- ucs-test-ucsschool/90_ucsschool/255_import-users_summary_ok_in_dry_run.py
+++ ucs-test-ucsschool/90_ucsschool/255_import-users_summary_ok_in_dry_run.py
@@ -1,4 +1,4 @@
-#!/usr/share/ucs-test/runner python
+#!/usr/share/ucs-test/runner python3
 ## -*- coding: utf-8 -*-
 ## desc: Test that the csv summary contains the changed user dta also in dry-run
 ## tags: [apptest,ucsschool,ucsschool_import1]
