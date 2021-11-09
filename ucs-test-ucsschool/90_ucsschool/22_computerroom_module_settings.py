7183a76c3f0b575902272338252f076abd9050c6
Jürn Brodersen <brodersen@univention.de>
2021-11-09 12:13:47 +0100

    Bug #54039: Fix missing "Marktplatz" share
    Due to bug #54039 the "Marktplatz" share was missing
    (01_test_school_creation_marktplatz_share.py had triggered the bug
    resulting in a missing share).
    The test now creates a new share for new test school.
    
    Traceback (most recent call last):
      File "/usr/share/ucs-test/90_ucsschool/22_computerroom_module_settings.py", line 37, in test_computerroom_module_settings
        room.test_settings(school, teacher, teacher_dn, created_computers[1].ip[0], ucr, client)
      File "/usr/lib/python3/dist-packages/univention/testing/ucsschool/computerroom.py", line 597, in test_settings
        ucr,
      File "/usr/lib/python3/dist-packages/univention/testing/ucsschool/computerroom.py", line 643, in check_behavior
        self.check_share_behavior(user, ip_address, shareMode)
      File "/usr/lib/python3/dist-packages/univention/testing/ucsschool/computerroom.py", line 307, in check_share_behavior
        self.check_share_access(user, ip_address, 0, 0)
      File "/usr/lib/python3/dist-packages/univention/testing/ucsschool/computerroom.py", line 302, in check_share_access
        self.check_marktplatz_read(user, ip_address, expected_result=expected_marktplatz_result)
      File "/usr/lib/python3/dist-packages/univention/testing/ucsschool/computerroom.py", line 291, in check_marktplatz_read
        user, ip_address, share="Marktplatz", passwd=passwd, expected_result=expected_result
      File "/usr/lib/python3/dist-packages/univention/testing/ucsschool/computerroom.py", line 56, in decorated
        lambda: func(*args, **kwargs), exceptions=(CmdCheckFail), retry_count=8, delay=4
      File "/usr/lib/python3/dist-packages/univention/testing/utils.py", line 166, in retry_on_error
        return func()
      File "/usr/lib/python3/dist-packages/univention/testing/ucsschool/computerroom.py", line 56, in <lambda>
        lambda: func(*args, **kwargs), exceptions=(CmdCheckFail), retry_count=8, delay=4
      File "/usr/lib/python3/dist-packages/univention/testing/ucsschool/computerroom.py", line 1020, in check_share_read
        expected_result,
    AssertionError: Read share ('Marktplatz') result (1), expected (0)

--- ucs-test-ucsschool/90_ucsschool/22_computerroom_module_settings.py
+++ ucs-test-ucsschool/90_ucsschool/22_computerroom_module_settings.py
@@ -6,6 +6,9 @@
 ## exposure: dangerous
 ## packages: [ucs-school-umc-computerroom]
 
+from ucsschool.lib.models.utils import ucr as lib_ucr
+from univention.config_registry import handler_set
+from univention.testing import utils
 from univention.testing.network import NetworkRedirector
 from univention.testing.ucsschool.computerroom import Computers, Room
 from univention.testing.umc import Client
@@ -13,7 +16,16 @@ from univention.testing.umc import Client
 
 def test_computerroom_module_settings(schoolenv, ucr):
     with NetworkRedirector() as nethelper:
+        handler_set(["ucsschool/import/generate/share/marktplatz=yes"])
+        lib_ucr.load()
+        ucr.load()
         school, oudn = schoolenv.create_ou(name_edudc=ucr.get("hostname"), use_cache=False)
+        utils.wait_for_replication()
+        utils.verify_ldap_object(
+            "cn=Marktplatz,cn=shares,%s" % (oudn,),
+            strict=True,
+            should_exist=True,
+        )
         teacher, teacher_dn = schoolenv.create_user(school, is_teacher=True)
         open_ldap_co = schoolenv.open_ldap_connection()
 
