f3981a987c06cb8230b3f0e7ed3528a6a1a6a3f0
Tobias Wenzel <wenzel@univention.de>
2022-05-13 13:55:47 +0200

    Bug #54755: move import_computer to separate file

---
+++ ucs-school-import/modules/ucsschool/importer/utils/constants.py
@@ -0,0 +1,8 @@
+def get_default_prefixlen():
+    # IP address prefix len concerning the netmask
+    return 24
+
+
+def get_sep_char():
+    # separator char in infile (default: \t for Tabulator)
+    return "\t"
--- ucs-school-import/modules/ucsschool/importer/utils/utils.py
+++ ucs-school-import/modules/ucsschool/importer/utils/utils.py
@@ -28,10 +28,7 @@
 # License with the Debian GNU/Linux or Univention distribution in file
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <http://www.gnu.org/licenses/>.
-
-"""
-Diverse helper functions.
-"""
+from ucsschool.lib.models.school import School
 
 from contextlib import contextmanager
 
@@ -54,3 +51,8 @@ def get_ldap_mapping_for_udm_property(udm_prop, udm_type):
 def nullcontext():
     """Context manager that does nothing."""
     yield None
+
+
+def verify_school_ou(school_nr, lo):
+    if not School(name=school_nr).get_udm_object(lo):
+        raise SystemExit("ERROR: The specified school %r does not exist." % (school_nr,))
