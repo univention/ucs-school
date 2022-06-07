37d824de71efa0c76781543414e9e81622cd95c4
Tobias Wenzel <wenzel@univention.de>
2022-06-07 11:20:27 +0200

    Bug #54755: remove import from ucsschool lib test

--- ucs-test-ucsschool/90_ucsschool/402_ucsschool_lib_computers.py
+++ ucs-test-ucsschool/90_ucsschool/402_ucsschool_lib_computers.py
@@ -7,9 +7,9 @@
 ## packages:
 ##   - python3-ucsschool-lib
 
-from ucsschool.lib.models.computer import SchoolComputer
+import univention.testing.strings as uts
+from ucsschool.lib.models.computer import IPComputer, MacComputer, SchoolComputer, WindowsComputer
 from univention.admin.uldap import getAdminConnection
-from univention.testing.ucsschool.importcomputers import ComputerImport, ImportFile
 
 
 def test_lookup(schoolenv):
@@ -18,13 +18,32 @@ def test_lookup(schoolenv):
     SchoolComputer
     """
     ou_name, ou_dn = schoolenv.create_ou(name_edudc=schoolenv.ucr.get("hostname"))
-    print("********** Generate school data")
-    computer_import = ComputerImport(ou_name, nr_windows=3, nr_macos=3, nr_ipmanagedclient=3)
-    print(computer_import)
-    import_file = ImportFile(False, True)
-
-    print("********** Create computers")
-    import_file.run_import(computer_import)
+    lo = schoolenv.lo
+    for _ in range(3):
+        WindowsComputer(
+            **{
+                "school": ou_name,
+                "name": uts.random_name(),
+                "ip_address": [uts.random_ip()],
+                "mac_address": [uts.random_mac()],
+            }
+        ).create(lo)
+        MacComputer(
+            **{
+                "school": ou_name,
+                "name": uts.random_name(),
+                "ip_address": [uts.random_ip()],
+                "mac_address": [uts.random_mac()],
+            }
+        ).create(lo)
+        IPComputer(
+            **{
+                "school": ou_name,
+                "name": uts.random_name(),
+                "ip_address": [uts.random_ip()],
+                "mac_address": [uts.random_mac()],
+            }
+        ).create(lo)
     lo, po = getAdminConnection()
     computers = SchoolComputer.lookup(lo, ou_name)
     assert len(computers) == 9, computers
