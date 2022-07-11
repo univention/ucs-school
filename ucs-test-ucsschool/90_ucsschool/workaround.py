e1f4b872cd0bffa833e6241a9b34768af12b34f5
Felix Botner <botner@univention.de>
2022-07-11 17:05:23 +0200

    Issue #804: added workaround for samba-tool output
    univention/ucsschool#803

--- ucs-test-ucsschool/90_ucsschool/workaround.py
+++ ucs-test-ucsschool/90_ucsschool/workaround.py
@@ -6,6 +6,8 @@ def filter_deprecated(stderr):
     for line in stderr.splitlines():
         if line.endswith('WARNING: The "blocking locks" option is deprecated'):
             continue
+        if line.endswith('WARNING: Using password on command line is insecure. Please install the setproctitle python module.'):
+            continue
         else:
             errors.append(line)
     return "\n".join(errors).strip()
