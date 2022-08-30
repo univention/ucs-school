9d9cfdf98b48039c04b206357a69eef084f33d9c
Nico Gulden <gulden@univention.de>
2022-08-30 12:16:55 +0200

    Bug #55099: Point to German UCS manual instead of English
    The UCS@school Quickstart guide is written in German. Therefore, the
    pointer to the UCS manual point to the German version.

--- doc/ucsschool-quickstart/conf.py
+++ doc/ucsschool-quickstart/conf.py
@@ -113,7 +113,7 @@ figure_language_filename = "{root}-{language}{ext}"
 univention_use_doc_base = True
 
 intersphinx_mapping = {
-    "uv-manual": ("https://docs.software-univention.de/manual/5.0/en", None),
+    "uv-manual": ("https://docs.software-univention.de/manual/5.0/de", None),
     "uv-domain": ("https://docs.software-univention.de/ext-domain/5.0/en/", None),
     "uv-inst": ("https://docs.software-univention.de/ext-installation/5.0/en/", None),
 }
