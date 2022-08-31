012baf10006a7655cc250ef7452831fdf1b26843
Nico Gulden <gulden@univention.de>
2022-08-31 10:32:18 +0200

    Bug #55149: Add RST docs structure

---
+++ doc/ucsschool-import/conf.py
@@ -0,0 +1,124 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+from datetime import date
+
+sys.path.append(os.path.abspath("./_ext"))
+
+# -- Project information -----------------------------------------------------
+
+project = "UCS@school - Handbuch zur CLI-Import-Schnittstelle"
+copyright = "2021-{}, Univention GmbH".format(date.today().year)
+author = ""
+
+# The full version, including alpha/beta/rc tags
+release = "5.0"
+version = release
+
+html_show_copyright = True
+language = "de"
+
+html_title = project
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "univention_sphinx_extension",
+    "sphinxcontrib.spelling",
+    #    "sphinx_last_updated_by_git",
+    "sphinx.ext.intersphinx",
+    "sphinxcontrib.bibtex",
+]
+
+bibtex_bibfiles = ["../bibliography-de.bib"]
+bibtex_encoding = "utf-8"
+bibtex_default_style = "unsrt"
+bibtex_reference_style = "label"
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+pdf_doc_base = os.path.basename(os.path.dirname(__file__))
+
+html_theme = "univention_sphinx_book_theme"
+html_context = {
+    "pdf_download_filename": f"{pdf_doc_base}.pdf",
+}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = []  # value usually is ['_static']
+html_last_updated_fmt = "%a, %d. %b %Y at %H:%M (UTC%z)"
+
+# https://github.com/mgeier/sphinx-last-updated-by-git
+git_last_updated_timezone = "Europe/Berlin"
+
+numfig = True
+
+if "spelling" in sys.argv:
+    spelling_lang = "en_US"
+    spelling_show_suggestions = True
+    spelling_warning = True
+    spelling_word_list_filename = ["spelling-word-list.txt"]
+    # Warnings may come up by sphinx-last-updated-by-git. Shall be suppressed in spelling job
+    suppress_warnings = ["git.too_shallow"]
+
+if "linkcheck" in sys.argv:
+    suppress_warnings = ["git.too_shallow"]
+
+root_doc = "index"
+
+latex_engine = "lualatex"
+latex_show_pagerefs = True
+latex_show_urls = "footnote"
+latex_documents = [(root_doc, f"{pdf_doc_base}.tex", "", author, "howto", False)]
+latex_elements = {
+    "papersize": "a4paper",
+    "babel": "\\usepackage{babel}",
+}
+
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-figure_language_filename
+figure_language_filename = "{root}-{language}{ext}"
+
+univention_use_doc_base = True
+
+intersphinx_mapping = {
+    "uv-manual": ("https://docs.software-univention.de/manual/5.0/de", None),
+    "uv-domain": ("https://docs.software-univention.de/ext-domain/5.0/en/", None),
+    "uv-inst": ("https://docs.software-univention.de/ext-installation/5.0/en/", None),
+    "python": ("https://docs.python.org/3.7/", None),
+}
+
+rst_epilog = """
+.. include:: /../substitutions-de.txt
+"""
+
+linkcheck_allowed_redirects = {
+    r"https://help\.univention\.com/t/\d+": r"https://help\.univention\.com/t/[\w-]+/\d+",
+}
---
+++ doc/ucsschool-import/index.rst
@@ -0,0 +1,52 @@
+.. _audience:
+.. _introduction:
+
+###################################################
+UCS\@school - Handbuch zur CLI-Import Schnittstelle
+###################################################
+
+Dieses Handbuch richtet sich an Administratoren und Programmierer. Die
+Importsoftware ist im Auslieferungszustand stark konfigurierbar, kann aber
+zusätzlich programmatisch erweitert werden. In diesem Handbuch werden der Ablauf
+eines Importvorganges, Konfigurationsoptionen und Programmierschnittstellen
+beschrieben.
+
+|UCSUAS| bringt für viele regelmäßig wiederkehrende Verwaltungsaufgaben
+Werkzeuge und Schnittstellen mit. Die Übernahme von Benutzerdaten aus der
+Schulverwaltung ist eine dieser wiederkehrenden Aufgaben, die über die neue
+Importschnittstelle für Benutzer automatisiert erledigt werden kann.
+
+Der |UCSUAS| Import ermöglicht es Benutzerdaten aus einer Datei auszulesen, die
+Daten zu normieren, automatisch eindeutige Benutzernamen und E-Mail-Adressen zu
+generieren und notwendige Änderungen (Hinzufügen/Modifizieren/Löschen)
+automatisch zu erkennen. Er wurde so konzipiert, dass die Konten einer UCS
+Domäne automatisch mit dem Datenbestand eines vorhandenen Benutzerverzeichnisses
+abgeglichen werden können.
+
+Die Importschnittstelle ist darauf ausgelegt, mit möglichst geringem Aufwand an
+die unterschiedlichen Gegebenheiten in Schulen angepasst zu werden. So ist die
+Basis der Importschnittstelle bereits vorbereitet, um unterschiedliche
+Dateiformate einlesen zu können. |UCSUAS| bringt einen Importfilter für
+CSV-Dateien mit, der für unterschiedlichste CSV-Formate konfiguriert werden
+kann.
+
+Über eigene Python-Plugins kann die Schnittstelle erheblich erweitert werden.
+Dies umfasst sowohl die Unterstützung für zusätzliche Dateiformate als auch die
+Implementierung von zusätzlichen Automatismen, die während des Imports greifen.
+
+In den nachfolgenden Kapiteln werden der Ablauf eines Imports, die
+unterschiedlichen Konfigurationsmöglichkeiten sowie die
+Erweiterungsmöglichkeiten der Schnittstelle um neue Funktionalitäten
+beschrieben.
+
+.. toctree::
+   :numbered:
+
+   procedure
+   configuration/index
+   school-change
+   year-change
+   spanning-accounts
+   extending
+   bibliography
+   indices
