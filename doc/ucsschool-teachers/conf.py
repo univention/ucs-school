d2874a551a581ce60c41c0d2c4c114d88690b5b3
Nico Gulden <gulden@univention.de>
2022-09-19 12:49:42 +0200

    Bug #55212: Add RST docs structure

---
+++ doc/ucsschool-teachers/conf.py
@@ -0,0 +1,130 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+from datetime import date
+
+# -- Project information -----------------------------------------------------
+
+project = "UCS@school - Handbuch für Lehrkräfte"
+copyright = "2021-{}, Univention GmbH".format(date.today().year)
+author = ""
+
+# The full version, including alpha/beta/rc tags
+release = "5.0"
+version = release
+
+html_show_copyright = True
+language = "de"
+
+html_title = project
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "univention_sphinx_extension",
+    "sphinxcontrib.spelling",
+    "sphinx_last_updated_by_git",
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+pdf_doc_base = os.path.basename(os.path.dirname(__file__))
+
+html_theme = "univention_sphinx_book_theme"
+html_context = {
+    "pdf_download_filename": f"{pdf_doc_base}.pdf",
+}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = []  # value usually is ['_static']
+html_last_updated_fmt = "%a, %d. %b %Y at %H:%M (UTC%z)"
+
+# https://github.com/mgeier/sphinx-last-updated-by-git
+git_last_updated_timezone = "Europe/Berlin"
+
+numfig = True
+
+if "spelling" in sys.argv:
+    spelling_lang = "de_DE"
+    tokenizer_lang = "de_DE"
+    spelling_show_suggestions = True
+    spelling_warning = True
+    spelling_word_list_filename = []
+    # Warnings may come up by sphinx-last-updated-by-git. Shall be suppressed in spelling job
+    suppress_warnings = ["git.too_shallow"]
+
+if "linkcheck" in sys.argv:
+    suppress_warnings = ["git.too_shallow"]
+
+root_doc = "index"
+
+latex_engine = "lualatex"
+latex_show_pagerefs = True
+latex_show_urls = "footnote"
+latex_documents = [(root_doc, f"{pdf_doc_base}.tex", "", author, "howto", False)]
+latex_elements = {
+    "papersize": "a4paper",
+    "babel": "\\usepackage{babel}",
+}
+
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-figure_language_filename
+figure_language_filename = "{root}-{language}{ext}"
+
+univention_use_doc_base = True
+
+rst_epilog = """
+.. include:: /../substitutions-de.txt
+"""
+
+
+def adapt_settings_to_translation(app, config):
+    """
+    Sets the document title correctly according to the target language.
+
+    See https://github.com/sphinx-doc/sphinx/issues/10282
+    """
+    if config.language == "fr":
+        config.project = "UCS@school manuel pour les enseignants"
+        config.html_title = config.project
+        config.spelling_lang = "fr_FR"
+        config.tokenizer_lang = "fr_FR"
+        config.rst_epilog = """
+.. include:: /../substitutions-fr.txt
+
+.. include:: /links-fr.txt
+"""
+
+
+def setup(app):
+    app.connect(
+        "config-inited",
+        adapt_settings_to_translation,
+    )
