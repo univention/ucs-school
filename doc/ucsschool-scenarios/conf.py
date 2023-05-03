7106bcb5b93f016dc28690f0c716ddfc466f1a25
Nico Gulden <gulden@univention.de>
2023-05-03 15:43:09 +0200

    Bug #55945: Annotate license header to source files for ucsschool-scenarios docs

--- doc/ucsschool-scenarios/conf.py
+++ doc/ucsschool-scenarios/conf.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2021-2023 Univention GmbH
+#
+# SPDX-License-Identifier: AGPL-3.0-only
+
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
