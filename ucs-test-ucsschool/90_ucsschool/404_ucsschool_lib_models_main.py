1cc9f5d87067c8929841d5e7f1ce233ef5fae981
Jürn Brodersen <brodersen@univention.de>
2025-01-22 15:16:22 +0100

    Issue univention/ucsschool#1052: fix flaky tests
    Looks like samba was overwriting the changes the test was doing.
    
    Rule of thumb: Always wait for replication if you will edit/delete an
    object shortly after you created it.

--- ucs-test-ucsschool/90_ucsschool/404_ucsschool_lib_models_main.py
+++ ucs-test-ucsschool/90_ucsschool/404_ucsschool_lib_models_main.py
@@ -82,7 +82,7 @@ def test_list(cmd_line_role, ucr_hostname, ucr_ldap_base):
         user_names = {}
         for role in ("student", "teacher", "staff", "teacher_and_staff"):
             create_func = getattr(schoolenv, "create_{}".format(role))
-            user_name, user_dn = create_func(ou_name, wait_for_replication=False)
+            user_name, user_dn = create_func(ou_name)
             user_names.setdefault(role, []).append(user_name)
             cmd = [
                 sys.executable,
@@ -111,7 +111,7 @@ def test_list(cmd_line_role, ucr_hostname, ucr_ldap_base):
         # list multiple users for same OU
         for role in ("student", "teacher", "staff", "teacher_and_staff"):
             create_func = getattr(schoolenv, "create_{}".format(role))
-            user_name, user_dn = create_func(ou_name, wait_for_replication=False)
+            user_name, user_dn = create_func(ou_name)
             user_names.setdefault(role, []).append(user_name)
             cmd = [
                 sys.executable,
@@ -275,8 +275,8 @@ def test_create_user_windows_reserved_name(cmd_line_role, ucr_hostname, ucr_ldap
 def test_create_school_class(ucr_hostname, ucr_ldap_base):
     with utu.UCSTestSchool() as schoolenv:
         ou_name, ou_dn = schoolenv.create_ou(name_edudc=ucr_hostname)
-        user_name1, user_dn1 = schoolenv.create_student(ou_name, wait_for_replication=False)
-        user_name2, user_dn2 = schoolenv.create_teacher(ou_name, wait_for_replication=False)
+        user_name1, user_dn1 = schoolenv.create_student(ou_name)
+        user_name2, user_dn2 = schoolenv.create_teacher(ou_name)
         school_class_name = "{}-{}".format(ou_name, uts.random_username())
         description = uts.random_username()
         cmd = [
@@ -323,7 +323,7 @@ def test_modify(cmd_line_role, ucr_hostname, ucr_ldap_base):
         ou_name, ou_dn = schoolenv.create_ou(name_edudc=ucr_hostname)
         for role in ("student", "teacher", "staff", "teacher_and_staff"):
             create_func = getattr(schoolenv, "create_{}".format(role))
-            user_name, user_dn = create_func(ou_name, wait_for_replication=False)
+            user_name, user_dn = create_func(ou_name)
             person = Person(ou_name, role)
             person.set_random_birthday()
 
@@ -444,8 +444,8 @@ def test_modify(cmd_line_role, ucr_hostname, ucr_ldap_base):
             should_exist=True,
         )
         description = uts.random_username()
-        user_name1, user_dn1 = schoolenv.create_student(ou_name, wait_for_replication=False)
-        user_name2, user_dn2 = schoolenv.create_teacher(ou_name, wait_for_replication=False)
+        user_name1, user_dn1 = schoolenv.create_student(ou_name)
+        user_name2, user_dn2 = schoolenv.create_teacher(ou_name)
         cmd = [
             sys.executable,
             "-m",
@@ -487,7 +487,7 @@ def test_delete(cmd_line_role, ucr_hostname):
         ou_name, ou_dn = schoolenv.create_ou(name_edudc=ucr_hostname)
         for role in ("student", "teacher", "staff", "teacher_and_staff"):
             create_func = getattr(schoolenv, "create_{}".format(role))
-            user_name, user_dn = create_func(ou_name, wait_for_replication=False)
+            user_name, user_dn = create_func(ou_name)
             cmd = [
                 sys.executable,
                 "-m",
