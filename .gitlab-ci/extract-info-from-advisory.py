efde0e0ddb5cf7d50b4e044c932a8c005060c8c4
Jürn Brodersen <brodersen@univention.de>
2025-07-02 17:34:47 +0200

    ci: build ucs-test-ucsschool in pipeline
    Issue univention/dev/education/ucsschool#1456

--- .gitlab-ci/extract-info-from-advisory.py
+++ .gitlab-ci/extract-info-from-advisory.py
@@ -4,6 +4,7 @@ import urllib
 
 import requests
 import yaml
+from debian.changelog import Changelog
 from debian.deb822 import Deb822
 
 STAGING_PATH = pathlib.Path("doc/errata/staging")
@@ -28,8 +29,7 @@ def main():
     source_packages = set()
     for file_path in STAGING_PATH.glob("*.yaml"):
         print(f"Processing {file_path}")
-        with open(file_path) as yaml_file:
-            content = yaml.safe_load(yaml_file)
+        content = yaml.safe_load(file_path.read_text())
         bugs = bugs.union(set(content.get("bug", [])))
         source = content.get("src", None)
         scope = content.get("scope", None)
@@ -39,6 +39,13 @@ def main():
         if not is_build(source, fix, scope):
             print(f"Adding {source} to DEBIAN_SOURCE_DIRECTORIES")
             source_packages.add(source)
+    # ucs-test-ucsschool doesn't have an advisory
+    changelog = Changelog(pathlib.Path("ucs-test-ucsschool/debian/changelog").read_text())
+    content = yaml.safe_load((STAGING_PATH / pathlib.Path("0template.yaml")).read_text())
+    scope = content.get("scope", None)
+    if scope:
+        if not is_build("ucs-test-ucsschool", changelog.full_version, scope):
+            source_packages.add("ucs-test-ucsschool")
 
     DOTFILE.write_text(
         f"RELEASE_BUGS={' '.join(str(bug) for bug in bugs)}"
