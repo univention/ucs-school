daa88995167679be7adfe67191bf3627a0e287ee
Florian Best <best@univention.de>
2025-07-08 12:53:20 +0200

    style: replace Copyright header with SPDX headers from REUSE 3.3 specification
    (cherry picked from commit 531982b41fefe727de53e6a0b147f6a47721ab20)
    
    Issue univention/dev/education/ucsschool#1464

--- ucs-school-selfservice-support/usr/lib/univention-directory-listener/system/school_password_change.py
+++ ucs-school-selfservice-support/usr/lib/univention-directory-listener/system/school_password_change.py
@@ -1,30 +1,7 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright 2017-2025 Univention GmbH
-#
-# https://www.univention.de/
-#
-# All rights reserved.
-#
-# The source code of this program is made available
-# under the terms of the GNU Affero General Public License version 3
-# (GNU AGPL V3) as published by the Free Software Foundation.
-#
-# Binary versions of this program provided by Univention to you as
-# well as other copyrighted, protected or trademarked materials like
-# Logos, graphics, fonts, specific documentations and configurations,
-# cryptographic keys etc. are subject to a license agreement between
-# you and Univention.
-#
-# This program is provided in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public
-# License with the Debian GNU/Linux or Univention distribution in file
-# /usr/share/common-licenses/AGPL-3; if not, see
-# <https://www.gnu.org/licenses/>.
+# SPDX-FileCopyrightText: 2017-2025 Univention GmbH
+# SPDX-License-Identifier: AGPL-3.0-only
 #
 
 from __future__ import absolute_import
