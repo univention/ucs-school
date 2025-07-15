18f864a1276740da683c8fad702f15cb356f2f48
Jürn Brodersen <brodersen@univention.de>
2025-07-15 14:27:54 +0200

    ci: remove stdout and stderr for successful tests
    Decrease filesize so gitlab doesn't refuse to parse the junit file
    
    Issue univention/dev/education/ucsschool#1457

--- .gitlab-ci/parse_junit.py
+++ .gitlab-ci/parse_junit.py
@@ -4,8 +4,39 @@ import sys
 import xml.etree.ElementTree as ET  # nosec
 
 
-def count_errors(test_suite):
-    return int(test_suite.attrib["failures"]) + int(test_suite.attrib["errors"])
+def count_errors(junit_file):
+    error_counter = 0
+    tree = ET.parse(junit_file)  # noqa: S314
+    root = tree.getroot()
+    testsuites = root.findall("testsuite") if root.tag == "testsuites" else [root]
+
+    for testsuite in testsuites:
+        errors = int(testsuite.attrib.get("errors", "0"))
+        failures = int(testsuite.attrib.get("failures", "0"))
+        error_counter += errors + failures
+    return error_counter
+
+
+def clean_junit_xml(junit_file):
+    """
+    gitlab refuses to parse large junit files.
+    This removes stdout and stderr from sucessful tests to decrease the file size.
+    """
+    tree = ET.parse(junit_file)  # noqa: S314
+    root = tree.getroot()
+    testsuites = root.findall("testsuite") if root.tag == "testsuites" else [root]
+
+    for testsuite in testsuites:
+        errors = int(testsuite.attrib.get("errors", "0"))
+        failures = int(testsuite.attrib.get("failures", "0"))
+        if errors == 0 and failures == 0:
+            for testcase in testsuite.findall("testcase"):
+                for tag in ["system-out", "system-err"]:
+                    elem = testcase.find(tag)
+                    if elem is not None:
+                        testcase.remove(elem)
+
+    tree.write(junit_file, encoding="utf-8", xml_declaration=True)
 
 
 if __name__ == "__main__":
@@ -13,15 +44,8 @@ if __name__ == "__main__":
     error_counter = 0
     for junit_file in test_report_path.glob("*/test-reports/**/*.xml"):
         print(f"Parsing {junit_file}")
-        root = ET.fromstring(junit_file.open().read())  # noqa: S314
-        if root.tag == "testsuites":
-            for test_suite in root:
-                error_counter += count_errors(test_suite)
-        elif root.tag == "testsuite":
-            error_counter = count_errors(root)
-        else:
-            print("Parse error.", file=sys.stderr)
-            sys.exit(1)
-        if error_counter > 0:
-            print(f"Found {error_counter} errors.")
-            sys.exit(1)
+        error_counter += count_errors(junit_file)
+        clean_junit_xml(junit_file)
+    if error_counter > 0:
+        print(f"Found {error_counter} errors.")
+        sys.exit(1)
