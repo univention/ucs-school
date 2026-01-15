59b6b72f1813f9bce78bc7a7879d42dfc7fb9afc
Jürn Brodersen <brodersen@univention.de>
2026-01-15 15:26:23 +0100

    docs: spelling update
    Issue univention/dev/education/ucsschool#1445

--- doc/ucsschool-quickstart/conf.py
+++ doc/ucsschool-quickstart/conf.py
@@ -98,7 +98,7 @@ if "spelling" in sys.argv:
     tokenizer_lang = "de_DE"
     spelling_show_suggestions = True
     spelling_warning = True
-    spelling_word_list_filename = []
+    spelling_word_list_filename = ["spelling_wordlist"]
 
 root_doc = "contents"
 
--- doc/ucsschool-teachers/conf.py
+++ doc/ucsschool-teachers/conf.py
@@ -89,7 +89,7 @@ if "spelling" in sys.argv:
     tokenizer_lang = "de_DE"
     spelling_show_suggestions = True
     spelling_warning = True
-    spelling_word_list_filename = []
+    spelling_word_list_filename = ["spelling_wordlist"]
 
 root_doc = "index"
 
