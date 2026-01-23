27f39d577ec29cb9089459d8a9f1c8ccf79f4d16
Jürn Brodersen <brodersen@univention.de>
2026-01-23 12:18:16 +0100

    test: add ucs-school-umc-users as tag for branch tests
    Issue univention/dev/education/ucsschool#1497

--- ucs-test-ucsschool/90_ucsschool/04_ucs-school-reset-password-required-attr.py
+++ ucs-test-ucsschool/90_ucsschool/04_ucs-school-reset-password-required-attr.py
@@ -1,7 +1,7 @@
 #!/usr/share/ucs-test/runner pytest-3 -s -l -v
 ## desc: ucs-school-reset-password-required-attr
 ## roles: [domaincontroller_master, domaincontroller_slave]
-## tags: [apptest,ucsschool,ucsschool_base1]
+## tags: [apptest,ucsschool,ucsschool_base1,ucs-school-umc-users]
 ## exposure: dangerous
 ## packages: [ucs-school-umc-users]
 
