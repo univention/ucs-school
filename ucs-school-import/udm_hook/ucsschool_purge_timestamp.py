b06eb145c778c99a850b2ee43d0cf0ed44b57dc5
Florian Best <best@univention.de>
2026-04-13 18:47:37 +0200

    fix(school-import): fix unmap of UcsschoolPurgeTimestamp hook
    This fixes that the unmapping of data is not done too late during
    `obj.open()` but aleady during the unmap phase of UDM. Therefore all
    data is always in a consistent state.
    The LDAP data format (GeneralizedTime) is different from the UDM
    representation (ISO8601 date). Therefore having the wrong format in
    `self.info` causes that the propery doesn't validate according to it's
    syntax class. This causes e.g. transformation into the UDM REST API
    representation to fail when 1) delegative administration is enabled and
    the object is going to be send to Guardian or 2) the object is fetched
    from UDM REST API without opening or 3) the object is transformed in the
    NATS based provisioning system.
    
    Bug #59150

--- ucs-school-import/udm_hook/ucsschool_purge_timestamp.py
+++ ucs-school-import/udm_hook/ucsschool_purge_timestamp.py
@@ -33,65 +33,79 @@ import re
 
 from univention.admin.hook import simpleHook
 
+try:
+    from univention.admin.mapping import (
+        mapISO8601DateToGeneralizedTime,
+        unmapGeneralizedTimeToISO8601Date,
+    )
+except ImportError:
+    mapISO8601DateToGeneralizedTime = None
+    unmapGeneralizedTimeToISO8601Date = None
+
 
 class UcsschoolPurgeTimestamp(simpleHook):
     ldap_date_format = "%Y%m%d%H%M%SZ"
     udm_date_format = "%Y-%m-%d"
     udm_date_format_pattern = re.compile(r"\d\d\d\d-\d\d-\d\d")
 
-    def hook_open(self, module):
-        old_purge_ts = module.get("ucsschoolPurgeTimestamp")
-        if old_purge_ts and not self.udm_date_format_pattern.match(old_purge_ts):
-            module["ucsschoolPurgeTimestamp"] = self.ldap2udm(old_purge_ts.encode("UTF-8"))
-            module.save()
+    if mapISO8601DateToGeneralizedTime:
+        map = staticmethod(mapISO8601DateToGeneralizedTime)
+        unmap = staticmethod(unmapGeneralizedTimeToISO8601Date)
+    else:
 
-    def hook_ldap_addlist(self, module, al=None):
-        if al is None:
-            al = []
-        if module.info.get("ucsschoolPurgeTimestamp"):
-            al = self.convert_ts_in_list(al)
-        return al
+        def hook_open(self, module):
+            old_purge_ts = module.get("ucsschoolPurgeTimestamp")
+            if old_purge_ts and not self.udm_date_format_pattern.match(old_purge_ts):
+                module["ucsschoolPurgeTimestamp"] = self.ldap2udm(old_purge_ts.encode("UTF-8"))
+                module.save()
 
-    def hook_ldap_modlist(self, module, ml=None):
-        if ml is None:
-            ml = []
-        if module.hasChanged("ucsschoolPurgeTimestamp"):
-            ml = self.convert_ts_in_list(ml)
-        return ml
+        def hook_ldap_addlist(self, module, al=None):
+            if al is None:
+                al = []
+            if module.info.get("ucsschoolPurgeTimestamp"):
+                al = self.convert_ts_in_list(al)
+            return al
 
-    @classmethod
-    def convert_ts_in_list(cls, add_mod_list):
-        """Convert timestamp format in the list."""
-        for item in [it for it in add_mod_list if it[0] == "ucsschoolPurgeTimestamp"]:
-            # FIXME: does not handle list values
-            if len(item) == 2:
-                attr, add_val = item
-                remove_val = b""
-            else:
-                attr, remove_val, add_val = item
+        def hook_ldap_modlist(self, module, ml=None):
+            if ml is None:
+                ml = []
+            if module.hasChanged("ucsschoolPurgeTimestamp"):
+                ml = self.convert_ts_in_list(ml)
+            return ml
 
-            new_val = cls.udm2ldap(add_val.decode("UTF-8"))
-            if cls.udm_date_format_pattern.match(remove_val.decode("UTF-8")):
-                # LDAP value was converted on open(), convert back to original value
-                remove_val = cls.udm2ldap(remove_val.decode("UTF-8"))
-            add_mod_list.remove(item)
-            add_mod_list.append(("ucsschoolPurgeTimestamp", remove_val, new_val))
-        return add_mod_list
+        @classmethod
+        def convert_ts_in_list(cls, add_mod_list):
+            """Convert timestamp format in the list."""
+            for item in [it for it in add_mod_list if it[0] == "ucsschoolPurgeTimestamp"]:
+                # FIXME: does not handle list values
+                if len(item) == 2:
+                    attr, add_val = item
+                    remove_val = b""
+                else:
+                    attr, remove_val, add_val = item
 
-    @classmethod
-    def ldap2udm(cls, ldap_val):
-        """Convert b'20090101000000Z' to u'2009-01-01'. Ignores timezones."""
-        ldap_val = ldap_val.decode("utf-8")
-        if not ldap_val:
-            return ""
-        ldap_date = datetime.datetime.strptime(ldap_val, cls.ldap_date_format)
-        return ldap_date.strftime(cls.udm_date_format)
+                new_val = cls.udm2ldap(add_val.decode("UTF-8"))
+                if cls.udm_date_format_pattern.match(remove_val.decode("UTF-8")):
+                    # LDAP value was converted on open(), convert back to original value
+                    remove_val = cls.udm2ldap(remove_val.decode("UTF-8"))
+                add_mod_list.remove(item)
+                add_mod_list.append(("ucsschoolPurgeTimestamp", remove_val, new_val))
+            return add_mod_list
 
-    @classmethod
-    def udm2ldap(cls, udm_val):
-        """Convert u'2009-01-01' to b'20090101000000Z'. Ignores timezones."""
-        if not udm_val:
-            return b""
-        udm_date = datetime.datetime.strptime(udm_val, cls.udm_date_format)
-        udm_date = udm_date.strftime(cls.ldap_date_format)
-        return udm_date.encode("ASCII")
+        @classmethod
+        def ldap2udm(cls, ldap_val):
+            """Convert b'20090101000000Z' to u'2009-01-01'. Ignores timezones."""
+            ldap_val = ldap_val.decode("utf-8")
+            if not ldap_val:
+                return ""
+            ldap_date = datetime.datetime.strptime(ldap_val, cls.ldap_date_format)
+            return ldap_date.strftime(cls.udm_date_format)
+
+        @classmethod
+        def udm2ldap(cls, udm_val):
+            """Convert u'2009-01-01' to b'20090101000000Z'. Ignores timezones."""
+            if not udm_val:
+                return b""
+            udm_date = datetime.datetime.strptime(udm_val, cls.udm_date_format)
+            udm_date = udm_date.strftime(cls.ldap_date_format)
+            return udm_date.encode("ASCII")
