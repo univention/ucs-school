a4ad0de636eaae596c19b35106b659bba8a614b3
Jürn Brodersen <brodersen@univention.de>
2026-06-08 12:07:47 +0200

    test: force English net output in PDF printer driver check
    Bug #59323

--- ucs-test-ucsschool/00_checks/15_ucsschool_check_windows_printer_driver_for_pdf_printer.py
+++ ucs-test-ucsschool/00_checks/15_ucsschool_check_windows_printer_driver_for_pdf_printer.py
@@ -7,21 +7,28 @@
 
 from __future__ import print_function
 
+import os
 import subprocess
 
 
-def check_value(path, key, value):
+def check_value(path: str, key: str, value: str) -> None:
     cmd = ["net", "registry", "getvalue", path, key]
-    out, err = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-    found = False
+    # Force English output so we can match on the "Value " prefix below;
+    # the `net` tool is localized and would otherwise print a translated prefix.
+    env = dict(os.environ, LANG="C", LC_ALL="C")
+    out, _ = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env).communicate()
+    found = None
     print(path + " " + key)
     for i in out.decode("UTF-8").split("\n"):
         print(i)
         if i.startswith("Value "):
-            v = i.split("=")[1].strip().strip('"')
-            if v == value:
-                found = True
-    assert found, "value in '%s' is '%s' not!" % (path + "\\" + key, value)
+            found = i.split("=")[1].strip().strip('"')
+    assert found == value, "expected '%s\\%s' to be '%s', but got '%s'" % (
+        path,
+        key,
+        value,
+        found,
+    )
 
 
 def test_ucsschool_check_windows_printer_driver_for_pdf_printer(ucr):
